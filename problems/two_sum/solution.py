975a6c53db6617306b73d73ab095e25a08a89511
hwennnn <hwendev@gmail.com>
2019-10-06 15:12:42 +0000

    Sync LeetCode submission - Two Sum (python)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,9 +1,13 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mp = {}
-        
-        for i,x in enumerate(nums):
-            if target - x in mp:
-                return [mp[target - x], i]
-            
-            mp[x] = i
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i in range(len(nums)):
+            if target-nums[i] not in dict:
+                dict[nums[i]]=i
+            else:
+                return [dict[target-nums[i]],i]
