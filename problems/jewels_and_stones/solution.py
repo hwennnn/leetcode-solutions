ab1a08a1423728f13ec4784575ae3d8351abe166
hwennnn <hwendev@gmail.com>
2019-10-06 16:05:09 +0000

    Sync LeetCode submission - Jewels and Stones (python)

--- problems/jewels_and_stones/solution.py
+++ problems/jewels_and_stones/solution.py
@@ -1,4 +1,17 @@
-class Solution:
-    def numJewelsInStones(self, J: str, S: str) -> int:
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        lst = []
+        for i in J:
+            lst.append(i)
+        count=0
         
-        return sum([i in J for i in S])
+        for i in S:
+            if i in lst:
+                count+=1
+        return count
+            
