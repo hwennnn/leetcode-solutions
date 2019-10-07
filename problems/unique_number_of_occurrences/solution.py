49d5deb636aafab8de621ef7ab211600202e977c
hwennnn <hwendev@gmail.com>
2019-10-07 18:01:49 +0000

    Sync LeetCode submission - Unique Number of Occurrences (python)

--- problems/unique_number_of_occurrences/solution.py
+++ problems/unique_number_of_occurrences/solution.py
@@ -1,5 +1,31 @@
-class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        c = collections.Counter(arr)
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        res = []
+        lst=  []
         
-        return len(set(c.values())) == len(c)
+        for i in arr:
+            if i not in res:
+                res.append(i)
+                
+        for i in range(len(res)):
+            lst.append(arr.count(res[i]))
+        
+        check_lst = []
+        for i in range(len(lst)):
+            if i>0:
+                if lst[i-1] == lst[i]:
+                    break
+            check_lst.append(lst.count(lst[i]))
+        
+        for i in check_lst:
+            if i>1:
+                return False
+            
+        return True
+        
+        
+       
