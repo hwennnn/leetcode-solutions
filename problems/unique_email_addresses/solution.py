fadbbf2d4ca8378d2a50c5ceb52533aba8a9fd19
hwennnn <hwendev@gmail.com>
2019-10-08 06:08:14 +0000

    Sync LeetCode submission - Unique Email Addresses (python)

--- problems/unique_email_addresses/solution.py
+++ problems/unique_email_addresses/solution.py
@@ -1,17 +1,36 @@
-class Solution:
-    def numUniqueEmails(self, emails: List[str]) -> int:
-        s = set()
-        
-        for email in emails:
-            email, provider = email.split("@")
-            curr = ""
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+
+        lst = []
+        for x in emails:
+ 
+            res = ""
+            a_index = x.index("@")
+            try:
+                var = x.index("+")
+            except:
+                var = None
+
+            for i in range(len(x)):
+                if var != None:
+                    if var<=i<a_index:
+                        continue
+                if i < a_index:
+                    if x[i] == "." :
+                        continue
+                    else:
+                        res+=x[i]
+
+                else:
+                    res+=x[i]
+
+            lst.append(res)
             
-            for x in email:
-                if x == ".": continue
-                if x == "+": break
+
+        return (len(set(lst)))
+                    
                 
-                curr += x
-            
-            s.add(curr + "@" + provider)
-        
-        return len(s)
