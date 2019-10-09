0b4a148116ab184d366cdb3c1c20fbeb74cfeb29
hwennnn <hwendev@gmail.com>
2019-10-09 08:53:19 +0000

    Sync LeetCode submission - Self Dividing Numbers (python)

--- problems/self_dividing_numbers/solution.py
+++ problems/self_dividing_numbers/solution.py
@@ -1,22 +1,24 @@
-class Solution:
-    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
-        
-        temp = []
-        check = True
-        for i in range(left,right+1):
-            
-            if i <10:
-                temp.append(i)
-            
-            elif "0" not in str(i):
-
+class Solution(object):
+    def selfDividingNumbers(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: List[int]
+        """
+        ans = []
+        isSelfDividing = True
+        for i in range(left, right+1):
+        # single digit
+            if i < 10:
+                ans.append(i)      
+            # check if not contains 0, aka mod 10 != 0
+            elif '0' not in str(i):
                 for d in str(i):
-                    if i%int(d) !=0:
-                        print(i)
-                        check = False
+                    if i%int(d):
+                        isSelfDividing = False
                         break
-                if check:
-                    temp.append(i)
-        
-            check = True
-        return temp
+                if isSelfDividing:
+                    ans.append(i)
+
+                isSelfDividing = True
+        return ans           
