eb837eee0e7f3a952466ceec72da9a4341fd98ac
hwennnn <hwendev@gmail.com>
2019-10-09 15:02:10 +0000

    Sync LeetCode submission - Subsets II (python)

--- problems/subsets_ii/solution.py
+++ problems/subsets_ii/solution.py
@@ -1,22 +1,13 @@
 class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        
-        n = len(nums)
+    def subsetsWithDup(self, nums):
+        if not nums:
+            return []
         nums.sort()
-        ans = [[]]
-        start = 1
-        
-        for i in range(n):
-            temp = []
-            for j, num in enumerate(ans):
-                if i > 0 and nums[i] == nums[i-1] and j < start: continue
-                    
-                c = num + [nums[i]]
-                temp.append(c)
-            
-            start = len(ans)
-            ans += temp
-        
-        return ans
-                
-        
+        res, cur = [[]], []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                cur = [item + [nums[i]] for item in cur]
+            else:
+                cur = [item + [nums[i]] for item in res]
+            res += cur
+        return res
