efde1a607d83cf097db39ab517accf43857e3d98
hwennnn <hwendev@gmail.com>
2020-02-22 05:53:14 +0000

    Sync LeetCode submission - Find N Unique Integers Sum up to Zero (python3)

--- problems/find_n_unique_integers_sum_up_to_zero/solution.py
+++ problems/find_n_unique_integers_sum_up_to_zero/solution.py
@@ -1,15 +1,4 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
-        res = []
-        if n & 1: res.append(0)
-            
-        for i in range(-1000,-1000+n//2):
-            res.append(i)
-
-
-        for i in range(1000, 1000 - n//2, -1):
-            res.append(i)
-
         
-        return res
-            
+        return range(1 - n, n, 2)
