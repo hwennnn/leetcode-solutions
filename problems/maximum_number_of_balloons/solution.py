d97f3e5f97dcead9cd9992473d3a0adf9540bdc4
hwennnn <hwendev@gmail.com>
2020-02-23 14:19:35 +0000

    Sync LeetCode submission - Maximum Number of Balloons (python3)

--- problems/maximum_number_of_balloons/solution.py
+++ problems/maximum_number_of_balloons/solution.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        counter = collections.Counter(text)
         
-        return min(counter['b'], counter['a'], counter['l'] // 2, counter['o'] // 2, counter['n'])
+        cnt = collections.Counter(text)
+        cntBalloon = collections.Counter('balloon')
+        return min([cnt[c] // cntBalloon[c] for c in cntBalloon])
