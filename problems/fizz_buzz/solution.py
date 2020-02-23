154adacd4df9653ac0451aecb2854a3edbe415cf
hwennnn <hwendev@gmail.com>
2020-02-23 13:41:08 +0000

    Sync LeetCode submission - Fizz Buzz (python3)

--- problems/fizz_buzz/solution.py
+++ problems/fizz_buzz/solution.py
@@ -1,15 +1,23 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        ans = []
+        
+        temp = []
         
         for i in range(1,n+1):
-            if i%3 == 0 and i%5 == 0: ans.append("FizzBuzz")
             
-            elif i%3 == 0: ans.append("Fizz")
+            if i%3 == 0 and i%5 == 0:
+                temp.append("FizzBuzz")
+            
+            elif i%3 == 0:
+                temp.append("Fizz")
                 
-            elif i%5 == 0: ans.append("Buzz")
-            
+            elif i%5 == 0:
+                temp.append("Buzz")
+                
             else:
-                ans.append(str(i))
+                temp.append(str(i))
+                
         
-        return ans
+        return temp
+                
+            
