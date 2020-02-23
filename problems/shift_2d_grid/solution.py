85d326f3ce70f647b4079538762e545df24804e1
hwennnn <hwendev@gmail.com>
2020-02-23 15:06:26 +0000

    Sync LeetCode submission - Shift 2D Grid (python3)

--- problems/shift_2d_grid/solution.py
+++ problems/shift_2d_grid/solution.py
@@ -1,23 +1,25 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-        rows, cols = len(grid), len(grid[0])
-        k %= rows * cols
         
-        for _ in range(k):
-            temp = [[0] * cols for _ in range(rows)]
-            
-            for i in range(rows):
-                if i + 1 < rows:
-                    temp[i + 1][0] = grid[i][cols - 1]
-                    
-                for j in range(cols):
-                    if j + 1 < cols:
-                        temp[i][j + 1] = grid[i][j]
-                
-            temp[0][0] = grid[rows - 1][cols - 1]
-            
-            grid = temp
         
-        return grid
+        m = len(grid)
+        n = len(grid[0])
 
-    
+        temp = [x for i in grid for x in i]
+
+        shift = k%len(temp)
+
+        lst = temp[len(temp)-shift:] + temp[:len(temp)-shift]
+
+        ans = []
+        count = 0
+        for row in range(m):
+            x = []
+
+            for column in range(n):
+                x.append(lst[count])
+                count+=1
+
+            ans.append(x)   
+
+        return ans
