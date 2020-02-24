22b057ee20f2ce0536dbba88794154c7ec7cd6ff
hwennnn <hwendev@gmail.com>
2020-02-24 15:49:31 +0000

    Sync LeetCode submission - Convert Integer to the Sum of Two No-Zero Integers (python3)

--- problems/convert_integer_to_the_sum_of_two_no-zero_integers/solution.py
+++ problems/convert_integer_to_the_sum_of_two_no-zero_integers/solution.py
@@ -1,19 +1,8 @@
 class Solution:
     def getNoZeroIntegers(self, n: int) -> List[int]:
         
-        def findZeroes(n):
-            return str(n).count("0") > 0
-        
-        a, b = 1, n - 1
-        
-        while findZeroes(a) or findZeroes(b):
-            if findZeroes(a):
-                a += 1
-                b -= 1
-            if findZeroes(b):
-                a += 1
-                b -= 1
-        
-        return [a,b]
+        a = 1
+        while '0' in f'{a}{n-a}':
+            a+=1
             
-        
+        return [a,n-a]
