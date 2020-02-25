84ba0b29d66f4f5a2bc1bdcd6a4d236996085934
hwennnn <hwendev@gmail.com>
2020-02-25 16:57:50 +0000

    Sync LeetCode submission - Add Digits (python3)

--- problems/add_digits/solution.py
+++ problems/add_digits/solution.py
@@ -1,13 +1,4 @@
 class Solution:
     def addDigits(self, num: int) -> int:
         
-        while num >= 10:
-            s = 0
-            
-            while num > 0:
-                s += num % 10
-                num //= 10
-            
-            num = s
-            
-        return num
+        return num % 9 if num % 9 else 9 if num else 0
