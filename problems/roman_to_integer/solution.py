a3a3d4063998a78d55abffb8a029f731ea1e774b
hwennnn <hwendev@gmail.com>
2020-02-26 13:37:33 +0000

    Sync LeetCode submission - Roman to Integer (python3)

--- problems/roman_to_integer/solution.py
+++ problems/roman_to_integer/solution.py
@@ -1,16 +1,13 @@
 class Solution:
-    def romanToInt(self, s: str):
-        
-        mp = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    def romanToInt(self, s: str) -> int:
+        _dict = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         prev = 0
-        total = 0
-        
+        sum = 0
         for i in s[::-1]:
-            curr = mp[i]
+            curr = _dict[i]
             if prev > curr:
-                total -= curr
+                sum -= curr
             else:
-                total += curr
+                sum += curr
             prev = curr
-            
-        return total
+        return sum
