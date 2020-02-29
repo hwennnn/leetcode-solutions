7325aee0a48d2de4fb4626e04982d491b403926b
hwennnn <hwendev@gmail.com>
2020-02-29 15:21:06 +0000

    Sync LeetCode submission - Rotate Array (python3)

--- problems/rotate_array/solution.py
+++ problems/rotate_array/solution.py
@@ -1,16 +1,25 @@
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        k %= n
-        
-        def reverseL(start, end):
-            while start < end:
-                nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
-        
-        reverseL(0, n - 1)
-        reverseL(0, k - 1)
-        reverseL(k, n - 1)
-        
-        
+    def rotate(self, nums, k) -> None:
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        self.reverse(nums,0,len(nums)-1)
+        self.reverse(nums,0, k-1)
+        self.reverse(nums,k, len(nums)-1)
+
+    def reverse(self, nums, start, end) -> None:
+        """
+        :type nums: List[int]
+        :type start: int
+        :type end: int
+        :rtype: None
+        """
+        while start < end: #
+            temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp 
+            start += 1
+            end -= 1
