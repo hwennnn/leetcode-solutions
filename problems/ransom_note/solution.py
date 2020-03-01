ba6100abf479b2392970e71b95b71cc09fdb6f9f
hwennnn <hwendev@gmail.com>
2020-03-01 19:09:07 +0000

    Sync LeetCode submission - Ransom Note (python3)

--- problems/ransom_note/solution.py
+++ problems/ransom_note/solution.py
@@ -1,13 +1,3 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        mp = collections.Counter(ransomNote)
-        n = len(mp)
-        
-        for word in magazine:
-            mp[word] -= 1
-            if mp[word] == 0:
-                n -= 1
-            
-            if n == 0: return True
-        
-        return False
+        return not collections.Counter(ransomNote) - collections.Counter(magazine) 
