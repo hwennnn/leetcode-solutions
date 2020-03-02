10542516d1425d7ea2ec18f7c2daeb3e8a3d3a02
hwennnn <hwendev@gmail.com>
2020-03-02 18:36:03 +0000

    Sync LeetCode submission - Happy Number (python3)

--- problems/happy_number/solution.py
+++ problems/happy_number/solution.py
@@ -1,16 +1,12 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()
         
-        while n != 1 and n not in seen:
-            seen.add(n)
-            curr = 0
+        mem = set()
+        
+        while n not in mem:
+            mem.add(n)
+            n = sum(int(i)**2 for i in str(n))
             
-            while n > 0:
-                a, b = divmod(n, 10)
-                curr += b * b
-                n = a
-            
-            n = curr
         
         return n == 1
+                
