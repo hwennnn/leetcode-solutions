d3a3def9ec6feb9b8f5765a0b09d08785f7b3227
hwennnn <hwendev@gmail.com>
2020-03-03 12:59:56 +0000

    Sync LeetCode submission - Third Maximum Number (python3)

--- problems/third_maximum_number/solution.py
+++ problems/third_maximum_number/solution.py
@@ -1,19 +1,16 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        if not nums: return 0
         
-        first = second = third = float('-inf')
+        one = two = three = -float('inf')
         
-        for num in nums:
-            if num > first:
-                first, second, third = num, first, second
+        for i in nums:
+            if i > one:
+                one, two, three = i, one, two
             
-            elif num > second and num < first:
-                second, third = num, second
-            
-            elif num > third and num < second:
-                third = num
-            
-            print(first, second, third)
-        
-        return third if third != float('-inf') else first
+            elif i > two and i < one:
+                two, three = i, two
+                
+            elif i > three and i < two:
+                three = i
+                
+        return three if three != -float('inf') else one
