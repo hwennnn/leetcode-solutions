0e51eed4933cf74d03d6b556bce2167df52dc50f
hwennnn <hwendev@gmail.com>
2020-03-06 07:13:16 +0000

    Sync LeetCode submission - Merge Sorted Array (python3)

--- problems/merge_sorted_array/solution.py
+++ problems/merge_sorted_array/solution.py
@@ -3,28 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        index = m + n - 1
-        m -= 1
-        n -= 1
         
-        while m >= 0 and n >= 0:
-            if nums1[m] > nums2[n]:
-                nums1[index] = nums1[m]
-                m -= 1
-            else:
-                nums1[index] = nums2[n]
-                n -= 1
-                
-            index -= 1
+        for i in range(n):
+            nums1[m+i] = nums2[i]
+
+        nums1[:] = sorted(nums1)
         
-        while m >= 0:
-            nums1[index] = nums1[m]
-            m -= 1
-            index -= 1
         
-        while n >= 0:
-            nums1[index] = nums2[n]
-            n -= 1
-            index -= 1
-        
-        return nums1
