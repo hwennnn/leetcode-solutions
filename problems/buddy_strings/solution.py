deb1d65f377dc994f7eada25270df8d8ae3172f6
hwennnn <hwendev@gmail.com>
2020-03-07 14:33:11 +0000

    Sync LeetCode submission - Buddy Strings (python3)

--- problems/buddy_strings/solution.py
+++ problems/buddy_strings/solution.py
@@ -1,9 +1,7 @@
 class Solution:
     def buddyStrings(self, A: str, B: str) -> bool:
+        
         if len(A) != len(B): return False
-        
-        if A == B and len(set(A)) < len(B): return True
-        
-        diff = [(a,b) for a,b in zip(A,B) if a != b]
-        
-        return len(diff) == 2 and diff[0] == diff[1][::-1]
+        if A == B and len(set(A)) < len(A): return True
+        dif = [(a, b) for a, b in zip(A, B) if a != b]
+        return len(dif) == 2 and dif[0] == dif[1][::-1]
