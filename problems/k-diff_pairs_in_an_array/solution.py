899097698c900ba36a14a7b5443dc04e3cf84a36
hwennnn <hwendev@gmail.com>
2020-03-07 15:49:54 +0000

    Sync LeetCode submission - K-diff Pairs in an Array (python3)

--- problems/k-diff_pairs_in_an_array/solution.py
+++ problems/k-diff_pairs_in_an_array/solution.py
@@ -1,10 +1,9 @@
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        s = Counter(nums)
-        res = 0
         
-        for x in s:
-            if k == 0 and s[x] >= 2 or k != 0 and k + x in s:
+        res = 0
+        c = collections.Counter(nums)
+        for i in c:
+            if k > 0 and i + k in c or k == 0 and c[i] > 1:
                 res += 1
-
         return res
