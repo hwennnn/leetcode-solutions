ac617929757a81f86e24653825623c111349879b
hwennnn <hwendev@gmail.com>
2020-03-08 17:00:18 +0000

    Sync LeetCode submission - Shortest Unsorted Continuous Subarray (python3)

--- problems/shortest_unsorted_continuous_subarray/solution.py
+++ problems/shortest_unsorted_continuous_subarray/solution.py
@@ -1,23 +1,6 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        n = len(nums)
         
-        right, prefixMax = -1, nums[0]
+        check = [a == b for a,b in zip(nums,sorted(nums))]
         
-        for i in range(1, n):
-            if nums[i] < prefixMax:
-                right = i
-            else:
-                prefixMax = nums[i]
-        
-        if right == -1: return 0
-        
-        left, suffixSmall = n - 1, nums[-1]
-        
-        for i in range(n - 2, -1, -1):
-            if nums[i] > suffixSmall:
-                left = i
-            else:
-                suffixSmall = nums[i]
-        
-        return right - left + 1
+        return 0 if all(check) else len(nums) - check.index(False) - check[::-1].index(False)
