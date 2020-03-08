032436d02b5e5f43a980e669f974607709f8f9cf
hwennnn <hwendev@gmail.com>
2020-03-08 08:25:26 +0000

    Sync LeetCode submission - Plus One (python3)

--- problems/plus_one/solution.py
+++ problems/plus_one/solution.py
@@ -1,16 +1,4 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        res = []
-        digits[-1] += 1
-        carry = 0
-        
-        for x in digits[::-1]:
-            carry += x
-            
-            res.append(carry % 10)
-            carry //= 10
-        
-        if carry > 0:
-            res.append(carry)
-        
-        return res[::-1]
+         
+        return map(int,list(str(int("".join(map(str,digits))) + 1)))
