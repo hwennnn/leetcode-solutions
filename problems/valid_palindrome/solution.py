33b67bee2d59c6ee848fa769e0b1b32f94d69e13
hwennnn <hwendev@gmail.com>
2020-03-11 12:01:14 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

--- problems/valid_palindrome/solution.py
+++ problems/valid_palindrome/solution.py
@@ -1,12 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        beg, end = 0, len(s) - 1
-        while beg < end:
-            while not s[beg].isalnum() and beg < end: beg += 1
-            while not s[end].isalnum() and beg < end: end -= 1
-            if s[beg] == s[end] or s[beg].upper() == s[end].upper():
-                beg, end = beg + 1, end - 1
-            else:
-                return False
-        return True
         
+        string = "".join(ch.lower() for ch in s if ch.isalnum())
+        
+        return string == string[::-1]
