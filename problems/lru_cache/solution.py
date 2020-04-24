ee0e3ebf4557d3133b7aa635ac1895b68f7c8a48
hwennnn <hwendev@gmail.com>
2020-04-24 15:15:02 +0000

    Sync LeetCode submission - LRU Cache (python3)

--- problems/lru_cache/solution.py
+++ problems/lru_cache/solution.py
@@ -1,64 +1,32 @@
-class Node:
-    
-    def __init__(self, key = None, value = None):
-        self.key = key
-        self.value = value
-        self.prev = self.next = None
-
 class LRUCache:
+    def __init__(self, MSize):
+        self.size = MSize
+        self.cache = {}
+        self.next, self.before = {}, {}
+        self.head, self.tail = '#', '$'
+        self.connect(self.head, self.tail)
 
-    def __init__(self, capacity: int):
-        self.size = 0
-        self.capacity = capacity
-        self.mp = dict()
-        self.head = Node()
-        self.tail = Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
-    
-    def append(self, node):
-        node.prev = self.head
-        node.next = self.head.next
-        node.next.prev = node
-        self.head.next = node
+    def connect(self, a, b):
+        self.next[a], self.before[b] = b, a
 
-    def get(self, key: int) -> int:
-        if key not in self.mp: return -1
-        
-        node = self.mp[key]
-        self.pop(node)
-        self.append(node)
-        
-        return node.value
+    def delete(self, key):
+        self.connect(self.before[key], self.next[key])
+        del self.before[key], self.next[key], self.cache[key]
 
-    def put(self, key: int, value: int) -> None:
-        if key in self.mp:
-            node = self.mp[key]
-            self.pop(node)
-            node.value = value
-            self.append(node)
-        else:
-            if self.size == self.capacity:
-                node = self.pop()
-                del self.mp[node.key]
-                self.size -= 1
-                
-            node = Node(key, value)
-            self.mp[key] = node
-            self.append(node)
-            self.size += 1
-            
-    def pop(self, node = None):
-        if not node:
-            node = self.tail.prev
-        
-        p = node.prev
-        n = node.next
-        p.next = n
-        n.prev = p
-        
-        return node
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+    def append(self, k, v):
+        self.cache[k] = v
+        self.connect(self.before[self.tail], k)
+        self.connect(k, self.tail)
+        if len(self.cache) > self.size:
+            self.delete(self.next[self.head])
+
+    def get(self, key):
+        if key not in self.cache: return -1
+        val = self.cache[key]
+        self.delete(key)
+        self.append(key, val)
+        return val
+
+    def put(self, key, value):
+        if key in self.cache: self.delete(key)
+        self.append(key, value)
