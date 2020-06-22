c999fa0c8eba2aa62655df359833e9d0c504c379
hwennnn <hwendev@gmail.com>
2020-06-22 16:03:59 +0000

    Sync LeetCode submission - Majority Element (python3)

--- problems/majority_element/solution.py
+++ problems/majority_element/solution.py
@@ -1,4 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
-        return sorted(nums)[len(nums)//2]
+        major, count = nums[0], 1
+        
+        for i in range(1, len(nums)):
+            
+            if count == 0:
+                major = nums[i]
+                count+=1;
+            
+            elif major == nums[i]:
+                count+=1;
+            
+            else:
+                count-=1;
+                
+        return major
+                
