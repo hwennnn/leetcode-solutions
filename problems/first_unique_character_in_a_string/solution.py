9494d5e82505889cc7e735df02f9aa64413f899d
hwennnn <hwendev@gmail.com>
2020-06-22 15:00:46 +0000

    Sync LeetCode submission - First Unique Character in a String (python3)

--- problems/first_unique_character_in_a_string/solution.py
+++ problems/first_unique_character_in_a_string/solution.py
@@ -4,3 +4,4 @@ class Solution:
         letters='abcdefghijklmnopqrstuvwxyz'
         index=[s.index(l) for l in letters if s.count(l) == 1]
         return min(index) if len(index) > 0 else -1
+        
