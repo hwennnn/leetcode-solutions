bb29fd2b087d0e30d86d0c58aaa64d7b4d6acb44
hwennnn <hwendev@gmail.com>
2020-08-02 05:44:13 +0000

    Sync LeetCode submission - Find the Winner of an Array Game (cpp)

--- problems/find_the_winner_of_an_array_game/solution.cpp
+++ problems/find_the_winner_of_an_array_game/solution.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
-        for (int i=1, j=0; i < arr.size() && j < k; ++i){
-            if (arr[0] > arr[i])
+        
+        for (int i=1,j=0; i < arr.size() && j < k; ++i){
+            
+            if (arr[0]>arr[i]){
                 ++j;
-            else{
-                swap(arr[0], arr[i]);
+            }else{
+                swap(arr[0],arr[i]);
                 j = 1;
             }
-                
         }
         
         return arr[0];
     }
 };
-
