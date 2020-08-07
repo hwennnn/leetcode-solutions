3919e7daf4e75930279d06baba2b356bd1a953dc
hwennnn <hwendev@gmail.com>
2020-08-07 18:55:12 +0000

    Sync LeetCode submission - Detect Capital (python3)

--- problems/detect_capital/solution.py
+++ problems/detect_capital/solution.py
@@ -1,10 +1,5 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        capitals = 0
-        n = len(word)
         
-        for x in word:
-            if ord("A") <= ord(x) <= ord("Z"):
-                capitals += 1
+        return word == word.lower() or word == word.upper() or word == word.capitalize()
         
-        return capitals == n or capitals == 0 or (capitals == 1 and ord("A") <= ord(word[0]) <= ord("Z"))
