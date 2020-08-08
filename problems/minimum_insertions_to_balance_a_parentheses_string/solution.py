1970ba9a9cc85242f3ea238fc78d0290fc6daae4
hwennnn <hwendev@gmail.com>
2020-08-08 21:17:02 +0000

    Sync LeetCode submission - Minimum Insertions to Balance a Parentheses String (python3)

--- problems/minimum_insertions_to_balance_a_parentheses_string/solution.py
+++ problems/minimum_insertions_to_balance_a_parentheses_string/solution.py
@@ -1,19 +1,16 @@
 class Solution:
-    def minInsertions(self, s: str) -> int:
-        res = closed = 0
-        
+    def minInsertions(self, s):
+        res = right = 0
         for c in s:
             if c == '(':
-                if closed & 1:
-                    closed -= 1
+                if right % 2:
+                    right -= 1
                     res += 1
-                    
-                closed += 2
-            else:
-                if closed == 0:
+                right += 2
+            if c == ')':
+                right -= 1
+                if right < 0:
+                    right += 2
                     res += 1
-                    closed += 2
-                    
-                closed -= 1
+        return right + res
         
-        return res + closed
