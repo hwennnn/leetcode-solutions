5e72e8568531f36a88235074b1217e8ad8d8d810
hwennnn <hwendev@gmail.com>
2020-08-09 18:59:53 +0000

    Sync LeetCode submission - Make The String Great (python3)

--- problems/make_the_string_great/solution.py
+++ problems/make_the_string_great/solution.py
@@ -1,12 +1,19 @@
 class Solution:
     def makeGood(self, s: str) -> str:
+        
+        n = len(s)
+        
         stack = []
         
-        for x in s:
-            stack.append(x)
-            while len(stack) >= 2 and stack[-1] != stack[-2] and stack[-1].upper() == stack[-2].upper():
-                stack.pop()
-                stack.pop()
-        
-        return "".join(stack)
+        for i in range(n):
             
+            if stack and abs(ord(stack[-1])-ord(s[i])) == 32:
+                stack.pop()
+            
+            else:
+                stack.append(s[i])
+        
+
+        return "".join(stack)
+                
+                
