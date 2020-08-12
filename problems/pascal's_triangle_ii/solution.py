77e19c489d1ea3fd705d5b6c5628ecff31fcb112
hwennnn <hwendev@gmail.com>
2020-08-12 15:39:33 +0000

    Sync LeetCode submission - Pascal's Triangle II (python3)

--- problems/pascal's_triangle_ii/solution.py
+++ problems/pascal's_triangle_ii/solution.py
@@ -1,15 +1,21 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        t = [[1], [1, 1]]
         
-        for index in range(2, rowIndex + 1):
-            tt = t[-1]
-            tmp = [1] + [0] * (index - 1) + [1]
-            for i in range(1, len(tmp) - 1):
-                left = tt[i - 1]
-                right = tt[-1] if i + 1 >= len(tt) else tt[i]
-                tmp[i] = left + right
+        ans = []
+        prev = None
+        
+        for i in range(1,rowIndex+2):
+            temp = []
             
-            t.append(tmp)
+            for j in range(1,i+1):
+                if j == 1 or j == i:
+                    temp.append(1)
+                else:
+                    temp.append(prev[j-1]+prev[j-2])
+                    
+            ans.append(temp)
+            prev = temp
+            
         
-        return t[rowIndex]
+        return ans[rowIndex]
+                
