ecb1ade1ccf417c883d82dc6dce69ed55b1b1883
hwennnn <hwendev@gmail.com>
2020-08-14 10:04:46 +0000

    Sync LeetCode submission - Transpose Matrix (python3)

--- problems/transpose_matrix/solution.py
+++ problems/transpose_matrix/solution.py
@@ -1,3 +1,14 @@
 class Solution:
-    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        return list(zip(*matrix))
+    def transpose(self, A: List[List[int]]) -> List[List[int]]:
+        
+        out_size = len(A[0])
+        in_size = len(A)
+        
+        ans = [[None for _ in range(in_size)] for _ in range(out_size)] 
+        
+        for i in range(in_size):
+            for j in range(out_size):
+                ans[j][i] = A[i][j]
+        
+        return ans
+        
