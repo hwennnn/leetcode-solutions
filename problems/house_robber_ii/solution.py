52ace710ec204c68ffab71a27c9ccf66b118c0bc
hwennnn <hwendev@gmail.com>
2020-08-14 17:09:39 +0000

    Sync LeetCode submission - House Robber II (python3)

--- problems/house_robber_ii/solution.py
+++ problems/house_robber_ii/solution.py
@@ -1,13 +1,38 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        
+        def rob0(nums):
+            n = len(nums)
+            ifRobbed = ifNotRobbed = 0
+            
+            for i in range(n-1):
+                rob_this = ifNotRobbed + nums[i]
+                
+                dont_rob_this = max(ifRobbed, ifNotRobbed)
+                
+                ifRobbed = rob_this
+                
+                ifNotRobbed = dont_rob_this
+                
+            return max(ifRobbed, ifNotRobbed)
+        
+        
+        def rob1(nums):
+            n = len(nums)
+            ifRobbed = ifNotRobbed = 0
+            
+            for i in range(1,n):
+                rob_this = ifNotRobbed + nums[i]
+                
+                dont_rob_this = max(ifRobbed, ifNotRobbed)
+                
+                ifRobbed = rob_this
+                
+                ifNotRobbed = dont_rob_this
+                
+            return max(ifRobbed, ifNotRobbed)
+                
+        
         if len(nums) == 1: return nums[0]
         
-        def rob_helper(start, end):
-            rob = not_rob = 0
-            
-            for i in range(start, end):
-                rob, not_rob = not_rob + nums[i], max(not_rob, rob)
-            
-            return max(rob, not_rob)
-        
-        return max(rob_helper(0, len(nums) - 1), rob_helper(1, len(nums)))
+        return max(rob0(nums), rob1(nums))
