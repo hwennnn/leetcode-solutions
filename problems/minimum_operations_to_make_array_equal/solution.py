96735f37d8cfc86ddfff0c4e614587e9f299fe9c
hwennnn <hwendev@gmail.com>
2020-08-16 02:58:13 +0000

    Sync LeetCode submission - Minimum Operations to Make Array Equal (python3)

--- problems/minimum_operations_to_make_array_equal/solution.py
+++ problems/minimum_operations_to_make_array_equal/solution.py
@@ -1,11 +1,39 @@
 class Solution:
-    def minOperations(self, n: int) -> int:
-        arr = [(2 * i) + 1 for i in range(n)]
-        t = sum(arr) // n
+    def minOperations(self, s: int) -> int:
         
-        res = 0
+        arr = [(2*i) + 1 for i in range(s)]
+        n = len(arr)
+
+        if (n <= 1):
+            return 0
+
+        if (n == 2):
+            return 1
+
+        if n%2 == 1:
+            ans = 1
+            i = 0
+            j = n-1
+            ans = 0
+            while (i != j):
+                ans += (arr[j]-arr[i])//2
+                i +=1
+                j -=1
+                
+            return ans
+        else:
+            first_median = n//2
+            second_median = first_median-1
+            ans = 1
+            i = 0
+            j = n-1
+            while (i != second_median or j != first_median):
+                ans += (arr[j]-arr[i])//2
+                i +=1
+                j -=1
+                
+            return ans
+
+
         
-        for num in arr:
-            res += abs(t - num)
         
-        return res // 2
