e98bc3953ca74e88ede48b9d7a3c773f6db81858
hwennnn <hwendev@gmail.com>
2020-08-17 05:48:18 +0000

    Sync LeetCode submission - Triangle (python3)

--- problems/triangle/solution.py
+++ problems/triangle/solution.py
@@ -1,11 +1,24 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        
         n = len(triangle)
         
-        for i in range(1, n):
-            for j in range(len(triangle[i])):
-                l = triangle[i - 1][j - 1] if j - 1 >= 0 else float('inf')
-                r = triangle[i - 1][j] if j < len(triangle[i - 1]) else float('inf')
-                triangle[i][j] += min(l, r)
+        if not triangle: return 0
+        if n == 1: return triangle[0][0]
+        
+        for i in range(1,n):
+            row = triangle[i]
+            
+            for j in range(len(row)):
+                if j == 0:
+                    row[j] += triangle[i-1][j]
+                
+                elif j == len(row)-1:
+                    row[j] += triangle[i-1][j-1]
+                
+                else:
+                    row[j] += min(triangle[i-1][j], triangle[i-1][j-1])
+                
         
         return min(triangle[-1])
+            
