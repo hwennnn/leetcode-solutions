78072899ce095f0a622e769371641e96aeec2c8b
hwennnn <hwendev@gmail.com>
2020-08-19 12:28:11 +0000

    Sync LeetCode submission - Count Number of Teams (python3)

--- problems/count_number_of_teams/solution.py
+++ problems/count_number_of_teams/solution.py
@@ -1,21 +1,39 @@
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
+        
+        # Brute force n^3
+#         n = len(rating)
+#         ans = 0
+    
+#         for i in range(n):
+#             for j in range(i+1,n):
+#                 for k in range(j+1,n):
+#                     ans += (rating[i]< rating[j] < rating[k] or rating[i] > rating[j] > rating[k])
+        
+#         return ans
+                
+        # n^2
+        n = len(rating)
         res = 0
-        for i in range(len(rating)):
-            ls = rs = lb = rb = 0
-            
+        
+        for i in range(n):
+            ls = lb = rs = rb = 0
             for j in range(i-1,-1,-1):
                 if rating[j] < rating[i]:
                     ls += 1
                 elif rating[j] > rating[i]:
                     lb += 1
             
-            for j in range(i+1,len(rating)):
+            for j in range(i+1,n):
                 if rating[j] > rating[i]:
                     rb += 1
                 elif rating[j] < rating[i]:
                     rs += 1
 
             res += ls*rb + lb*rs
+        
         return res
-            
+                
+                
+                
+        
