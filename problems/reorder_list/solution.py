52ea30067b0341d337848fa800fc05c95167ddbf
hwennnn <hwendev@gmail.com>
2020-08-20 10:05:15 +0000

    Sync LeetCode submission - Reorder List (python3)

--- problems/reorder_list/solution.py
+++ problems/reorder_list/solution.py
@@ -4,35 +4,39 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
+    def reorderList(self, head):
         """
-        Do not return anything, modify head in-place instead.
+        :type head: ListNode
+        :rtype: void Do not return anything, modify head in-place instead.
         """
-        p1, p2 = head, head.next
-        while p2 and p2.next:
-            p1 = p1.next
-            p2 = p2.next.next
-        
-        def reverseLL(curr):
-            prev = None
-
-            while curr:
-                temp = curr.next
-                curr.next = prev
-                prev = curr
-                curr = temp
+        if not head or not head.next:
+            return
+        slow = head
+        fast = head.next
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        slow = slow.next
+        if fast.next:
+            fast = fast.next
             
-            return prev
-        
-        head2 = reverseLL(p1.next)
-        p1.next = None
-        
-        while head and head2:
-            tmp1 = head.next
-            tmp2 = head2.next
-            head2.next = head.next
-            head.next = head2
-            head = tmp1
-            head2 = tmp2
-        
+        # reverse the second half of the list
+        prev = slow
+        curr = slow.next
+        prev.next = None
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+            
+        # turn the list into zigzag manner
+        trav = head
+        while fast.next:
+            tmp1 = trav.next
+            tmp2 = fast.next
+            trav.next = fast
+            fast.next = tmp1
+            trav = tmp1
+            fast = tmp2
         
