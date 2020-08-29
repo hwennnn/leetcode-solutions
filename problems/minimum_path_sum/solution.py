79cd035df2ae1faec669cf9fe62c6d876bf11af9
hwennnn <hwendev@gmail.com>
2020-08-29 11:11:40 +0000

    Sync LeetCode submission - Minimum Path Sum (python3)

--- problems/minimum_path_sum/solution.py
+++ problems/minimum_path_sum/solution.py
@@ -1,13 +1,16 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        for i in range(1, n):
-            grid[0][i] += grid[0][i-1]
-        for i in range(1, m):
+        n = len(grid)
+        m = len(grid[0])
+        
+        for i in range(1,n):
             grid[i][0] += grid[i-1][0]
         
-        for i in range(1, m):
-            for j in range(1, n):
+        for i in range(1,m):
+            grid[0][i] += grid[0][i-1]
+        
+        for i in range(1,n):
+            for j in range(1,m):
                 grid[i][j] += min(grid[i-1][j], grid[i][j-1])
-        return grid[-1][-1]
+        
+        return grid[n-1][m-1]
