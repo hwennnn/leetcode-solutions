d2552140fae7231b0856fb4190678f8fbb501144
hwennnn <hwendev@gmail.com>
2020-08-29 06:52:32 +0000

    Sync LeetCode submission - Increasing Decreasing String (python3)

--- problems/increasing_decreasing_string/solution.py
+++ problems/increasing_decreasing_string/solution.py
@@ -1,23 +1,30 @@
 class Solution:
-    def sortString(self, S: str) -> str:
-        mp = collections.defaultdict(int)
-        n = len(S)
-        for s in S:
-            mp[s] += 1
-        
-        mp = dict(sorted(mp.items()))
-        res = []
-        while len(res) != n:
-            for key in mp:
-                if mp[key]:
-                    res.append(key)
-                    mp[key] -= 1
+    def sortString(self, s: str) -> str:
+        dic = {}
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        high = float('-inf')
+        c = set(alphabet)
+        for a in alphabet:
+            dic[a] = s.count(a)
+            high = max(high, dic[a])
 
-            for key in dict(sorted(mp.items(), reverse = True)):
-                if mp[key]:
-                    res.append(key)
-                    mp[key] -= 1
+        ans = []
+    
+        while high:
+            for d in dic:
+                if dic.get(d,0) > 0:
+                    ans.append(d)
+                    dic[d] = dic.get(d,0) - 1
+                    if dic[d] == 0:
+                        c.remove(d)
+            for d in reversed(dic):
+                if dic.get(d,0) > 0:
+                    ans.append(d)
+                    dic[d] = dic.get(d,0) - 1
+                    if dic[d] == 0:
+                        c.remove(d)
 
-        
-        return "".join(res)
-            
+            high -= 1
+                
+                
+        return "".join(ans)
