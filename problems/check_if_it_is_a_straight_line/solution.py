9b6ab19cf3ecf519b4ef5e3120efce72cbbb3674
hwennnn <hwendev@gmail.com>
2020-08-30 01:58:43 +0000

    Sync LeetCode submission - Check If It Is a Straight Line (python3)

--- problems/check_if_it_is_a_straight_line/solution.py
+++ problems/check_if_it_is_a_straight_line/solution.py
@@ -1,5 +1,15 @@
 class Solution:
-    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        (x0,y0), (x1,y1) = coordinates[:2]
+    def checkStraightLine(self, arr: List[List[int]]) -> bool:
+        n = len(arr)
         
-        return all((x1-x0) * (y-y1) == (x-x1) * (y1-y0) for x,y in coordinates)
+        x0 = arr[0][0]
+        x1 = arr[1][0]
+        y0 = arr[0][1]
+        y1 = arr[1][1]
+        
+        for a in arr:
+            x, y = a[0], a[1]
+            if (x1-x0) * (y-y1) != (y1-y0) * (x-x1):
+                return False
+        
+        return True
