a54cf2501548aa40adfdee68d99698b12da7e5b6
hwennnn <hwendev@gmail.com>
2020-08-31 12:54:05 +0000

    Sync LeetCode submission - Subdomain Visit Count (python3)

--- problems/subdomain_visit_count/solution.py
+++ problems/subdomain_visit_count/solution.py
@@ -1,16 +1,10 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        mp = collections.defaultdict(int)
-        
-        for domain in cpdomains:
-            count, url = domain.split()
-            s = url.split('.')
-            for i in range(len(s)):
-                mp[".".join(s[i:])] += int(count)
-        
-        res = []
-        
-        for k, v in mp.items():
-            res.append(f'{v} {k}')
-        
-        return res
+        c = collections.Counter()
+        for cd in cpdomains:
+            n, d = cd.split()
+            c[d] += int(n)
+            for i in range(len(d)):
+                if d[i] == '.': c[d[i + 1:]] += int(n)
+
+        return ["%d %s" % (c[k], k) for k in c]
