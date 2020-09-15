b107655bdbb527909e167dd3faad3f1d37b162aa
hwennnn <hwendev@gmail.com>
2020-09-15 12:04:32 +0000

    Sync LeetCode submission - First Bad Version (python3)

--- problems/first_bad_version/solution.py
+++ problems/first_bad_version/solution.py
@@ -5,17 +5,15 @@
 
 class Solution:
     def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        left, right = 0, n
+        left = 1
+        right = n
         
         while left < right:
-            mid = left + (right - left) // 2
-            if isBadVersion(mid):
-                right = mid
+            mid = left + (right-left)//2
+            
+            if isBadVersion(mid): right = mid
             else:
                 left = mid + 1
         
         return left
+        
