a010a78afc8364e9b7e2883483288af1ecaf3bdf
hwennnn <hwendev@gmail.com>
2020-10-01 13:16:44 +0000

    Sync LeetCode submission - Number of Recent Calls (python3)

--- problems/number_of_recent_calls/solution.py
+++ problems/number_of_recent_calls/solution.py
@@ -1,19 +1,10 @@
 class RecentCounter:
 
     def __init__(self):
-        self.s = []
-        self.index = 0
+        self.p = collections.deque()        
 
-    def ping(self, t: int) -> int:
-        self.s.append(t)
-        
-        while self.index < len(self.s) and not (t - 3000 <= self.s[self.index] <= t):
-            self.index += 1
-        
-        return len(self.s) - self.index
-        
-
-
-# Your RecentCounter object will be instantiated and called as such:
-# obj = RecentCounter()
-# param_1 = obj.ping(t)
+    def ping(self, t):
+        self.p.append(t)
+        while self.p[0] < t - 3000:
+            self.p.popleft()
+        return len(self.p)
