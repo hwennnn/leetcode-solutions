7ff421d893f5dce125d01d1cd46ed78d36985044
hwennnn <hwendev@gmail.com>
2020-10-03 03:35:25 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (cpp)

--- problems/partition_equal_subset_sum/solution.cpp
+++ problems/partition_equal_subset_sum/solution.cpp
@@ -4,9 +4,9 @@ public:
         bitset<10001> bits(1);
         int total = 0;
         
-        for (auto n:nums)
+        for (auto n: nums)
             bits |= bits << n, total += n;
-        
-        return !(total&1) && bits[total / 2];
+
+        return !(total&1) && bits[total>>1];
     }
 };
