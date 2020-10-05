53f67f9204ebb365c899a5a1cef5d7786f03f982
hwennnn <hwendev@gmail.com>
2020-10-05 12:01:54 +0000

    Sync LeetCode submission - Complement of Base 10 Integer (python3)

--- problems/complement_of_base_10_integer/solution.py
+++ problems/complement_of_base_10_integer/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def bitwiseComplement(self, n: int) -> int:
-        if n == 0: return 1
+        x = 1
+        while n > x:
+            x = x*2 + 1
         
-        full = (1 << n.bit_length()) - 1
-        
-        return full ^ n
+        return x - n
