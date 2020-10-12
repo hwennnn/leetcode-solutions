8a67817cc8f33fda4a04a3471606c487fd3aee73
hwennnn <hwendev@gmail.com>
2020-10-12 04:53:21 +0000

    Sync LeetCode submission - Palindrome Number (python3)

--- problems/palindrome_number/solution.py
+++ problems/palindrome_number/solution.py
@@ -1,3 +1,10 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
+        x = str(x)
+        i, j = 0, len(x)-1
+        
+        while i < j and x[i] == x[j]:
+            i += 1
+            j -= 1
+        
+        return i >= j
