6060926bb55788c53016ec0d8937de44a63c390c
hwennnn <hwendev@gmail.com>
2020-10-12 11:20:17 +0000

    Sync LeetCode submission - Maximum Number of Non-Overlapping Subarrays With Sum Equals Target (python3)

--- problems/maximum_number_of_non-overlapping_subarrays_with_sum_equals_target/solution.py
+++ problems/maximum_number_of_non-overlapping_subarrays_with_sum_equals_target/solution.py
@@ -1,22 +1,16 @@
 class Solution:
     def maxNonOverlapping(self, nums: List[int], target: int) -> int:
         
-        n = len(nums)
-        total = count = 0
-        last = -1
         d = {0:-1}
+        last = -1
+        s = res = 0
         
-        for i in range(n):
-            
-            total += nums[i]
-            
-            if total-target in d and d[total-target] >= last:
-                count += 1
+        for i,num in enumerate(nums):
+            s += num
+            if s - target in d and d[s-target] >= last:
                 last = i
-                
-            d[total] = i
+                res += 1
+            
+            d[s] = i
         
-        return count
-                
-            
-            
+        return res
