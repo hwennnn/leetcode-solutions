41c77b52a4fa31022101090683f75985df6235fe
hwennnn <hwendev@gmail.com>
2020-10-13 04:11:11 +0000

    Sync LeetCode submission - Build an Array With Stack Operations (python3)

--- problems/build_an_array_with_stack_operations/solution.py
+++ problems/build_an_array_with_stack_operations/solution.py
@@ -1,23 +1,15 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
         arr = [i for i in range(1,n+1)]
-        n = len(arr)
         res = []
-        temp = []
-        i = j = 0
-        
-        while temp != target:
-            temp.append(arr[i])
-            res.append("Push")
+        j = 0
+        for i in range(n):
+            if j >= len(target):
+                break
             if arr[i] != target[j]:
-                temp.pop()
+                res.append("Push")
                 res.append("Pop")
             else:
+                res.append("Push")
                 j += 1
-            i += 1
-            
         return res
-            
-            
-            
-        
