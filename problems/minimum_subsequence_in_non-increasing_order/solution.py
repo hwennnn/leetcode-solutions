43f93ddc6f16d9da569c9e1aab70264b00a2cf3c
hwennnn <hwendev@gmail.com>
2020-10-14 07:37:36 +0000

    Sync LeetCode submission - Minimum Subsequence in Non-Increasing Order (python3)

--- problems/minimum_subsequence_in_non-increasing_order/solution.py
+++ problems/minimum_subsequence_in_non-increasing_order/solution.py
@@ -2,11 +2,8 @@ class Solution:
     def minSubsequence(self, nums: List[int]) -> List[int]:
         nums.sort()
         res = []
-        numSum = sum(nums) 
-        resSum = 0 
-        while resSum <= numSum:
-            v = nums.pop()
-            res.append(v)
-            resSum += v
-            numSum -= v
-        return res   
+        while sum(res) <= sum(nums):
+            res.append(nums.pop())
+        
+        return res
+    
