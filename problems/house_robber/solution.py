b374976fc99517738c7a6bc4b25bed6222371d7d
hwennnn <hwendev@gmail.com>
2020-10-14 07:20:03 +0000

    Sync LeetCode submission - House Robber (python3)

--- problems/house_robber/solution.py
+++ problems/house_robber/solution.py
@@ -1,12 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        rob, not_rob = 0, 0
         
-        if_rob = 0
-        if_not_rob = 0
+        for i in range(len(nums)):
+            rob, not_rob = not_rob + nums[i], max(rob, not_rob)
         
-        for num in nums:
-            temp = if_rob
-            if_rob = num + if_not_rob
-            if_not_rob = max(temp, if_not_rob)
-        
-        return max(if_rob, if_not_rob)
+        return max(rob, not_rob)
