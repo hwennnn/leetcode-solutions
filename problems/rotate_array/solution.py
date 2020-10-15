f34aed57264ef6868a401bdd27ab533fb7b602b9
hwennnn <hwendev@gmail.com>
2020-10-15 07:42:59 +0000

    Sync LeetCode submission - Rotate Array (python3)

--- problems/rotate_array/solution.py
+++ problems/rotate_array/solution.py
@@ -3,23 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        def reverse(nums, start, end):
-            while start < end:
-                temp = nums[start]
-                nums[start] = nums[end]
-                nums[end] = temp
-                start += 1
-                end -= 1
-                
         n = len(nums)
         k %= n
-        reverse(nums, 0, n-1)
-        reverse(nums, 0, k-1)
-        reverse(nums, k, n-1)
-        
-        
-    
-       
-        
-        
+        nums[:] = nums[n-k:] + nums[:n-k]
