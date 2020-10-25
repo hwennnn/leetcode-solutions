09dd131f5228d0992b1d938cb0855184300f9cbc
hwennnn <hwendev@gmail.com>
2020-10-25 06:09:20 +0000

    Sync LeetCode submission - Slowest Key (python3)

--- problems/slowest_key/solution.py
+++ problems/slowest_key/solution.py
@@ -1,18 +1,17 @@
 class Solution:
-    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
-        mp = collections.defaultdict(int)
-        curr = 0
-        
-        for time, key in zip(releaseTimes, keysPressed):
-            t = time - curr
-            mp[key] = max(mp[key], t)
-            curr = time
-        
-        mp = list(mp.items())
-        res = mp[0]
-        
-        for char, t in mp:
-            if t > res[1] or (t == res[1] and char > res[0]):
-                res = (char, t)
-        
-        return res[0]
+    def slowestKey(self, rt: List[int], kp: str) -> str:
+
+        m = float('-inf')
+        res = ""
+        prev = 0
+        for i,t in enumerate(rt):
+            v = t - prev
+
+            if v >= m:
+                m = v
+                res = kp[i]
+            
+            prev = t
+
+        return res
+            
