52c266b852357b33130f5ebddd97ec6492178d7f
hwennnn <hwendev@gmail.com>
2020-10-29 09:38:33 +0000

    Sync LeetCode submission - Maximize Distance to Closest Person (python3)

--- problems/maximize_distance_to_closest_person/solution.py
+++ problems/maximize_distance_to_closest_person/solution.py
@@ -2,9 +2,9 @@ class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
         res, last, n = 0, -1, len(seats)
         
-        for i in range(n):
-            if seats[i]:
-                res = max(res, i if last < 0 else (i-last)//2)
+        for i,s in enumerate(seats):
+            if s:
+                res = max(res, i if last == -1 else (i-last) // 2)
                 last = i
         
         return max(res, n - last - 1)
