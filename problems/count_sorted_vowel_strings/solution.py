24156138b8c0b343afcc13ebc83e5aabf2a6183f
hwennnn <hwendev@gmail.com>
2020-11-01 04:22:11 +0000

    Sync LeetCode submission - Count Sorted Vowel Strings (python3)

--- problems/count_sorted_vowel_strings/solution.py
+++ problems/count_sorted_vowel_strings/solution.py
@@ -1,8 +1,10 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
+
         a = e = i = o = u = 1
         
-        for _ in range(n - 1):
-            a, e, i, o, u = a + e + i + o + u, e + i + o + u, i + o + u, o + u, u
+        for _ in range(n-1):
+            a, e ,i, o, u = a+e+i+o+u, e+i+o+u , i+o+u, o+u, u
         
-        return a + e + i + o + u
+        return a+e+i+o+u
+                
