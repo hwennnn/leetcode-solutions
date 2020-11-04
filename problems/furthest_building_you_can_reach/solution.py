be519cca093de118d9c0b189ce204a0903abb09b
hwennnn <hwendev@gmail.com>
2020-11-04 10:24:59 +0000

    Sync LeetCode submission - Furthest Building You Can Reach (python3)

--- problems/furthest_building_you_can_reach/solution.py
+++ problems/furthest_building_you_can_reach/solution.py
@@ -1,21 +1,20 @@
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        
         n = len(heights)
         heap = []
         
-        for i in range(n - 1):
-            delta = heights[i + 1] - heights[i]
+        for i in range(n-1):
+            d = heights[i+1] - heights[i]
             
-            if delta > 0:
-                heappush(heap, delta)
-                if ladders > 0:
-                    ladders -= 1
-                else:
-                    use = heappop(heap)
-                    if bricks >= use:
-                        bricks -= use
-                    else:
-                        return i
-                    
+            if d > 0:
+                heapq.heappush(heap,d)
+            
+            if len(heap) > ladders:
+                bricks -= heapq.heappop(heap)
+            
+            if bricks < 0:
+                return i
         
         return n - 1
+            
