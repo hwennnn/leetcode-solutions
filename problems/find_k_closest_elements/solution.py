305e8657026582c746ee2910c32210415b7f645e
hwennnn <hwendev@gmail.com>
2020-11-05 03:41:58 +0000

    Sync LeetCode submission - Find K Closest Elements (python3)

--- problems/find_k_closest_elements/solution.py
+++ problems/find_k_closest_elements/solution.py
@@ -1,20 +1,14 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        pq = []
+        n = len(arr)
 
-        for i, num in enumerate(arr):
-            d = abs(num - x)
-            if len(pq) == k:
-                heappushpop(pq, (-d, -i))
-            else:
-                heappush(pq, (-d, -i))
+        i, j = 0, n-1
         
-        res = []
-
-        while pq:
-            d, index = heappop(pq)
-            index = -index
-            res.append(arr[index])
-
-        return sorted(res)
-            
+        while j - i >= k:
+            if abs(arr[i]- x) <= abs(arr[j] - x):
+                j -= 1
+            else:
+                i += 1
+                
+        
+        return arr[i:j+1]
