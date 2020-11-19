bc21a5ed56f3059881788b01f5d80cadde4b0254
hwennnn <hwendev@gmail.com>
2020-11-19 12:28:49 +0000

    Sync LeetCode submission - Decode String (python3)

--- problems/decode_string/solution.py
+++ problems/decode_string/solution.py
@@ -3,22 +3,21 @@ class Solution:
         n = len(s)
         stack = []
         
-        for x in s:
-            if x == ']':
-                chars = ""
-                
-                while stack and stack[-1] != '[':
-                    chars = stack.pop() + chars
+        for c in s:
+            if c == "]":
+                t = ""
+                while stack[-1] != "[":
+                    t = stack.pop() + t
                 
                 stack.pop()
                 
                 times = ""
-                
                 while stack and stack[-1].isnumeric():
                     times = stack.pop() + times
                 
-                stack.append(int(times) * chars)  
+                stack.append(int(times)*t)
+            
             else:
-                stack.append(x)
+                stack.append(c)
         
         return "".join(stack)
