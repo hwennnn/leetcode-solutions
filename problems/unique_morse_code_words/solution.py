7be75ca2a81248ce8e9cf74a4618773511f1131c
hwennnn <hwendev@gmail.com>
2020-11-22 15:54:27 +0000

    Sync LeetCode submission - Unique Morse Code Words (python3)

--- problems/unique_morse_code_words/solution.py
+++ problems/unique_morse_code_words/solution.py
@@ -1,30 +1,14 @@
-class Solution(object):
-    def uniqueMorseRepresentations(self, words):
-        """
-        :type words: List[str]
-        :rtype: int
-        """
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
         
+        s = set()
         
-        string = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-        table = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-        lst = []
-
-        for i in words:
-            i_str = ""
-            for x in i:
-                str_index = string.index(x)
-                i_str+=table[str_index]  
-            lst.append(i_str)
-
-        another_lst = []
-        
-        for i in lst:
-            if i not in another_lst:
-                another_lst.append(i)
-        
-        return len(another_lst)
-    
-        
-        
+        for word in words:
+            w = []
+            for c in word:
+                w.append(morse[ord(c) - ord('a')])
             
+            s.add("".join(w))
+        
+        return len(s)
