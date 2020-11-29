b90829243705e7f69d48401fe2d9ca2c2217a38b
hwennnn <hwendev@gmail.com>
2020-11-29 04:24:22 +0000

    Sync LeetCode submission - Find the Most Competitive Subsequence (python3)

--- problems/find_the_most_competitive_subsequence/solution.py
+++ problems/find_the_most_competitive_subsequence/solution.py
@@ -1,14 +1,12 @@
 class Solution:
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
-        res = []
-        n = len(nums)
+        stack = []
         
-        for i,x in enumerate(nums):
-            while res and len(res) + (n-i) > k and x < res[-1]:
-                res.pop()
-            
-            if len(res) < k:
-                res.append(x)
+        for i, x in enumerate(nums):
+            while stack and x < stack[-1] and len(stack) + len(nums) - i > k:
+                stack.pop()
+                
+            if len(stack) < k:
+                stack.append(x)
         
-        return res
-        
+        return stack
