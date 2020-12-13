8f8eab7602c19581054f03181f95df0e0093748b
hwennnn <hwendev@gmail.com>
2020-12-13 15:25:47 +0000

    Sync LeetCode submission - N-th Tribonacci Number (python3)

--- problems/n-th_tribonacci_number/solution.py
+++ problems/n-th_tribonacci_number/solution.py
@@ -1,18 +1,16 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
+        seen = [None] * (38)
+        seen[0] = 0
+        seen[1] = 1
+        seen[2] = 1
         
-        temp = {0:0, 1:1, 2:1}
+        def recur(n):
+            if seen[n] != None: return seen[n]
+            
+            result = recur(n-1) + recur(n-2) + recur(n-3)
+            seen[n] = result
+            
+            return result
         
-        def tri(n):
-            if n in temp:
-                return temp.get(n)
-            
-            else:
-                value = tri(n-1) + tri(n-2) + tri(n-3)
-                temp[n] = value
-                return value
-            
-        return tri(n)
-            
-            
-        
+        return recur(n)
