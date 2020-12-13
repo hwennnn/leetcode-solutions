04ea4a77e8b2668e376cf7760cb0ba4cee3b8688
hwennnn <hwendev@gmail.com>
2020-12-13 03:04:00 +0000

    Sync LeetCode submission - Partitioning Into Minimum Number Of Deci-Binary Numbers (python3)

--- problems/partitioning_into_minimum_number_of_deci-binary_numbers/solution.py
+++ problems/partitioning_into_minimum_number_of_deci-binary_numbers/solution.py
@@ -1,3 +1,8 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
-        return max(int(x) for x in n)
+        res = float('-inf')
+        
+        for c in n:
+            res = max(int(c), res)    
+        
+        return res
