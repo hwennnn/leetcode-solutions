e90b1c40305ed262f2eaabbca7bc6ee459126964
hwennnn <hwendev@gmail.com>
2020-12-24 05:28:10 +0000

    Sync LeetCode submission - Maximum Erasure Value (python3)

--- problems/maximum_erasure_value/solution.py
+++ problems/maximum_erasure_value/solution.py
@@ -1,16 +1,20 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        s = set()
-        i = total = res = 0
         
-        for j, x in enumerate(nums):
-            while x in s:
-                total -= nums[i]
-                s.remove(nums[i])
+        c = collections.Counter()
+        curr = res = i = 0
+        
+        for j,x in enumerate(nums):
+            curr += x
+            c[x] += 1
+            
+            while c[x] >= 2:
+                target = nums[i]
+                c[target] -= 1
+                curr -= target
                 i += 1
-                
-            total += x
-            s.add(x)
-            res = max(res, total)
+            
+            res = max(res, curr)
         
         return res
+            
