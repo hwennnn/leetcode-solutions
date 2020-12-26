d5395d1192834d9383585ac7d073204499fa1eab
hwennnn <hwendev@gmail.com>
2020-12-26 06:51:30 +0000

    Sync LeetCode submission - Design Parking System (python3)

--- problems/design_parking_system/solution.py
+++ problems/design_parking_system/solution.py
@@ -1,13 +1,17 @@
 class ParkingSystem:
+
     def __init__(self, big: int, medium: int, small: int):
         self.lst = [big,medium,small]
 
     def addCar(self, carType: int) -> bool:
-        if self.lst[carType-1] > 0:
-            self.lst[carType-1] -= 1
+        t = carType - 1
+        if self.lst[t] > 0:
+            self.lst[t] -= 1
             return True
+        
         return False
-    
+
+
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
 # param_1 = obj.addCar(carType)
