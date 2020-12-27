a08168fae082a718f94ba431d07131b8c33f2984
hwennnn <hwendev@gmail.com>
2020-12-27 11:02:10 +0000

    Sync LeetCode submission - Minimum Deletions to Make Character Frequencies Unique (python3)

--- problems/minimum_deletions_to_make_character_frequencies_unique/solution.py
+++ problems/minimum_deletions_to_make_character_frequencies_unique/solution.py
@@ -1,18 +1,15 @@
 class Solution:
     def minDeletions(self, S: str) -> int:
-        c = Counter(S)
-        s = set()
+        c = collections.Counter(S)
         res = 0
+        s = set()
         
-        for v in c.values():
-            t = v
+        for ch in c.values():
             
-            while t in s:
+            while ch > 0 and ch in s:
                 res += 1
-                t -= 1
+                ch -= 1
             
-            if t != 0:
-                s.add(t)
-            
-        return res
+            if ch > 0: s.add(ch)
         
+        return res
