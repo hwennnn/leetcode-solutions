4b91b7a6b34ebd5239d4e1d3b4f9a1253b2254fd
hwennnn <hwendev@gmail.com>
2020-12-27 14:52:22 +0000

    Sync LeetCode submission - Deepest Leaves Sum (python3)

--- problems/deepest_leaves_sum/solution.py
+++ problems/deepest_leaves_sum/solution.py
@@ -6,25 +6,20 @@
 #         self.right = right
 class Solution:
     def deepestLeavesSum(self, root: TreeNode) -> int:
-        
-        def depth(node):
-            if not node: return 0
-            
-            return 1 + max(depth(node.left), depth(node.right))
-        
-        d = depth(root)
+        deq = collections.deque([root])
         res = 0
-        q = collections.deque([(root, 1)])
         
-        while q:
-            node, depth = q.popleft()
+        while deq:
+            l = len(deq)
+            res = 0
+            
+            while l:
+                node = deq.popleft()
+                res += node.val
+                
+                for n in (node.left, node.right):
+                    if n: deq.append(n)
+                l -= 1
+            
 
-            if depth == d: res += node.val
-                
-            for leaf in (node.left, node.right):
-                if leaf:
-                    q.append((leaf, depth + 1))
-        
         return res
-                
-        
