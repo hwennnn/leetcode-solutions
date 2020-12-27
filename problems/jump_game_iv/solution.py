41bdb2d6761672b6a05819b0849ba344cc2aa892
hwennnn <hwendev@gmail.com>
2020-12-27 10:45:54 +0000

    Sync LeetCode submission - Jump Game IV (python3)

--- problems/jump_game_iv/solution.py
+++ problems/jump_game_iv/solution.py
@@ -1,21 +1,23 @@
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
-        nei = collections.defaultdict(list)
-        _ = [nei[v].append(i) for i,v in enumerate(arr)]
+        graph = collections.defaultdict(list)
+        _ = [graph[x].append(i) for i,x in enumerate(arr)]
         
-        pos_seen, num_seen = set(), set()
+        n = len(arr)
         deq = collections.deque([(0,0)])
+        pos_seen, num_seen = {0}, set()
         
         while deq:
             pos, step = deq.popleft()
-            if pos == len(arr) - 1: return step
+            
+            if pos == n - 1: return step
+            
             num = arr[pos]
-            pos_seen.add(pos)
             
-            
-            for p in [pos-1, pos+1] + nei[num] * (num not in num_seen):
-                if not 0 <= p <= len(arr)-1 or p in pos_seen: continue
+            for p in [pos-1, pos+1] + graph[num] * (num not in num_seen):
+                if not (0 <= p < n) or p in pos_seen: continue
+                
                 deq.append((p, step+1))
+                pos_seen.add(p)
             
             num_seen.add(num)
-        
