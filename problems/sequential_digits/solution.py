3983e9ce56fe02b5a04a28d5931db7ff9ccef020
hwennnn <hwendev@gmail.com>
2020-12-30 11:32:21 +0000

    Sync LeetCode submission - Sequential Digits (python3)

--- problems/sequential_digits/solution.py
+++ problems/sequential_digits/solution.py
@@ -1,12 +1,17 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        l = [12,23,34,45,56,67,78,89,123,234,345,456,567,678,789,1234,2345,3456,4567,5678,6789,
-            12345,23456,34567,45678,56789,123456,234567,345678,456789,1234567,2345678,3456789,
-            12345678,23456789,123456789]
         
-        ans = []
-        for li in l:
-            if li >= low and li <= high: 
-                ans.append(li)
-        return ans
+        q = collections.deque(range(1,10))
+        res = []
         
+        while q:
+            v = q.popleft()
+            if low <= v <= high:
+                res.append(v)
+            
+            last = v%10
+            
+            if last < 9:
+                q.append(v*10 + last + 1)
+        
+        return res
