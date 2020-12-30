d3ca7f12ff479c2d50daefffe081ccf450541052
hwennnn <hwendev@gmail.com>
2020-12-30 13:57:52 +0000

    Sync LeetCode submission - Find the Smallest Divisor Given a Threshold (python3)

--- problems/find_the_smallest_divisor_given_a_threshold/solution.py
+++ problems/find_the_smallest_divisor_given_a_threshold/solution.py
@@ -1,18 +1,13 @@
 class Solution:
     def smallestDivisor(self, nums: List[int], threshold: int) -> int:
         
-        def good(divisor):
-            total = 0
-            for num in nums:
-                total += (num-1)//divisor + 1
-            
-            return total <= threshold
-            
-        left = 1
-        right = 10 ** 8
-        
+        def good(d):
+            return sum(ceil(num/mid) for num in nums) <= threshold
+    
+        left, right = 1, 10**6
+    
         while left < right:
-            mid = (left + right) // 2
+            mid = (left+right)//2
             
             if good(mid):
                 right = mid
