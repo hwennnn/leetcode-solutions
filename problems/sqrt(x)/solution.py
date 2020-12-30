c2d4970680a0f45f2a8bc47a7a99253d2d223199
hwennnn <hwendev@gmail.com>
2020-12-30 14:21:08 +0000

    Sync LeetCode submission - Sqrt(x) (python3)

--- problems/sqrt(x)/solution.py
+++ problems/sqrt(x)/solution.py
@@ -1,6 +1,18 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        r = x
-        while r*r > x:
-            r = (r + x/r) // 2
-        return int(r)
+        if x <= 1: return x
+        
+        def good(v):
+            return v*v <= x
+        
+        left, right = 0, x
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            
+            if good(mid):
+                left = mid + 1
+            else:
+                right = mid
+        
+        return left - 1
