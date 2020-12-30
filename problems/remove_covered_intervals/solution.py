f11b9f82056b1e56ce616ab7322afb630af4db3b
hwennnn <hwendev@gmail.com>
2020-12-30 13:20:15 +0000

    Sync LeetCode submission - Remove Covered Intervals (python3)

--- problems/remove_covered_intervals/solution.py
+++ problems/remove_covered_intervals/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda a: (a[0], -a[1]))
         res = right = 0
         
-        for x,y in intervals:
-            res += y > right
-            right = max(right, y)
-
+        intervals.sort(key = lambda x:(x[0],-x[1]))
+        
+        for start,end in intervals:
+            res += end > right
+            right = max(right, end)
+        
         return res
