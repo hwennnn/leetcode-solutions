64a2b4277310b3ff10eaca76ac6cc2283ce55fb6
hwennnn <hwendev@gmail.com>
2021-01-04 06:41:27 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -1,22 +1,21 @@
 class Solution:
-    def numIslands(self, grid):
-        if not grid:
-            return 0
-
-        count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.dfs(grid, i, j)
-                    count += 1
-        return count
-
-    def dfs(self, grid, i, j):
-        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
-            return
-        grid[i][j] = '#'
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i-1, j)
-        self.dfs(grid, i, j+1)
-        self.dfs(grid, i, j-1)
+    def numIslands(self, grid: List[List[str]]) -> int:
         
+        def dfs(x, y):
+            if not (0 <= x < rows and 0 <= y < cols) or grid[x][y] == "0": return
+            
+            grid[x][y] = "0"
+            for dx,dy in [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]:
+                if 0 <= dx < rows and 0 <= dy < cols:
+                    dfs(dx, dy)
+                
+        rows, cols = len(grid), len(grid[0])
+        res = 0
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1":
+                    dfs(i, j)
+                    res += 1
+        
+        return res
