d5bf61bce41b571e78578294f6b9aa93b258b236
hwennnn <hwendev@gmail.com>
2021-01-08 12:51:59 +0000

    Sync LeetCode submission - Split a String in Balanced Strings (python3)

--- problems/split_a_string_in_balanced_strings/solution.py
+++ problems/split_a_string_in_balanced_strings/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
+        r = l = res = 0
         
-        r_count = l_count = total = 0
-        for i in s:
-            l_count += (i=="L")
-            r_count += (i=="R")
-            total += (l_count==r_count)
+        for c in s:
+            if c == "R": r += 1
+            else: l += 1
             
-        return total
+            res += l == r
+        
+        return res
