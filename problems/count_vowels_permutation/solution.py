e7092e41d538bd3048f9acdc4eea90366483211c
hwennnn <hwendev@gmail.com>
2021-01-08 14:08:15 +0000

    Sync LeetCode submission - Count Vowels Permutation (python3)

--- problems/count_vowels_permutation/solution.py
+++ problems/count_vowels_permutation/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
+        M = 10 ** 9 + 7
         a = e = i = o = u = 1
         
-        for _ in range(n - 1):
-            a, e, i, o, u = e, a + i, a + e + o + u, i + u, a
+        for _ in range(2, n+1):
+            a,e,i,o,u = e, a+i, a+e+o+u, i+u, a
         
-        return (a + e + i + o + u) % (10 ** 9 + 7)
+        return (a+e+i+o+u) % M
