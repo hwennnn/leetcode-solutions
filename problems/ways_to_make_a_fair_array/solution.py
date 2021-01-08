748f04ccaa908d016334411e4147248ee9a48cbd
hwennnn <hwendev@gmail.com>
2021-01-08 09:15:15 +0000

    Sync LeetCode submission - Ways to Make a Fair Array (python3)

--- problems/ways_to_make_a_fair_array/solution.py
+++ problems/ways_to_make_a_fair_array/solution.py
@@ -1,29 +1,18 @@
 class Solution:
     def waysToMakeFair(self, nums: List[int]) -> int:
-        odd = even = 0
-        res = 0
-        for i, num in enumerate(nums):
-            if i % 2 == 0:
-                even += num
-            else:
-                odd += num
+        ro = re = 0
+        for i,x in enumerate(nums):
+            if i & 1: ro += x
+            else: re += x
         
-        co = ce = 0
-        for i, num in enumerate(nums):
-            e,o = even, odd
-            if i % 2 == 0:
-                ce += num
-                e -= num
-            else:
-                co += num
-                o -= num
-                
-            e -= (even - ce)
-            e += (odd - co)
-            o -= (odd - co)
-            o += (even - ce)
-                
-            if (o == e):
-                res += 1
+        lo = le = res = 0
+        for i,x in enumerate(nums):
+            if i & 1: ro -= x
+            else: re -= x
+            
+            res += (lo + re) == (le + ro)
+            
+            if i & 1: lo += x
+            else: le += x
         
         return res
