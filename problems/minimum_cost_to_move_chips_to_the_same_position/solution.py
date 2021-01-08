baf37e83f3411b9938ce07fbb35451e93d7a4e7e
hwennnn <hwendev@gmail.com>
2021-01-08 13:15:14 +0000

    Sync LeetCode submission - Minimum Cost to Move Chips to The Same Position (python3)

--- problems/minimum_cost_to_move_chips_to_the_same_position/solution.py
+++ problems/minimum_cost_to_move_chips_to_the_same_position/solution.py
@@ -3,9 +3,7 @@ class Solution:
         odd = even = 0
         
         for p in position:
-            if p & 1:
-                odd += 1
-            else:
-                even += 1
+            if p % 2 == 0: even += 1
+            else: odd += 1
         
-        return min(odd, even)
+        return min(odd,even)
