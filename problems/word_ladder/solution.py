25c8a2c6c1d984ae40c7fb3a894b18bba89e2405
hwennnn <hwendev@gmail.com>
2021-01-09 08:56:21 +0000

    Sync LeetCode submission - Word Ladder (python3)

--- problems/word_ladder/solution.py
+++ problems/word_ladder/solution.py
@@ -1,22 +1,19 @@
 class Solution:
-    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        n = len(beginWord)
-        wordList = set(wordList)
-        visited = set([beginWord])
-        if endWord not in wordList: return 0
+    def ladderLength(self, begin: str, end: str, wordList: List[str]) -> int:
+        s = set(wordList)
+        deq = collections.deque([(begin, 1)])
+        visited = set()
         
-        queue = deque([(beginWord, 1)])
-        
-        while queue:
-            word, steps = queue.popleft()
+        while deq:
+            word, count = deq.popleft()
+            if word == end: return count
             
-            if word == endWord: return steps
-            
-            for i in range(n):
-                for c in string.ascii_lowercase:
-                    new = word[:i] + c + word[i + 1:]
-                    if new in wordList and new not in visited:
-                        visited.add(new)
-                        queue.append((new, steps + 1))
+            for i in range(len(word)):
+                for x in string.ascii_lowercase:
+                    w = word[:i] + x + word[i+1:] 
+
+                    if w in s and w not in visited:
+                        visited.add(w)
+                        deq.append((w, count+1))
         
         return 0
