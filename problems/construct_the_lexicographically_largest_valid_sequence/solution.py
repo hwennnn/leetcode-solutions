9606d59996235e0f60c9062e04112a6f455899f7
hwennnn <hwendev@gmail.com>
2021-01-10 11:52:01 +0000

    Sync LeetCode submission - Construct the Lexicographically Largest Valid Sequence (python3)

--- problems/construct_the_lexicographically_largest_valid_sequence/solution.py
+++ problems/construct_the_lexicographically_largest_valid_sequence/solution.py
@@ -1,36 +1,41 @@
 class Solution:
     def constructDistancedSequence(self, n: int) -> List[int]:
-        res = [0] * (n * 2 - 1)
-        used = [0] * (n + 1)
+        res = [0] * (n*2 - 1)
+        used = [0] * (n+1)
         
         def backtrack(i):
-            if i == len(res): return True
+            if i == len(res): 
+                return True
             
-            if res[i]: return backtrack(i + 1)
+            if res[i]:
+                return backtrack(i+1)
             
             for k in range(n, 1, -1):
                 if used[k]: continue
                 
-                if i + k < len(res) and res[i] == res[i + k] == 0:
-                    res[i] = res[i + k] = k
+                if i + k < len(res) and not res[i+k]:
+                    res[i] = res[i+k] = k
                     used[k] = 1
                     
-                    if backtrack(i + 1): return True
+                    if backtrack(i+1): return True
                     
-                    res[i] = res[i + k] = 0
+                    res[i] = res[i+k] = 0
                     used[k] = 0
             
             if not used[1]:
                 res[i] = 1
                 used[1] = 1
                 
-                if backtrack(i + 1): return True
+                if backtrack(i+1): return True
                 
                 res[i] = 0
                 used[1] = 0
             
+            
             return False
+                
         
         backtrack(0)
         
         return res
+        
