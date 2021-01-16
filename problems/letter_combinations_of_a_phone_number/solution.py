4bf38b7cedb301d251e3fda9db78423d4468d572
hwennnn <hwendev@gmail.com>
2021-01-16 05:59:25 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (python3)

--- problems/letter_combinations_of_a_phone_number/solution.py
+++ problems/letter_combinations_of_a_phone_number/solution.py
@@ -1,19 +1,20 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits: return []
+        nums = ["0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
         
-        mp = {2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
-        n = len(digits)
-        res = []
+        deq = collections.deque([""])   
         
-        def go(index, curr):
-            if index == n:
-                res.append(curr)
-                return
+        for d in map(int, digits):
+            tmp = []
             
-            for x in mp[int(digits[index])]:
-                go(index + 1, curr + x)
+            while deq:
+                curr = deq.popleft()
+                for digit in nums[d]:
+                    tmp.append(curr+digit)
             
-        go(0, "")
-            
-        return res
+            for e in tmp:
+                deq.append(e)
+
+        
+        return list(deq)
