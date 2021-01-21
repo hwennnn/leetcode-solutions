6f0a653ff791ab50c999781a602bc2874ecef550
hwennnn <hwendev@gmail.com>
2021-01-21 10:58:40 +0000

    Sync LeetCode submission - Search Suggestions System (python3)

--- problems/search_suggestions_system/solution.py
+++ problems/search_suggestions_system/solution.py
@@ -1,20 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.words = []
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+            node.words.append(word)
+            node.words.sort()
+            while len(node.words) > 3:
+                node.words.pop()
+    
+    def search(self, word):
+        res = []
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                break
+            node = node.children[char]
+            res.append(node.words[:])
+        l_remain = len(word) - len(res)
+        for _ in range(l_remain):
+            res.append([])
+        return res
+
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        mp = defaultdict(list)
-        products.sort()
-        
-        for product in products:
-            curr = ""
-            
-            for x in product:
-                curr += x
-                if len(mp[curr]) == 3: continue
-                mp[curr].append(product)
-        
-        res = []
-        curr = ""
-        for word in searchWord:
-            curr += word
-            res.append(mp[curr][:3])
-        
-        return res
+        trie = Trie()
+        for prod in products:
+            trie.insert(prod)
+        return trie.search(searchWord)
