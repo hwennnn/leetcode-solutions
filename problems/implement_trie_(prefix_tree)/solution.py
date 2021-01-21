8ceb1134bcd59135ccf2fb57c10f0546d4f55f82
hwennnn <hwendev@gmail.com>
2021-01-21 02:07:55 +0000

    Sync LeetCode submission - Implement Trie (Prefix Tree) (python3)

--- problems/implement_trie_(prefix_tree)/solution.py
+++ problems/implement_trie_(prefix_tree)/solution.py
@@ -1,41 +1,58 @@
 class TrieNode:
-    
     def __init__(self):
-        self.children = defaultdict(TrieNode)
-        self.hasEnd = False
-
+        self.mp = [None] * 26
+        self.end = False
+        
 class Trie:
 
     def __init__(self):
+        """
+        Initialize your data structure here.
+        """
         self.root = TrieNode()
+        
 
     def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
         curr = self.root
-        
-        for x in word:
-            curr = curr.children[x]
-        
-        curr.hasEnd = True
-
-    def search(self, word: str) -> bool:
-        curr = self.root
-        
-        for x in word:
-            if x not in curr.children:
-                return False
+        n = len(word)
+        for i in range(n):
+            index = ord(word[i]) - ord('a')
+            if not curr.mp[index]:
+                curr.mp[index] = TrieNode()
             
-            curr = curr.children[x]
+            curr = curr.mp[index]
         
-        return curr.hasEnd
+        curr.end = True
+        
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        curr = self.root
+        n = len(word)
+        for i in range(n):
+            index = ord(word[i]) - ord('a')
+            if not curr.mp[index]: return False
+            
+            curr = curr.mp[index]
+        
+        return curr.end
+        
 
     def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
         curr = self.root
-        
-        for x in prefix:
-            if x not in curr.children:
-                return False
+        n = len(prefix)
+        for i in range(n):
+            index = ord(prefix[i]) - ord('a')
+            if not curr.mp[index]: return False
             
-            curr = curr.children[x]
+            curr = curr.mp[index]
         
         return True
 
