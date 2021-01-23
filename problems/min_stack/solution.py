70c845188b38fbf8e66fe68a2b736d9e6ef4c3cc
hwennnn <hwendev@gmail.com>
2021-01-23 03:20:38 +0000

    Sync LeetCode submission - Min Stack (python3)

--- problems/min_stack/solution.py
+++ problems/min_stack/solution.py
@@ -1,32 +1,30 @@
 class MinStack:
 
     def __init__(self):
-        self.q = []
+        self.stack = []
+        self.m = float('inf')
+        
+    def push(self, x: int) -> None:
+        self.m = min(self.m, x)
+        self.stack.append((x, self.m))
 
-    # @param x, an integer
-    # @return an integer
-    def push(self, x):
-        curMin = self.getMin()
-        if curMin == None or x < curMin:
-            curMin = x
-        self.q.append((x, curMin));
-
-    # @return nothing
-    def pop(self):
-        self.q.pop()
-
-
-    # @return an integer
-    def top(self):
-        if len(self.q) == 0:
-            return None
+    def pop(self) -> None:
+        self.stack.pop()
+        if len(self.stack) > 0:
+            self.m = self.stack[-1][1]
         else:
-            return self.q[len(self.q) - 1][0]
+            self.m = float('inf')
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
 
 
-    # @return an integer
-    def getMin(self):
-        if len(self.q) == 0:
-            return None
-        else:
-            return self.q[len(self.q) - 1][1]
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
