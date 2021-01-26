6101721110464436a64154c4c2c3805b267091dc
hwennnn <hwendev@gmail.com>
2021-01-26 11:11:18 +0000

    Sync LeetCode submission - Path With Minimum Effort (python3)

--- problems/path_with_minimum_effort/solution.py
+++ problems/path_with_minimum_effort/solution.py
@@ -1,29 +1,33 @@
 class Solution:
-    def minimumEffortPath(self, A: List[List[int]]) -> int:
-        R, C = len(A), len(A[0])
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        rows, cols = len(heights), len(heights[0])
         
-        def isPath(effort):
-            deq, seen = deque([(0,0)]), {(0,0)}
+        def good(k):
+            visited = set()
             
-            while deq:
-                x, y = deq.popleft()
-                if x == R-1 and y == C-1:
-                    return True
+            def dfs(x, y):
+                if x == rows - 1 and y == cols - 1: return True
                 
-                for r,c in (x+1,y),(x-1,y),(x,y+1),(x,y-1):
-                    if 0 <= r < R and 0 <= c < C and abs(A[r][c] - A[x][y]) <= effort and (r,c) not in seen:
-                        deq.append((r,c))
-                        seen.add((r,c))
+                if 0 <= x < rows and 0 <= y < cols:
+                    for dx,dy in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
+                        if 0 <= dx < rows and 0 <= dy < cols and (dx,dy) not in visited and abs(heights[x][y] - heights[dx][dy]) <= k:
+                            visited.add((dx,dy))
+                            if dfs(dx,dy): 
+                                return True
+                    
+                return False
             
-            return False
+            return dfs(0,0)
+                            
+            
         
-        low, high = 0, 10**6
-        while low < high:
-            effort = low + high >> 1
-            if isPath(effort):
-                high = effort
+        left, right = 0, 1000000
+        
+        while left < right:
+            mid = left + (right-left) // 2
+            if good(mid):
+                right = mid
             else:
-                low = effort + 1
-        
-        return low
-        
+                left = mid + 1
+            
+        return left
