eb02f778879625a6a8e1a30311302977e2913383
hwennnn <hwendev@gmail.com>
2021-01-27 09:01:24 +0000

    Sync LeetCode submission - Concatenation of Consecutive Binary Numbers (python3)

--- problems/concatenation_of_consecutive_binary_numbers/solution.py
+++ problems/concatenation_of_consecutive_binary_numbers/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
         M = 10 ** 9 + 7
+        
         s = []
         for i in range(1, n+1):
             s.append(str(bin(i)[2:]))
