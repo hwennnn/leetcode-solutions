f98dd031f4ff5c9a3176acce7791936e74374495
hwennnn <hwendev@gmail.com>
2021-01-28 10:05:20 +0000

    Sync LeetCode submission - Smallest String With A Given Numeric Value (python3)

--- problems/smallest_string_with_a_given_numeric_value/solution.py
+++ problems/smallest_string_with_a_given_numeric_value/solution.py
@@ -1,25 +1,11 @@
 class Solution:
     def getSmallestString(self, n: int, k: int) -> str:
+        res = ["a" for _ in range(n)]
+        k -= n
         
-        res = []
-        
-        while k > 0 and n > 0:
-            if (26 + n) <= k:
-                res.append('z')
-                k -= 26
-                n -= 1
-            else:
-                if n == 1:
-                    t = k % 27
-                    res.append(chr(t+96))
-                    k -= t
-                    n -= 1
-                else:
-                    t = (k-n+1) % 27
-                    res.append(chr(t+96))
-                    k -= t
-                    n -= 1
-        
-        res.sort()
+        for i in range(n-1, -1, -1):
+            x = min(k, 25)
+            k -= x
+            res[i] = chr(ord('a') + x)
         
         return "".join(res)
