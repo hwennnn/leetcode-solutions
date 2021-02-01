1301a09ef242edd2588d7fc36a478a87de1d45cf
hwennnn <hwendev@gmail.com>
2021-02-01 10:31:45 +0000

    Sync LeetCode submission - Number of 1 Bits (python3)

--- problems/number_of_1_bits/solution.py
+++ problems/number_of_1_bits/solution.py
@@ -1,10 +1,3 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        res = 0
-        
-        while n > 0:
-            if n & 1:
-                res += 1
-            n >>= 1
-        
-        return res
+        return bin(n).count("1")
