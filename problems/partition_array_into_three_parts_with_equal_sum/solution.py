960663ac1c00ea7d4be90f7ecabb8791757a9d42
hwennnn <hwendev@gmail.com>
2021-02-15 06:11:59 +0000

    Sync LeetCode submission - Partition Array Into Three Parts With Equal Sum (python3)

--- problems/partition_array_into_three_parts_with_equal_sum/solution.py
+++ problems/partition_array_into_three_parts_with_equal_sum/solution.py
@@ -2,15 +2,14 @@ class Solution:
     def canThreePartsEqualSum(self, arr: List[int]) -> bool:
         n = len(arr)
         total = sum(arr)
-        if total % 3 != 0: return False
-        target = total // 3
-        count = s = 0
+
+        found = 0
+        s = 0
         
-        for i, x in enumerate(arr):
+        for x in arr:
             s += x
-            
-            if s == target:
+            if s == total // 3:
                 s = 0
-                count += 1
+                found += 1
         
-        return count >= 3
+        return found >= 3
