d2c99e836ea4b4808e80f9e2b3c3479ed51b2870
hwennnn <hwendev@gmail.com>
2021-03-23 13:01:27 +0000

    Sync LeetCode submission - 3Sum With Multiplicity (python3)

--- problems/3sum_with_multiplicity/solution.py
+++ problems/3sum_with_multiplicity/solution.py
@@ -1,17 +1,15 @@
 class Solution:
-    def threeSumMulti(self, arr: List[int], target: int) -> int:
-        n = len(arr)
-        count = Counter()
-        M = 10 ** 9 + 7
+    def threeSumMulti(self, A: List[int], target: int) -> int:
+        mp = collections.defaultdict(int)
+        n = len(A)
         res = 0
+        M = 10 ** 9 + 7
         
         for i in range(n):
-            res = (res + count[target - arr[i]]) % M
+            res = (res + mp[target - A[i]]) % M
             
             for j in range(i):
-                m = arr[i] + arr[j]
-                
-                count[m] += 1
+                t = A[i] + A[j]
+                mp[t] += 1
         
-        return res
-        
+        return res % M
