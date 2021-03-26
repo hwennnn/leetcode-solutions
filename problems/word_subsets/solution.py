dff20a445e61d21842d059166a057b49ae46d622
hwennnn <hwendev@gmail.com>
2021-03-26 14:39:23 +0000

    Sync LeetCode submission - Word Subsets (python3)

--- problems/word_subsets/solution.py
+++ problems/word_subsets/solution.py
@@ -1,24 +1,8 @@
 class Solution:
-    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        counter = Counter()
+    def wordSubsets(self, A, B):
+        count = collections.Counter()
         
-        for word in words2:
-            cnt = Counter(word)
+        for b in B:
+            count |= collections.Counter(b)
             
-            for x, value in cnt.items():
-                counter[x] = max(counter[x], value)
-        
-        res = []
-        
-        for word in words1:
-            curr = Counter(word)
-            
-            for x, value in counter.items():
-                if curr[x] < value:
-                    flag = False
-                    break
-            else:
-                res.append(word)
-                
-        return res
-        
+        return [a for a in A if not count - Counter(a)]
