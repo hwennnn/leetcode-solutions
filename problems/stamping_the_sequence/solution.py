13ed0ef86c9563914fa6bf03163127ba842cae38
hwennnn <hwendev@gmail.com>
2021-03-31 12:50:29 +0000

    Sync LeetCode submission - Stamping The Sequence (python3)

--- problems/stamping_the_sequence/solution.py
+++ problems/stamping_the_sequence/solution.py
@@ -1,26 +1,32 @@
 class Solution:
-    def movesToStamp(self, S: str, T: str) -> List[int]:
-        if S == T: return [0]
+    def movesToStamp(self, stamp, target):
+        m, n = len(stamp), len(target)
+        masks = [(1<<m) - 1]*(n - m + 1)
+        target_mask = (1<<n) - 1
+        queue = deque()
+        visited = set()
+        for i in range(n - m + 1):
+            for j in range(m):
+                masks[i] ^= (1<<j)*(target[j+i] == stamp[j])
+            if masks[i] == 0:
+                visited.add(i)
+                queue.append(i)
         
-        S, T = list(S), list(T)
-        sn, tn = len(S), len(T)
-        res = []
-        sDiff = tDiff = True
+        ans = []
+                
+        while queue and target_mask != 0:
+            cand = queue.popleft()
+            ans.append(cand)
+            to_update = [i for i in range(cand, min(n, cand + m)) if target_mask & (1<<i)]
+
+            for i in to_update:
+                for j in range(max(0, i - m + 1), min(i + 1, n - m + 1)):
+                    masks[j] = masks[j] - (masks[j] & (1<<(i - j)))
+                target_mask ^= (1<<i)
+
+            for j in range(max(0, cand - m + 1), min(cand + m, n - m + 1)):
+                if masks[j] == 0 and j not in visited:
+                    visited.add(j)
+                    queue.append(j)
         
-        while tDiff:
-            tDiff = False
-            
-            for i in range(tn - sn + 1):
-                sDiff = False
-                for j in range(sn):
-                    if T[i + j] == "*": continue
-                    if T[i + j] != S[j]: break
-                    sDiff = True
-                else:
-                    if sDiff:
-                        for j in range(i, i + sn):
-                            T[j] = "*"
-                        tDiff = True    
-                        res.append(i)
-        
-        return reversed(res) if all(c == "*" for c in T) else []
+        return ans[::-1] if target_mask == 0 else []
