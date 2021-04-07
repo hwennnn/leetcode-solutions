518fc4c4641cf6b7feb637b559339c761b218582
hwennnn <hwendev@gmail.com>
2021-04-07 10:34:24 +0000

    Sync LeetCode submission - Determine if String Halves Are Alike (python3)

--- problems/determine_if_string_halves_are_alike/solution.py
+++ problems/determine_if_string_halves_are_alike/solution.py
@@ -1,8 +1,6 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
         n = len(s) // 2
-        
-        def count(s):
-            return sum(1 for c in s if c in "aeiouAEIOU")
-        
-        return count(s[:n]) == count(s[n:])
+        ss = set("aeiouAEIOU")
+
+        return sum(c in ss for c in s[:n]) == sum(c in ss for c in s[n:])
