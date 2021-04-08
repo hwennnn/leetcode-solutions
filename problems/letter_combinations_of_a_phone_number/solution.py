8fffae147ff33467f4b22663c4f0f79098e7041f
hwennnn <hwendev@gmail.com>
2021-04-08 08:12:05 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (python3)

--- problems/letter_combinations_of_a_phone_number/solution.py
+++ problems/letter_combinations_of_a_phone_number/solution.py
@@ -1,20 +1,18 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits: return []
-        nums = ["0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
         
-        deq = collections.deque([""])   
+        dic = {"2":"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
+        res = []
         
-        for d in map(int, digits):
-            tmp = []
-            
-            while deq:
-                curr = deq.popleft()
-                for digit in nums[d]:
-                    tmp.append(curr+digit)
-            
-            for e in tmp:
-                deq.append(e)
+        self.dfs(dic, digits, 0, "", res)
+        
+        return res
+    
+    def dfs(self, dic, digits, index, curr, res):
+        if len(curr) == len(digits):
+            res.append(curr)
+            return
 
-        
-        return list(deq)
+        for c in dic[digits[index]]:
+            self.dfs(dic, digits, index+1, curr+c, res)
