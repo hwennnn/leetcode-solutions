1d64ad964f951e7195c21cb348777fccc5aaf965
hwennnn <hwendev@gmail.com>
2021-04-10 16:12:06 +0000

    Sync LeetCode submission - Minimum Window Substring (python3)

--- problems/minimum_window_substring/solution.py
+++ problems/minimum_window_substring/solution.py
@@ -1,24 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        k = len(t)
-        counter = Counter(t)
-        i = 0
-        res = (100001, 0)
         
-        for j, x in enumerate(s):
-            if counter[x] > 0:
-                k -= 1
-                
-            counter[x] -= 1
+        mp = collections.Counter(t)
+        
+        n, start, end, required = len(s), 0, 0, len(t)
+        res = (float('inf'), 0)
+        
+        while end < n:
+            if mp[s[end]] > 0:
+                required -= 1
             
-            while k == 0:
-                if j - i + 1 < res[0]:
-                    res = (j - i + 1, i)
-                    
-                if counter[s[i]] == 0:
-                    k += 1
-                    
-                counter[s[i]] += 1
-                i += 1
+            mp[s[end]] -= 1
+            
+            while required == 0:
+                l = end - start + 1
+                if l < res[0]:
+                    res = (l, start)
+                
+                mp[s[start]] += 1
+                if mp[s[start]] > 0:
+                    required += 1
+                
+                start += 1
+            
+            end += 1
         
-        return "" if res[0] == 100001 else s[res[1] : res[0] + res[1]]
+        return "" if res[0] == float('inf') else s[res[1]: res[0] + res[1]]
