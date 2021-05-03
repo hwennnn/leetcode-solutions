f85ebb414dc44fb85efeebf0454473931b8b5061
hwennnn <hwendev@gmail.com>
2021-05-03 13:00:43 +0000

    Sync LeetCode submission - Distance Between Bus Stops (python3)

--- problems/distance_between_bus_stops/solution.py
+++ problems/distance_between_bus_stops/solution.py
@@ -1,8 +1,6 @@
 class Solution:
-    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
-        
-        a = min(start,destination)
-        
-        b = max(start,destination)
+    def distanceBetweenBusStops(self, distance: List[int], start: int, end: int) -> int:
+        a = min(start, end)
+        b = max(start, end)
         
         return min(sum(distance[a:b]), sum(distance) - sum(distance[a:b]))
