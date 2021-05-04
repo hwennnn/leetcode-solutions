881d1470112baa3613f84c94a342b23622f772b5
hwennnn <hwendev@gmail.com>
2021-05-04 11:48:20 +0000

    Sync LeetCode submission - Non-decreasing Array (python3)

--- problems/non-decreasing_array/solution.py
+++ problems/non-decreasing_array/solution.py
@@ -1,11 +1,8 @@
 class Solution:
     def checkPossibility(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+
+        if len(nums) < 3: return True
         
-        if len(nums)<3: return True
         count = 0
         for i in range(1, len(nums)):
             if nums[i-1] > nums[i]:
@@ -16,6 +13,5 @@ class Solution:
                     nums[i-1] = nums[i]
                 else:
                     nums[i] = nums[i-1]
-                if count > 1:
-                    return False
+                
         return count <= 1
