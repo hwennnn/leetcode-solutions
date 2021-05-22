46744949e41601564d6c570b6816d5e66654a5e2
hwennnn <hwendev@gmail.com>
2021-05-22 16:29:56 +0000

    Sync LeetCode submission - Number of Equivalent Domino Pairs (python3)

--- problems/number_of_equivalent_domino_pairs/solution.py
+++ problems/number_of_equivalent_domino_pairs/solution.py
@@ -1,18 +1,16 @@
 class Solution:
-    def numEquivDominoPairs(self, arr: List[List[int]]) -> int:
+    def numEquivDominoPairs(self, A: List[List[int]]) -> int:
+        n = len(A)
+        mp = collections.defaultdict(int)
         
-        n = len(arr)
-        dic = {}
+        def make(A):
+            return (min(A), max(A))
+        
         res = 0
-        
-        for pair in arr:
-            num = min(pair[0],pair[1])*10 + max(pair[0],pair[1])
-            if num in dic:
-                res += dic[num]
-                dic[num] += 1
-            else:
-                dic[num] = 1
-
-        return res
+        for i in range(n):
+            m = make(A[i])
+            res += mp[m]
             
-                    
+            mp[m] += 1
+        
+        return res
