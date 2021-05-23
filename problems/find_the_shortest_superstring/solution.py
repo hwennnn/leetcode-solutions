c5629134b3e69ad79e16b421b64fa597b30f2bd6
hwennnn <hwendev@gmail.com>
2021-05-23 08:48:35 +0000

    Sync LeetCode submission - Find the Shortest Superstring (python3)

--- problems/find_the_shortest_superstring/solution.py
+++ problems/find_the_shortest_superstring/solution.py
@@ -1,53 +1,50 @@
 class Solution:
-    def shortestSuperstring(self, words: List[str]) -> str:
-        
-        def distance(s1, s2):
-            for i in range(1, len(s1)):
-                if s2.startswith(s1[i:]):
-                    return len(s1) - i + 1
-            
-            return 1
-        
-        n = len(words)
-        weights = [[0] * n for _ in range(n)]
-        dp = [[0] * n for _ in range(1 << n)]
-        queue = deque([(0, i, 1 << i, [i]) for i in range(n)])
-        full_mask = (1 << n) - 1
-        maxWeight, maxPath = -1, []
-        
-        for i in range(n):
-            for j in range(i, n):
-                weights[i][j] = distance(words[i], words[j])
-                weights[j][i] = distance(words[j], words[i])
-        
-        while queue:
-            w, node, mask, path = queue.popleft()
-            
-            if dp[mask][node] != w:
-                continue
-            
-            if mask == full_mask and w > maxWeight:
-                maxWeight = w
-                maxPath = path
-                continue
-            
-            for nei in range(n):
-                if mask & (1 << nei) > 0: continue
+    def shortestSuperstring(self, A):
+
+        def overlap(p, n):
+            l = min(len(p), len(n))
+            for i in range(l - 1, -1, -1):
+                if p[len(p) - i:] == n[:i]:
+                    return i
+            return 0
+                    
+        l = len(A)
+        dist=[[0] * l for _ in range(l)]
+        for i in range(l):
+            for j in range(i + 1, l):
+                dist[i][j] = overlap(A[i], A[j])
+                dist[j][i] = overlap(A[j], A[i])
+                # print(dist[i][j], dist[j][i])
                 
-                new_mask = mask | (1 << nei)
-                old = dp[new_mask][nei]
-                new = dp[mask][node] + weights[node][nei]
+        dp = [[0] * l for _ in range(1 << l)] # total overlap
+        parent = [[None] * l for _ in range(1 << l)]
+        
+        for mask in range(1 << l):
+            for bit in range(l):
+                if (1 << bit) & mask:
+                    pmask = mask ^ (1 << bit)
+                    if pmask == 0: continue
+                        
+                    for pbit in range(l):
+                        if (1 << pbit) & pmask:
+                            if dp[pmask][pbit] + dist[pbit][bit] >= dp[mask][bit]:
+                                dp[mask][bit] = dp[pmask][pbit] + dist[pbit][bit]
+                                parent[mask][bit] = pbit
+                                        
+        mask = (1 << l) - 1
+        bit = dp[mask].index(max(dp[mask]))
+        idxList = []
+        
+        while bit is not None:
+            idxList.append(bit)
+            bit, mask = parent[mask][bit], mask ^ (1 << bit)
+        idxList = idxList[::-1]
+        
+        res = []
+        for i in range(len(idxList)):
+            if i == 0:
+                res.append(A[idxList[i]])
+            else:
+                res.append(A[idxList[i]][dist[idxList[i - 1]][idxList[i]]:])
                 
-                if new > old:
-                    dp[new_mask][nei] = new
-                    queue.append((new, nei, new_mask, path + [nei]))
-        
-        res = words[maxPath[0]]
-        
-        for i in range(1, n):
-            prev, curr = maxPath[i - 1], maxPath[i]
-            overlap = weights[prev][curr] - 1
-            res += words[curr][overlap:]
-            
-        return res
-            
+        return ''.join(res)
