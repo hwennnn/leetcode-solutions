6e65afdc5a3ce5852bc527f80aabd31becc36cd9
hwennnn <hwendev@gmail.com>
2021-05-29 09:48:12 +0000

    Sync LeetCode submission - Shortest Path in Binary Matrix (python3)

--- problems/shortest_path_in_binary_matrix/solution.py
+++ problems/shortest_path_in_binary_matrix/solution.py
@@ -1,21 +1,19 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
         if grid[0][0] == 1: return -1
         
-        rows, cols = len(grid), len(grid[0])
-        queue = collections.deque([(0,0,1)])
+        queue = collections.deque([(0, 0, 1)])
+        visited = set()
         
         while queue:
             x, y, steps = queue.popleft()
-            
             if x == rows - 1 and y == cols - 1: return steps
             
-            for nx in range(-1, 2):
-                for ny in range(-1, 2):
-                    dx, dy = x + nx, y + ny
-                    if 0 <= dx < rows and 0 <= dy < cols and grid[dx][dy] == 0:
-                        grid[dx][dy] = 1
-                        queue.append((dx,dy,steps+1))
-
-        return -1
+            for dx in range(x - 1, x + 2):
+                for dy in range(y - 1, y + 2):
+                    if 0 <= dx < rows and 0 <= dy < cols and grid[dx][dy] == 0 and (dx, dy) not in visited:
+                        queue.append((dx, dy, steps + 1))
+                        visited.add((dx, dy))
         
+        return -1
