0c0e82260433d0b421985acf7e327f6dfe96b0ff
hwennnn <hwendev@gmail.com>
2021-05-30 13:09:28 +0000

    Sync LeetCode submission - Convert to Base -2 (python3)

--- problems/convert_to_base_-2/solution.py
+++ problems/convert_to_base_-2/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def baseNeg2(self, n: int) -> str:
         res = []
-
-        while n != 0:
+        
+        while n:
             res.append(n & 1)
             n = -(n >> 1)
         
