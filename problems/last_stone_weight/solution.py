2b65fcb08e6677a3f052a00c7f83f30c51e311d6
hwennnn <hwendev@gmail.com>
2021-06-05 14:10:09 +0000

    Sync LeetCode submission - Last Stone Weight (python3)

--- problems/last_stone_weight/solution.py
+++ problems/last_stone_weight/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-    
+        
         while len(stones) > 1:
             stones.sort()
-            val1,val2 = stones[-1], stones[-2]
-            val = val1 - val2 if val1 != val2 else 0
-            stones = stones[:-2] + [val] if val else stones[:-2]
-            
-        return stones[-1] if stones else 0
+            if stones[-1] == stones[-2]:
+                stones.pop(); stones.pop()
+            else:
+                stones.append(stones.pop() - stones.pop())
+        
+        return stones[0] if stones else 0
