c746e50af2f1aea8285394e325b2d4e12de67a85
hwennnn <hwendev@gmail.com>
2021-06-06 10:47:38 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (python3)

--- problems/longest_consecutive_sequence/solution.py
+++ problems/longest_consecutive_sequence/solution.py
@@ -7,12 +7,12 @@ class Solution:
             first = last = s.pop()
             
             while first - 1 in s:
+                s.remove(first - 1)
                 first -= 1
-                s.remove(first)
             
             while last + 1 in s:
+                s.remove(last + 1)
                 last += 1
-                s.remove(last)
             
             res = max(res, last - first + 1)
         
