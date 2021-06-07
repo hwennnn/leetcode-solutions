4c95c8ea29807d115b277277c7af99f46feb251d
hwennnn <hwendev@gmail.com>
2021-06-07 07:09:12 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (python3)

--- problems/min_cost_climbing_stairs/solution.py
+++ problems/min_cost_climbing_stairs/solution.py
@@ -1,15 +1,7 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        for i in range(2, len(cost)):
+            cost[i] += min(cost[i - 1], cost[i - 2])
         
-        n = len(cost)
-        if n <= 2: return min(cost)
-        
-        dp = [0] * n
-        dp[0] = cost[0]
-        dp[1] = cost[1]
-        
-        for i in range(2,n):
-            dp[i] = min(cost[i]+dp[i-1], cost[i]+dp[i-2])
-        
-        return min(dp[n-1],dp[n-2])
-        
+        return min(cost[-1], cost[-2])
