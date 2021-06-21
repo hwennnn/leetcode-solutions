3495e015cf255dc4583a6aec9af7992db55207fa
hwennnn <hwendev@gmail.com>
2021-06-21 07:29:46 +0000

    Sync LeetCode submission - Pascal's Triangle (python3)

--- problems/pascal's_triangle/solution.py
+++ problems/pascal's_triangle/solution.py
@@ -1,18 +1,14 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        ans = []
-        prev = None
+        res = [[0] * (i + 1) for i in range(numRows)]
         
-        for i in range(1,numRows+1):
-            temp = []
-            for j in range(1,i+1):
+        for i, row in enumerate(res):
+            row[0] = row[-1] = 1
+            
+            for j in range(1, len(row) - 1):
+                left = res[i - 1][j - 1]
+                right = res[i - 1][j]
                 
-                if j == 1 or j == i:
-                    temp.append(1)
-                else:
-                    temp.append(prev[j-1]+prev[j-2])
-                    
-            ans.append(temp)
-            prev = temp
-
-        return ans
+                row[j] = left + right
+            
+        return res
