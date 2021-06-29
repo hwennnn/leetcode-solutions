24638dca2803a6ba72bfae4cfe8147180a2a928d
hwennnn <hwendev@gmail.com>
2021-06-29 07:33:27 +0000

    Sync LeetCode submission - Max Consecutive Ones III (python3)

--- problems/max_consecutive_ones_iii/solution.py
+++ problems/max_consecutive_ones_iii/solution.py
@@ -4,13 +4,11 @@ class Solution:
         i = 0
         
         for j in range(n):
-            if nums[j] == 0:
-                k -= 1
+            if nums[j] == 0: k -= 1
             
             if k < 0:
                 if nums[i] == 0:
                     k += 1
-                
                 i += 1
-        
+
         return j - i + 1
