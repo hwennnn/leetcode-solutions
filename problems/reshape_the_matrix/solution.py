4fcc96be509e5910788e7c2830807d08f1a2da87
hwennnn <hwendev@gmail.com>
2021-07-05 08:49:49 +0000

    Sync LeetCode submission - Reshape the Matrix (python3)

--- problems/reshape_the_matrix/solution.py
+++ problems/reshape_the_matrix/solution.py
@@ -1,24 +1,18 @@
 class Solution:
-    def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        rows, cols = len(mat), len(mat[0])
+        if rows * cols != r * c: return mat
         
-        row = len(nums)
-        column = len(nums[0])
-
-        if row<=r and column<=c:
-            return nums
-
-
-        lst = [x for i in nums for x in i]
-        count = 0
-        ans = []
-
-        for _ in range(r):
-            temp = []
-
-            for _ in range(c):
-                temp.append(lst[count])
-                count+=1
-            ans.append(temp)
-
+        res = [[0] * c for _ in range(r)]
+        ii = jj = 0
         
-        return ans
+        for i in range(rows):
+            for j in range(cols):
+                if jj == c:
+                    ii += 1
+                    jj = 0
+                
+                res[ii][jj] = mat[i][j]
+                jj += 1
+        
+        return res
