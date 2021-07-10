a42ef90c43c9a33277bc2d7798444198b6f24879
hwennnn <hwendev@gmail.com>
2021-07-10 16:22:38 +0000

    Sync LeetCode submission - Nearest Exit from Entrance in Maze (python3)

--- problems/nearest_exit_from_entrance_in_maze/solution.py
+++ problems/nearest_exit_from_entrance_in_maze/solution.py
@@ -1,18 +1,19 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         rows, cols = len(maze), len(maze[0])
-        sx, sy = entrance
-        queue = deque([(sx, sy, 0)])
-        maze[sx][sy] = "+"
+        ox, oy = entrance
+        if maze[ox][oy] == '+': return -1
+        
+        queue = collections.deque([[ox, oy, 0]])
+        visited = set([(ox, oy)])
         
         while queue:
             x, y, steps = queue.popleft()
+            if steps > 0 and (x == 0 or y == 0 or x == rows - 1 or y == cols - 1): return steps
             
-            if (x, y) != (sx, sy) and (x == 0 or x == rows - 1 or y == 0 or y == cols - 1): return steps
-            
-            for dx, dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-                if 0 <= dx < rows and 0 <= dy < cols and maze[dx][dy] == ".":
-                    maze[dx][dy] = "+"
+            for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+                if 0 <= dx < rows and 0 <= dy < cols and (dx, dy) not in visited and maze[dx][dy] == '.':
+                    visited.add((dx, dy))
                     queue.append((dx, dy, steps + 1))
         
         return -1
