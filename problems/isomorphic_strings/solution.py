1400efca9ec4fe2f9f33afd1c4303998e00c1219
hwennnn <hwendev@gmail.com>
2021-07-12 09:45:11 +0000

    Sync LeetCode submission - Isomorphic Strings (python3)

--- problems/isomorphic_strings/solution.py
+++ problems/isomorphic_strings/solution.py
@@ -1,15 +1,17 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        s, t = list(s), list(t)
+        s1, s2 = {}, {}
         
-        def decode(s):
-            dic = {}
-            lst = []
-            for char in s:
-                if char not in dic:
-                    dic[char] = len(dic)
-                lst.append(dic[char])
-            return str(lst)
-        
-        
+        for i, (a,b) in enumerate(zip(s, t)):
+            if a in s1:
+                s[i] = s1[a]
+            else:
+                s[i] = s1[a] = len(s1)
             
-        return decode(s) == decode(t)
+            if b in s2:
+                t[i] = s2[b]
+            else:
+                t[i] = s2[b] = len(s2)
+        
+        return s == t
