02e6dae1f14a9c756f5d3587946019eddf8f9051
hwennnn <hwendev@gmail.com>
2021-07-28 15:53:23 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- problems/combination_sum/solution.py
+++ problems/combination_sum/solution.py
@@ -1,17 +1,18 @@
 class Solution:
-    def combinationSum(self, candidates, target):
-
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
         res = []
-        candidates.sort()
         
         def dfs(target, index, path):
-            if target < 0:
-                return  # backtracking
+            if target < 0: return
+            
             if target == 0:
                 res.append(path)
-                return 
-            for i in range(index, len(candidates)):
-                dfs(target-candidates[i], i, path+[candidates[i]])
+                return
+            
+            for i in range(index, n):
+                dfs(target - candidates[i], i, path + [candidates[i]])
         
         dfs(target, 0, [])
+        
         return res
