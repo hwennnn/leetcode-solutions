806605384a2ef4c5a8cf1a26e1d8833499e8645d
hwennnn <hwendev@gmail.com>
2021-08-04 12:39:53 +0000

    Sync LeetCode submission - Path Sum II (python3)

--- problems/path_sum_ii/solution.py
+++ problems/path_sum_ii/solution.py
@@ -5,18 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
         res = []
-
-        def go(node, curr, path):
-            if not node: return
-
-            if not node.left and not node.right and curr + node.val == targetSum:
-                res.append(path + [node.val])
-                return
-            
-            go(node.left, curr + node.val, path + [node.val])
-            go(node.right, curr + node.val, path + [node.val])
         
-        go(root, 0, [])
+        def dfs(node, curr, path):
+            if not node: return
+            
+            if not node.left and not node.right and curr == targetSum:
+                res.append(path)
+            
+            for leaf in (node.left, node.right):
+                if leaf:
+                    dfs(leaf, curr + leaf.val, path + [leaf.val])
+        
+        if root:
+            dfs(root, root.val, [root.val])
+        
         return res
