fcebfb838c8ff19a1d4a61c5443babdc8ab0ccef
hwennnn <hwendev@gmail.com>
2021-08-15 07:29:29 +0000

    Sync LeetCode submission - Minimum Window Substring (python3)

--- problems/minimum_window_substring/solution.py
+++ problems/minimum_window_substring/solution.py
@@ -3,19 +3,19 @@ class Solution:
         
         mp = collections.Counter(t)
         
-        n, start, end, required = len(s), 0, 0, len(t)
-        res = (float('inf'), 0)
+        start, end, required = 0, 0, len(t)
+        res = (float(inf), 0)
         
-        while end < n:
+        while end < len(s):
             if mp[s[end]] > 0:
                 required -= 1
             
             mp[s[end]] -= 1
             
             while required == 0:
-                l = end - start + 1
-                if l < res[0]:
-                    res = (l, start)
+                length = end - start + 1
+                if length < res[0]:
+                    res = (length, start)
                 
                 mp[s[start]] += 1
                 if mp[s[start]] > 0:
@@ -24,5 +24,5 @@ class Solution:
                 start += 1
             
             end += 1
-        
-        return "" if res[0] == float('inf') else s[res[1]: res[0] + res[1]]
+            
+        return "" if res[0] == float('inf') else s[res[1]: res[1] + res[0]]
