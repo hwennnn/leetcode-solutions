534b8ab19c361fd33e56ce5801ae72ddf9fb9c9d
hwennnn <hwendev@gmail.com>
2021-08-28 06:22:46 +0000

    Sync LeetCode submission - Sort Colors (python3)

--- problems/sort_colors/solution.py
+++ problems/sort_colors/solution.py
@@ -1,14 +1,22 @@
 class Solution:
-    def sortColors(self, nums):
-        red, white, blue = 0, 0, len(nums)-1
-
-        while white <= blue:
-            if nums[white] == 0:
-                nums[red], nums[white] = nums[white], nums[red]
-                white += 1
-                red += 1
-            elif nums[white] == 1:
-                white += 1
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        low, high = 0, n - 1
+        i = low
+        
+        while i <= high:
+            if nums[i] == 0:
+                if nums[low] != 0:
+                    nums[i], nums[low] = nums[low], nums[i]
+                i += 1
+                low += 1
+            elif nums[i] == 2:
+                if nums[high] != 2:
+                    nums[i], nums[high] = nums[high], nums[i]
+                high -= 1
             else:
-                nums[white], nums[blue] = nums[blue], nums[white]
-                blue -= 1
+                i += 1
+        
