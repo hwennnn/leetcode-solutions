fea2d76fc145021190d7f9dc33b47fe079313df8
hwennnn <hwendev@gmail.com>
2021-08-30 13:09:05 +0000

    Sync LeetCode submission - Minimum Size Subarray Sum (python3)

--- problems/minimum_size_subarray_sum/solution.py
+++ problems/minimum_size_subarray_sum/solution.py
@@ -1,30 +1,14 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        n = len(nums)
-        total = sum(nums)
+        res = float('inf')
+        i = ssum = 0
         
-        if total < target: return 0
-        
-        left, right = 1, n
-        
-        def good(k):
-            s = 0
+        for j,x in enumerate(nums):
+            ssum += x
             
-            for i, x in enumerate(nums):
-                if i >= k:
-                    s -= nums[i - k]
-                s += x
-                
-                if s >= target: return True
-            
-            return False
+            while i <= j and ssum >= target:
+                res = min(res, j - i + 1)
+                ssum -= nums[i]
+                i += 1
         
-        while left < right:
-            mid = left + (right - left) // 2
-            
-            if good(mid):
-                right = mid
-            else:
-                left = mid + 1
-        
-        return left
+        return 0 if res == float('inf') else res
