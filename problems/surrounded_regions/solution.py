1cd525b48bc733320a183e72f6f4abdcf60c1192
hwennnn <hwendev@gmail.com>
2021-09-02 13:44:20 +0000

    Sync LeetCode submission - Surrounded Regions (python3)

--- problems/surrounded_regions/solution.py
+++ problems/surrounded_regions/solution.py
@@ -1,37 +1,13 @@
 class Solution:
-    def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        rows, cols = len(board), len(board[0])
-        queue = collections.deque()
-        
-        for x in range(rows):
-            for y in [0, cols - 1]:
-                if board[x][y] == 'O':
-                    board[x][y] = 'G'
-                    queue.append((x, y))
-        
-        for x in [0, rows - 1]:
-            for y in range(cols):
-                if board[x][y] == 'O':
-                    board[x][y] = 'G'
-                    queue.append((x, y))
+    def solve(self, board):
+        if not any(board): return
 
-        while queue:
-            x, y = queue.popleft()
-            
-            for dx, dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-                if 0 <= dx < rows and 0 <= dy < cols and board[dx][dy] == 'O':
-                    board[dx][dy] = 'G'
-                    queue.append((dx, dy))
-        
-        for x in range(rows):
-            for y in range(cols):
-                if board[x][y] == 'O':
-                    board[x][y] = 'X'
-                elif board[x][y] == 'G':
-                    board[x][y] = 'O'
-            
-        
-        
+        m, n = len(board), len(board[0])
+        save = [ij for k in range(m+n) for ij in ((0, k), (m-1, k), (k, 0), (k, n-1))]
+        while save:
+            i, j = save.pop()
+            if 0 <= i < m and 0 <= j < n and board[i][j] == 'O':
+                board[i][j] = 'S'
+                save += (i, j-1), (i, j+1), (i-1, j), (i+1, j)
+
+        board[:] = [['XO'[c == 'S'] for c in row] for row in board]
