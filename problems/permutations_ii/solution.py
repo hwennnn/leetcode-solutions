3b5c99dc95a4651e757456ff30475b8d558e23ba
hwennnn <hwendev@gmail.com>
2021-09-04 09:40:52 +0000

    Sync LeetCode submission - Permutations II (python3)

--- problems/permutations_ii/solution.py
+++ problems/permutations_ii/solution.py
@@ -1,15 +1,22 @@
 class Solution:
-    def permuteUnique(self, nums):
-        ans = [[]]
-        for n in nums:
-            new_ans = []
-            for l in ans:
-                for i in range(len(l)+1):
-                    new_ans.append(l[:i]+[n]+l[i:])
-                    if i<len(l) and l[i]==n: break             
-            ans = new_ans
-        return ans
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        n = len(nums)
         
+        def backtrack(visited = set(), path = []):
+            if len(path) == n:
+                res.append(path)
+                return
+            
+            for i in range(n):
+                if i in visited: continue
+                
+                if i > 0 and nums[i] == nums[i - 1] and i - 1 not in visited: continue
+                
+                visited.add(i)
+                backtrack(visited, path + [nums[i]])
+                visited.remove(i)
         
-        
-        
+        backtrack()
+        return res
