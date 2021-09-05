4de995cf9a53d66fbc729c6004f9f2527465ee57
hwennnn <hwendev@gmail.com>
2021-09-05 11:05:38 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (python3)

--- problems/letter_combinations_of_a_phone_number/solution.py
+++ problems/letter_combinations_of_a_phone_number/solution.py
@@ -2,17 +2,19 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits: return []
         
-        dic = {"2":"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
+        n = len(digits)
         res = []
+        dic = {"2":"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
+
+        def backtrack(i, curr):
+            if len(curr) == n:
+                res.append(curr)
+                return
+            
+            for j in range(i, n):
+                for word in dic[digits[j]]:
+                    backtrack(j + 1, curr + word)
         
-        self.dfs(dic, digits, 0, "", res)
+        backtrack(0, "")
         
         return res
-    
-    def dfs(self, dic, digits, index, curr, res):
-        if len(curr) == len(digits):
-            res.append(curr)
-            return
-
-        for c in dic[digits[index]]:
-            self.dfs(dic, digits, index+1, curr+c, res)
