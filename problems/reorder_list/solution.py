97778245f14cbffd02b99244986fae280f5b1468
hwennnn <hwendev@gmail.com>
2021-09-07 13:39:23 +0000

    Sync LeetCode submission - Reorder List (python3)

--- problems/reorder_list/solution.py
+++ problems/reorder_list/solution.py
@@ -4,39 +4,43 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reorderList(self, head):
+    def reverseLL(self, head):
+        prev = None
+
+        while head:
+            nxt = head.next
+            head.next = prev
+            prev = head
+            head = nxt
+
+        return prev
+    
+    def mergeLL(self, l1, l2):
+        while l1:
+            n1, n2 = l1.next, l2.next
+            l1.next = l2
+            
+            if not n1: break
+            
+            l2.next = n1
+            l1 = n1
+            l2 = n2
+            
+    def reorderList(self, head: Optional[ListNode]) -> None:
         """
-        :type head: ListNode
-        :rtype: void Do not return anything, modify head in-place instead.
+        Do not return anything, modify head in-place instead.
         """
-        if not head or not head.next:
-            return
-        slow = head
-        fast = head.next
-        while fast.next and fast.next.next:
+        if not head or not head.next: return head
+        
+        prev, slow, fast, l1 = None, head, head, head
+        
+        while fast and fast.next:
+            prev = slow
             slow = slow.next
             fast = fast.next.next
-        slow = slow.next
-        if fast.next:
-            fast = fast.next
-            
-        # reverse the second half of the list
-        prev = slow
-        curr = slow.next
-        prev.next = None
-        while curr:
-            tmp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = tmp
-            
-        # turn the list into zigzag manner
-        trav = head
-        while fast.next:
-            tmp1 = trav.next
-            tmp2 = fast.next
-            trav.next = fast
-            fast.next = tmp1
-            trav = tmp1
-            fast = tmp2
         
+        prev.next = None
+        
+        l2 = self.reverseLL(slow)
+
+        self.mergeLL(l1, l2)
