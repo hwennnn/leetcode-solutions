a8336725dd65632a8d2402c6cac9ecd08eb89609
hwennnn <hwendev@gmail.com>
2021-09-11 06:16:49 +0000

    Sync LeetCode submission - Binary Search Tree Iterator (python3)

--- problems/binary_search_tree_iterator/solution.py
+++ problems/binary_search_tree_iterator/solution.py
@@ -5,26 +5,27 @@
 #         self.left = left
 #         self.right = right
 class BSTIterator:
-    # @param root, a binary search tree's root node
-    def __init__(self, root):
-        self.stack = list()
-        self.pushAll(root)
 
-    # @return a boolean, whether we have a next smallest number
-    def hasNext(self):
-        return self.stack
+    def __init__(self, root: Optional[TreeNode]):
+        self.stack = []
+        
+        while root:
+            self.stack.append(root)
+            root = root.left
 
-    # @return an integer, the next smallest number
-    def next(self):
-        tmpNode = self.stack.pop()
-        self.pushAll(tmpNode.right)
-        return tmpNode.val
+    def next(self) -> int:
+        node = self.stack.pop()
+        res = node.val
+        right = node.right
         
-    def pushAll(self, node):
-        while node is not None:
-            self.stack.append(node)
-            node = node.left
-        
+        while right:
+            self.stack.append(right)
+            right = right.left
+            
+        return res
+
+    def hasNext(self) -> bool:
+        return len(self.stack) > 0
 
 
 # Your BSTIterator object will be instantiated and called as such:
