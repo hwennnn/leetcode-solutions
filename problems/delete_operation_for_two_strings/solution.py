7647fd073ce0234216f4916ac64fea0058816fd4
hwennnn <hwendev@gmail.com>
2021-09-11 04:15:34 +0000

    Sync LeetCode submission - Delete Operation for Two Strings (python3)

--- problems/delete_operation_for_two_strings/solution.py
+++ problems/delete_operation_for_two_strings/solution.py
@@ -1,16 +1,13 @@
 class Solution:
-    def minDistance(self, w1, w2):
-
-        m, n = len(w1), len(w2)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+    def minDistance(self, word1: str, word2: str) -> int:
+        n, m = len(word1), len(word2)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
         
-        for i in range(m):
-            for j in range(n):
-                if w1[i] == w2[j]:
-                    dp[i + 1][j + 1] = 1 + dp[i][j]
+        for i in range(n):
+            for j in range(m):
+                if word1[i] == word2[j]:
+                    dp[i + 1][j + 1] = dp[i][j] + 1
                 else:
                     dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
         
-        lcs = dp[-1][-1]
-
-        return m + n - (2 * lcs)
+        return n + m - 2 * dp[-1][-1]
