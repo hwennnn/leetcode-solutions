f62957d0b2ac6b925fdf597936075ea7c970f580
hwennnn <hwendev@gmail.com>
2021-09-19 05:51:11 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mp = {}
         
-        for i, x in enumerate(nums):
+        for i,x in enumerate(nums):
             if target - x in mp:
                 return [mp[target - x], i]
             
