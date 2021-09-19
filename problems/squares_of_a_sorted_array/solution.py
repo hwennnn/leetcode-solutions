254ec67653954991ff5f933c0855579fde46f587
hwennnn <hwendev@gmail.com>
2021-09-19 05:42:16 +0000

    Sync LeetCode submission - Squares of a Sorted Array (python3)

--- problems/squares_of_a_sorted_array/solution.py
+++ problems/squares_of_a_sorted_array/solution.py
@@ -2,16 +2,16 @@ class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         n = len(nums)
         res = [0] * n
-        idx = n-1
-        i,j = 0, n-1
+        i, j, p = 0, n - 1, n - 1
         
         while i <= j:
-            if abs(nums[i]) > abs(nums[j]):
-                res[idx] = nums[i] * nums[i]
+            if abs(nums[i]) >= abs(nums[j]):
+                res[p] = nums[i] * nums[i]
                 i += 1
             else:
-                res[idx] = nums[j] * nums[j]
+                res[p] = nums[j] * nums[j]
                 j -= 1
             
-            idx -= 1
+            p -= 1
+        
         return res
