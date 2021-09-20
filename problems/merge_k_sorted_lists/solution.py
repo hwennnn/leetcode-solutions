1bfb16860e8ff9c7f3bf8fe0fda87fb5fda7ce8f
hwennnn <hwendev@gmail.com>
2021-09-20 03:53:37 +0000

    Sync LeetCode submission - Merge k Sorted Lists (python3)

--- problems/merge_k_sorted_lists/solution.py
+++ problems/merge_k_sorted_lists/solution.py
@@ -4,21 +4,41 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        nums = []
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists: return None
+        n = len(lists)
         
-        for l in lists:
-            curr = l
-            while curr:
-                nums.append(curr.val)
-                curr = curr.next
+        def mergeLists(start, end):
+            if start == end: 
+                return lists[start]
+            elif start < end:
+                mid = start + (end - start) // 2
                 
-        nums.sort()
+                l1 = mergeLists(start, mid)
+                l2 = mergeLists(mid + 1, end)
+                
+                return merge(l1, l2)
+            else:
+                return None
         
-        curr = res = ListNode()
-        for x in nums:
-            node = ListNode(x)
-            curr.next = node
-            curr = curr.next
+        def merge(l1, l2):
+            if not l1: return l2
+            if not l2: return l1
+            
+            res = curr = ListNode()
+            
+            while l1 and l2:
+                if l1.val <= l2.val:
+                    curr.next = ListNode(l1.val)
+                    l1 = l1.next
+                else:
+                    curr.next = ListNode(l2.val)
+                    l2 = l2.next
+                    
+                curr = curr.next
+            
+            curr.next = l1 or l2
+            
+            return res.next
         
-        return res.next
+        return mergeLists(0, n - 1)
