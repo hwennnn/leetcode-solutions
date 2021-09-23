c0826f6193a8ef93ea842dbf8d542025b4c0dfc4
hwennnn <hwendev@gmail.com>
2021-09-23 09:09:13 +0000

    Sync LeetCode submission - Break a Palindrome (python3)

--- problems/break_a_palindrome/solution.py
+++ problems/break_a_palindrome/solution.py
@@ -1,9 +1,7 @@
 class Solution:
     def breakPalindrome(self, A: str) -> str:
-        if len(A) < 2: return ''
-        
         for i in range(len(A) // 2):
             if A[i] != 'a':
                 return A[:i] + 'a' + A[i + 1:]
         
-        return A[:-1] + 'b'
+        return A[:-1] + 'b' if A[:-1] else ''
