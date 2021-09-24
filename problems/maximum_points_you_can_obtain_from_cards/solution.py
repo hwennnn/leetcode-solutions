3eb27d945538e03d9427ca1973183cc47f5fe67b
hwennnn <hwendev@gmail.com>
2021-09-24 13:06:14 +0000

    Sync LeetCode submission - Maximum Points You Can Obtain from Cards (python3)

--- problems/maximum_points_you_can_obtain_from_cards/solution.py
+++ problems/maximum_points_you_can_obtain_from_cards/solution.py
@@ -1,16 +1,13 @@
 class Solution:
-    def maxScore(self, A: List[int], k: int) -> int:
-        n = len(A)
-        total = sum(A)
-        
-        size = n - k
-        
-        prefix = [0]
-        for x in A:
-            prefix.append(prefix[-1] + x)
-        
+    def maxScore(self, cardPoints: List[int], k: int) -> int: 
+        n = len(cardPoints)
+        t = n - k
+        total = sum(cardPoints)
+        prefix = [0] + list(accumulate(cardPoints))
         res = float('inf')
-        for i in range(n - size + 1):
-            res = min(res, prefix[i + size] - prefix[i])
-        
+
+        for i in range(t, n + 1):
+            res = min(res, prefix[i] - prefix[i - t])
+
         return total - res
+        
