385ab39e38dab34c8eece9737b76dfc2e505ff05
hwennnn <hwendev@gmail.com>
2021-09-24 03:41:42 +0000

    Sync LeetCode submission - Decode Ways (python3)

--- problems/decode_ways/solution.py
+++ problems/decode_ways/solution.py
@@ -1,26 +1,19 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        memo = {}
         
-        def backtrack(i):
-            if i in memo: return memo[i]
-            
+        @cache
+        def go(i):
             if i == n: return 1
             
-            curr = 0
+            count = 0
             
-            # single char
-            if s[i] != "0" and i < n:
-                curr += backtrack(i + 1)
+            if s[i] != '0':
+                count += go(i + 1)
             
-            # two char
-            if s[i] != "0" and i + 1 < n and int(s[i : i + 2]) <= 26:
-                curr += backtrack(i + 2)
+                if i + 1 < n and int(s[i: i + 2]) <= 26:
+                    count += go(i + 2)
             
-            memo[i] = curr
-            
-            return curr
-            
-        return backtrack(0)
-                
+            return count
+    
+        return go(0)
