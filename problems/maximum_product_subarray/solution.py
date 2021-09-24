148c3c7584c07247562ea54e8f28854eaea51a42
hwennnn <hwendev@gmail.com>
2021-09-24 04:16:56 +0000

    Sync LeetCode submission - Maximum Product Subarray (python3)

--- problems/maximum_product_subarray/solution.py
+++ problems/maximum_product_subarray/solution.py
@@ -1,18 +1,14 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if not nums: return 0
-        pos = neg = res = nums[0]
+        res = pos = neg = nums[0]
         
         for i in range(1, len(nums)):
-            if nums[i] > 0:
-                pos = max(pos*nums[i], nums[i])
-                neg = min(neg*nums[i], nums[i])
             
-            else:
-                temp = pos
-                pos = max(neg*nums[i], nums[i])
-                neg = min(temp*nums[i], nums[i])
-        
+            if nums[i] < 0:
+                pos, neg = neg, pos
+            
+            pos = max(nums[i], pos * nums[i])
+            neg = min(nums[i], neg * nums[i])
+            
             res = max(res, pos)
-        
         return res
