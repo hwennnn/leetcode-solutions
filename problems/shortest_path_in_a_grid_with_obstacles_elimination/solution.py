737f56387f35677283efd749ea6fbc19ed50c33c
hwennnn <hwendev@gmail.com>
2021-09-25 07:58:35 +0000

    Sync LeetCode submission - Shortest Path in a Grid with Obstacles Elimination (python3)

--- problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.py
+++ problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.py
@@ -1,23 +1,22 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         rows, cols = len(grid), len(grid[0])
-        
-        queue = deque([(k, 0, 0, 0)])
+        queue = collections.deque([(0, 0, 0, k)])
         visited = set([(0, 0, k)])
         
         while queue:
-            r, steps, x, y = queue.popleft()
-            
+            x, y, steps, skip = queue.popleft()
+
             if x == rows - 1 and y == cols - 1:
                 return steps
             
             for dx, dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
                 if 0 <= dx < rows and 0 <= dy < cols:
-                    if r > 0 and grid[dx][dy] == 1 and (dx, dy, r - 1) not in visited:
-                        queue.append((r - 1, steps + 1, dx, dy))
-                        visited.add((dx, dy, r - 1))
-                    elif grid[dx][dy] == 0 and (dx, dy, r) not in visited:
-                        queue.append((r, steps + 1, dx, dy))
-                        visited.add((dx, dy, r))
-        
+                    if grid[dx][dy] == 0 and (dx, dy, skip) not in visited:
+                        queue.append((dx, dy, steps + 1, skip))
+                        visited.add((dx, dy, skip))
+                    elif grid[dx][dy] == 1 and skip >= 1 and (dx, dy, skip - 1) not in visited:
+                        queue.append((dx, dy, steps + 1, skip - 1))
+                        visited.add((dx, dy, skip - 1))
+            
         return -1
