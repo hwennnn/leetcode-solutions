3f5bc571223dfdb84539a378b192afeab34d613b
hwennnn <hwendev@gmail.com>
2021-09-25 04:47:26 +0000

    Sync LeetCode submission - Ugly Number II (python3)

--- problems/ugly_number_ii/solution.py
+++ problems/ugly_number_ii/solution.py
@@ -1,24 +1,13 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
+        t2 = t3 = t5 = 0
+        dp = [1] * n
         
-        if (n<=0): return 0
-        if (n==1): return 1
+        for i in range(1, n):
+            dp[i] = min(dp[t2] * 2, dp[t3] * 3, dp[t5] * 5)
+            
+            if dp[i] == dp[t2] * 2: t2 += 1
+            if dp[i] == dp[t3] * 3: t3 += 1
+            if dp[i] == dp[t5] * 5: t5 += 1
         
-        t2 =  t3 = t5 = 0 
-        
-        arr = [None] * n
-        arr[0] = 1
-        
-        for i in range(1,n):
-            
-            arr[i] = min(arr[t2]*2, arr[t3]*3, arr[t5]*5)
-            
-            if arr[i] == arr[t2]*2: t2+=1
-            if arr[i] == arr[t3]*3: t3+=1
-            if arr[i] == arr[t5]*5: t5+=1
-            
-        return arr[-1]
-        
-            
-                
-            
+        return dp[-1]
