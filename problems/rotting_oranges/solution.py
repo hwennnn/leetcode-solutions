498219554dcd128c7b0256b0cde8866e96fd43ae
hwennnn <hwendev@gmail.com>
2021-09-26 05:10:56 +0000

    Sync LeetCode submission - Rotting Oranges (python3)

--- problems/rotting_oranges/solution.py
+++ problems/rotting_oranges/solution.py
@@ -1,12 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        row, col = len(grid), len(grid[0])
-        rotting = {(i, j) for i in range(row) for j in range(col) if grid[i][j] == 2}
-        fresh = {(i, j) for i in range(row) for j in range(col) if grid[i][j] == 1}
-        timer = 0
-        while fresh:
-            if not rotting: return -1
-            rotting = {(i+di, j+dj) for i, j in rotting for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)] if (i+di, j+dj) in fresh}
-            fresh -= rotting
-            timer += 1
-        return timer
+        rows, cols = len(grid), len(grid[0])
+        fresh = mins = 0
+        queue = collections.deque()
+        
+        for x in range(rows):
+            for y in range(cols):
+                if grid[x][y] == 2:
+                    queue.append((x, y))
+                    
+                elif grid[x][y] == 1:
+                    fresh += 1
+                    
+        if fresh == 0: return 0
+        
+        while queue:
+            n = len(queue)
+            
+            for _ in range(n):
+                x, y = queue.popleft()
+                
+                for dx, dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+                    if 0 <= dx < rows and 0 <= dy < cols and grid[dx][dy] == 1:
+                        grid[dx][dy] = 2
+                        queue.append((dx, dy))
+                        fresh -= 1
+                        
+            mins += 1
+        
+        return mins - 1 if fresh == 0 else -1
