389bc254d1a99f2211794d14ac49716fc00de10a
hwennnn <hwendev@gmail.com>
2021-09-28 03:05:54 +0000

    Sync LeetCode submission - Permutations (python3)

--- problems/permutations/solution.py
+++ problems/permutations/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        visited = set()
+        n = len(nums)
         res = []
-        self.backtracking(res,visited,[],nums)
-        return res
-    
-    def backtracking(self,res,visited,subset,nums):
-        if len(subset) == len(nums):
-            res.append(subset)
-        for i in range(len(nums)):
-            if i not in visited:
-                visited.add(i)
-                self.backtracking(res,visited,subset+[nums[i]],nums)
-                visited.remove(i)
         
+        def backtrack(curr, mask):
+            if len(curr) == n:
+                res.append(curr)
+                return
+            
+            for i in range(n):
+                if (mask >> i) & 1:
+                    backtrack(curr + [nums[i]], mask ^ (1 << i))
+                    
+        backtrack([], (1 << n) - 1)
+        return res
