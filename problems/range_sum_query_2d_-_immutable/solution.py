fc5bd07c005a5470cbc80e5e744c86962fd627f2
hwennnn <hwendev@gmail.com>
2021-09-28 13:10:30 +0000

    Sync LeetCode submission - Range Sum Query 2D - Immutable (python3)

--- problems/range_sum_query_2d_-_immutable/solution.py
+++ problems/range_sum_query_2d_-_immutable/solution.py
@@ -1,27 +1,16 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.rows, self.cols = len(matrix), len(matrix[0])
+        rows, cols = len(matrix), len(matrix[0])
         
-        self.prefix = [[0] * (self.cols + 1) for _ in range(self.rows + 1)]
+        self.prefix = [[0] * (cols + 1) for _ in range(rows + 1)]
         
-        for i in range(1, self.rows + 1):
-            for j in range(1, self.cols + 1):
-                self.prefix[i][j] += matrix[i - 1][j - 1] 
+        for i in range(rows):
+            for j in range(cols):
+                self.prefix[i + 1][j + 1] = self.prefix[i + 1][j] + self.prefix[i][j + 1] - self.prefix[i][j] + matrix[i][j]
         
-        for i in range(1, self.rows + 1):
-            for j in range(1, self.cols + 1):
-                self.prefix[i][j] += self.prefix[i][j - 1]
-        
-        for i in range(1, self.rows + 1):
-            for j in range(1, self.cols + 1):
-                self.prefix[i][j] += self.prefix[i - 1][j]
-
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        left = self.prefix[row2 + 1][col1] - self.prefix[row1][col1]
-        right = self.prefix[row2 + 1][col2 + 1] - self.prefix[row1][col2 + 1]
-
-        return right - left
+        return (self.prefix[row2 + 1][col2 + 1] - self.prefix[row1][col2 + 1]) - (self.prefix[row2 + 1][col1] - self.prefix[row1][col1])
 
 
 # Your NumMatrix object will be instantiated and called as such:
