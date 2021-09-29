b36fb3c5d0145dfe7f4a7368a70b4ab7b4e3e4ab
hwennnn <hwendev@gmail.com>
2021-09-29 14:26:23 +0000

    Sync LeetCode submission - Unique Paths (python3)

--- problems/unique_paths/solution.py
+++ problems/unique_paths/solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        aux = [[1 for x in range(n)] for x in range(m)]
-        
+        aux = [[1] * n for x in range(m)]
+
         for i in range(1, m):
             for j in range(1, n):
-                aux[i][j] = aux[i][j-1]+aux[i-1][j]
+                aux[i][j] = aux[i][j-1] + aux[i-1][j]
         
         return aux[-1][-1]
