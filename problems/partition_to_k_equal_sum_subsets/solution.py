a887d40efed64bad74d61229d333691ddc5cac11
hwennnn <hwendev@gmail.com>
2021-09-30 11:18:05 +0000

    Sync LeetCode submission - Partition to K Equal Sum Subsets (python3)

--- problems/partition_to_k_equal_sum_subsets/solution.py
+++ problems/partition_to_k_equal_sum_subsets/solution.py
@@ -3,27 +3,22 @@ class Solution:
         n = len(nums)
         total = sum(nums)
         if total % k != 0: return False
-        
         target = total // k
-        nums.sort(reverse = 1)
         
         @cache
-        def go(mask):
-            curr = 0
-            
-            for i in range(n):
-                if (mask >> i) & 1 > 0:
-                    curr += nums[i]
-            
-            done, side = divmod(curr, target)
-            
+        def dfs(mask, curr, done):
             if done == k - 1: return True
-            
-            for i in range(n):
-                if (mask & (1 << i)) == 0:
-                    if side + nums[i] <= target and go(mask | (1 << i)):
-                        return True
+            if mask == 0: return False
+        
+            for j in range(n):
+                if mask >> j & 1:
+                    if nums[j] + curr == target:
+                        if dfs(mask ^ (1 << j), 0, done + 1):
+                            return True
+                    elif nums[j] + curr < target:
+                        if dfs(mask ^ (1 << j), curr + nums[j], done):
+                            return True
             
             return False
         
-        return go(0)
+        return dfs((1 << n) - 1, 0, 0)
