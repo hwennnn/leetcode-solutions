783f27f3c7206badfe88ff1ffef62f43963db67c
hwennnn <hwendev@gmail.com>
2021-10-01 05:45:39 +0000

    Sync LeetCode submission - Longest Palindromic Subsequence (python3)

--- problems/longest_palindromic_subsequence/solution.py
+++ problems/longest_palindromic_subsequence/solution.py
@@ -1,15 +1,11 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        n = len(s)
-        dp = [[0]*n for _ in range(n)]
         
-        return self.helperR(0, n-1, s, dp)
-
-    def helperR(self, i, j, s, dp):
-        if i == j: return 1
-        if i > j: return 0
-        if dp[i][j]: return dp[i][j]
+        @cache
+        def go(i, j):
+            if i == j: return 1
+            if i > j: return 0
+            
+            return 2 + go(i + 1, j - 1) if s[i] == s[j] else max(go(i + 1, j), go(i, j - 1))
         
-        dp[i][j] = 2 + self.helperR(i+1, j-1, s, dp) if s[i] == s[j] else max(self.helperR(i+1, j, s, dp), self.helperR(i, j-1, s, dp))
-        
-        return dp[i][j]
+        return go(0, len(s) - 1)
