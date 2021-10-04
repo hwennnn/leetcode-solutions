9224d9adcb72b77a62b2efb46477dc9e8a98826a
hwennnn <hwendev@gmail.com>
2021-10-04 12:31:04 +0000

    Sync LeetCode submission - Island Perimeter (python3)

--- problems/island_perimeter/solution.py
+++ problems/island_perimeter/solution.py
@@ -1,30 +1,20 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
+        visited = set()
         
-        def countPerimeter(x, y):
-            top = 1 if y == 0 or (y - 1 >= 0 and grid[x][y - 1]) == 0 else 0
-            left = 1 if x == 0 or (x - 1 >= 0 and grid[x - 1][y] == 0) else 0
-            right = 1 if x == rows - 1 or (x + 1 < rows and grid[x + 1][y] == 0) else 0
-            bottom = 1 if y == cols - 1 or (y + 1 < cols and grid[x][y + 1] == 0) else 0
+        def go(x, y):
+            if not (0 <= x < rows and 0 <= y < cols) or grid[x][y] == 0: return 1
+            
+            visited.add((x, y))
+            count = 0
+            for dx, dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+                if (dx, dy) not in visited:
+                    count += go(dx, dy)
 
-            return top + left + right + bottom
-        
-        def dfs(x, y):
-            count = countPerimeter(x, y)
-            grid[x][y] = -1
-            
-            for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
-                if 0 <= dx < rows and 0 <= dy < cols and grid[dx][dy] == 1:
-                    count += dfs(dx, dy)
-            
             return count
         
-        res = float('-inf')
-        
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 1:
-                    res = max(res, dfs(i, j))
-        
-        return res
+        for x in range(rows):
+            for y in range(cols):
+                if grid[x][y] == 1:
+                    return go(x, y)
