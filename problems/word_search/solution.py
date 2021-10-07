0486a6e747ee5972d0ab961902291bac8917abee
hwennnn <hwendev@gmail.com>
2021-10-07 01:06:46 +0000

    Sync LeetCode submission - Word Search (python3)

--- problems/word_search/solution.py
+++ problems/word_search/solution.py
@@ -10,10 +10,9 @@ class Solution:
             for dx,dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
                 if 0 <= dx < rows and 0 <= dy < cols and board[dx][dy] == word[i] and (dx, dy) not in visited:
                     visited.add((dx, dy))
-                    result = backtrack(dx, dy, i + 1, curr + board[dx][dy], visited)
+                    if backtrack(dx, dy, i + 1, curr + board[dx][dy], visited):
+                        return True
                     visited.remove((dx, dy))
-                    
-                    if result: return True
             
             return False
         
