f1a45a74fac81deff1f90941f32c5e56b2998fab
hwennnn <hwendev@gmail.com>
2021-10-15 13:41:06 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock with Cooldown (python3)

--- problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.py
+++ problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.py
@@ -1,20 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
         n = len(prices)
-        if n < 2: return 0
-        s0 = []
-        s1 = []
-        s2 = []
-        s0.append(0)
-        s1.append(-prices[0])
-        s2.append(float('-inf'))
         
-        for i in range(1,n):
+        s0 = [0]
+        s1 = [-prices[0]]
+        s2 = [float('-inf')]
+        
+        for i in range(1, n):
             s0.append(max(s0[i - 1], s2[i - 1]))
             s1.append(max(s1[i - 1], s0[i - 1] - prices[i]))
             s2.append(prices[i] + s1[i - 1])
-            
         
-        
-        return max(s0[n - 1], s2[n - 1])
+        return max(s0[-1], s2[-1])
