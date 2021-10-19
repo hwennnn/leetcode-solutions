329399d7fbd50a5226da41ea9a0dfbd917f54031
hwennnn <hwendev@gmail.com>
2021-10-19 05:09:03 +0000

    Sync LeetCode submission - Maximum Sum of Two Non-Overlapping Subarrays (python3)

--- problems/maximum_sum_of_two_non-overlapping_subarrays/solution.py
+++ problems/maximum_sum_of_two_non-overlapping_subarrays/solution.py
@@ -1,19 +1,18 @@
 class Solution:
-    def maxSumTwoNoOverlap(self, nums: List[int], firstLen: int, secondLen: int) -> int:
-        n = len(nums)
-        prefix = [0] + list(accumulate(nums))
-        res = 0
-        
-        for i in range(firstLen, n + 1):
-            first = prefix[i] - prefix[i - firstLen]
-            second = 0
-            
-            for j in range(secondLen, i - firstLen + 1):
-                second = max(second, prefix[j] - prefix[j - secondLen])
-            
-            for j in range(secondLen + i + 1, n + 1):
-                second = max(second, prefix[j] - prefix[j - secondLen])
+    def maxSumTwoNoOverlap(self, A: List[int], L: int, M: int) -> int:
+        for i in range(1, len(A)):
+            A[i] += A[i - 1]
 
-            res = max(res, first + second)
+        res, Lmax, Mmax = A[L + M - 1], A[L - 1], A[M - 1]
         
+        # window  | --- L --- | --- M --- |
+        for i in range(L + M, len(A)):
+            Lmax = max(Lmax, A[i - M] - A[i - L - M])
+            res = max(res, Lmax + A[i] - A[i - M])
+            
+        # window  | --- M --- | --- L --- |
+        for i in range(L + M, len(A)):
+            Mmax = max(Mmax, A[i - L] - A[i - L - M])
+            res = max(res, Mmax + A[i] - A[i - L])
+
         return res
