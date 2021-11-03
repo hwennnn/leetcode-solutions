0ec524ed1a1b5c1ccb5e8c07b5d0f5b1c3ac4f9a
hwennnn <hwendev@gmail.com>
2021-11-03 13:58:22 +0000

    Sync LeetCode submission - Repeated DNA Sequences (python3)

--- problems/repeated_dna_sequences/solution.py
+++ problems/repeated_dna_sequences/solution.py
@@ -1,21 +1,21 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        mp = collections.defaultdict(int)
-        n = len(s)
-        if n < 10: return []
-        word = s[:10]
-        mp[word] += 1
-        word = deque(word)
+        mp = {"A": 1, "C": 2, "G": 3, "T": 4}
+        q = (1 << 31) - 1
+        h = (pow(4, 9)) % q
+        seen, res = set(), set()
+        t = 0
         
-        for i in range(10, n):
-            word.popleft()
-            word += s[i]
-            
-            mp["".join(word)] += 1
+        for i, x  in enumerate(s):
+            if i >= 10:
+                t -= (mp[s[i - 10]] * h)
+            t = (4 * t + mp[x]) % q
+
+            if i >= 9 and t in seen:
+                res.add(s[i - 9: i + 1])
+            else:
+                seen.add(t)
         
-        res = []
-        for word, count in mp.items():
-            if count > 1:
-                res.append(word)
+        return list(res)
         
-        return res
+        
