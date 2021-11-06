a009ceb0bc565ec721bb6826eed55f6b921304b4
hwennnn <hwendev@gmail.com>
2021-11-06 08:33:51 +0000

    Sync LeetCode submission - Arranging Coins (python3)

--- problems/arranging_coins/solution.py
+++ problems/arranging_coins/solution.py
@@ -1,18 +1,4 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        
-        stack = 0
-        i = 0
-        
-        while n > stack:
-            stack = stack + 1
-            i += 1
-            n -= stack
-            
-        return i
-            
-            
-            
-
-        
-        
+        return (int) (sqrt(1 + (8 * n)) - 1) // 2
+    
