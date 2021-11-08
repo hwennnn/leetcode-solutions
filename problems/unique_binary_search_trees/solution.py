8340a5b50844349dae4df344ec566320f9df40b5
hwennnn <hwendev@gmail.com>
2021-11-08 04:23:25 +0000

    Sync LeetCode submission - Unique Binary Search Trees (python3)

--- problems/unique_binary_search_trees/solution.py
+++ problems/unique_binary_search_trees/solution.py
@@ -1,11 +1,15 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-
-        dp = [0] * (n + 1)
-        dp[0] = dp[1] = 1
         
-        for i in range(2, n + 1):
-            for j in range(1, i + 1):
-                dp[i] += dp[j - 1] * dp[i - j]
+        @cache
+        def go(n):
+            if n == 0: return 0
+            if n == 1: return 1
+            
+            res = 0
+            for head in range(n):
+                res += max(1, go(head)) * max(1, go(n - head - 1))
+            
+            return res
         
-        return dp[-1]
+        return go(n)
