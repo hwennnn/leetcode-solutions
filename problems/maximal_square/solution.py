639302fdb97f35b936f9049e2f6c8c69c004d872
hwennnn <hwendev@gmail.com>
2021-11-30 03:41:51 +0000

    Sync LeetCode submission - Maximal Square (python3)

--- problems/maximal_square/solution.py
+++ problems/maximal_square/solution.py
@@ -1,22 +1,23 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
+        res = 0
         rows, cols = len(matrix), len(matrix[0])
         dp = [[0] * cols for _ in range(rows)]
-        res = 0
         
-        for i in range(rows):
-            for j in range(cols):
-                if matrix[i][j] == '1':
-                    dp[i][j] = 1
+        for x in range(rows):
+            for y in range(cols):
+                if matrix[x][y] == "1":
+                    dp[x][y] = 1
                     res = 1
-
-        for i in range(1, rows):
-            for j in range(1, cols):
-                if matrix[i][j] == '1':
-                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+        
+        for x in range(1, rows):
+            for y in range(1, cols):
+                if matrix[x][y] == "1":
+                    z = 1 + min(dp[x][y - 1], dp[x - 1][y - 1], dp[x - 1][y])
+                    dp[x][y] = z 
+                    res = max(res, z * z)
                 else:
-                    dp[i][j] = 0
-
-                res = max(res, dp[i][j])
-            
-        return res ** 2
+                    dp[x][y] = 0
+        
+        return res
+        
