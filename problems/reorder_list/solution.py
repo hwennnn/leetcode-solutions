2f307122346ada213b9911711eeca9e5eb72fb2f
hwennnn <hwendev@gmail.com>
2021-12-22 16:19:33 +0000

    Sync LeetCode submission - Reorder List (python3)

--- problems/reorder_list/solution.py
+++ problems/reorder_list/solution.py
@@ -4,43 +4,35 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseLL(self, head):
-        prev = None
-
-        while head:
-            nxt = head.next
-            head.next = prev
-            prev = head
-            head = nxt
-
-        return prev
-    
-    def mergeLL(self, l1, l2):
-        while l1:
-            n1, n2 = l1.next, l2.next
-            l1.next = l2
-            
-            if not n1: break
-            
-            l2.next = n1
-            l1 = n1
-            l2 = n2
-            
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head or not head.next: return head
+        p1, p2 = head, head.next
+        while p2 and p2.next:
+            p1 = p1.next
+            p2 = p2.next.next
         
-        prev, slow, fast, l1 = None, head, head, head
-        
-        while fast and fast.next:
-            prev = slow
-            slow = slow.next
-            fast = fast.next.next
-        
-        prev.next = None
-        
-        l2 = self.reverseLL(slow)
+        def reverseLL(curr):
+            prev = None
 
-        self.mergeLL(l1, l2)
+            while curr:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+            
+            return prev
+        
+        head2 = reverseLL(p1.next)
+        p1.next = None
+        
+        while head and head2:
+            tmp1 = head.next
+            tmp2 = head2.next
+            head2.next = head.next
+            head.next = head2
+            head = tmp1
+            head2 = tmp2
+        
+        
