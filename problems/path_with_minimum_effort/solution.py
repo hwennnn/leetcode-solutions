9f744c8fe9b02e159645fd3c7236d57e6efa05e4
hwennnn <hwendev@gmail.com>
2021-12-30 15:30:30 +0000

    Sync LeetCode submission - Path With Minimum Effort (python3)

--- problems/path_with_minimum_effort/solution.py
+++ problems/path_with_minimum_effort/solution.py
@@ -3,31 +3,30 @@ class Solution:
         rows, cols = len(heights), len(heights[0])
         
         def good(k):
-            visited = set()
+            queue = collections.deque([(0, 0)])
+            visited = set([(0, 0)])
             
-            def dfs(x, y):
-                if x == rows - 1 and y == cols - 1: return True
+            while queue:
+                x, y = queue.popleft()
                 
-                if 0 <= x < rows and 0 <= y < cols:
-                    for dx,dy in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
-                        if 0 <= dx < rows and 0 <= dy < cols and (dx,dy) not in visited and abs(heights[x][y] - heights[dx][dy]) <= k:
-                            visited.add((dx,dy))
-                            if dfs(dx,dy): 
-                                return True
-                    
-                return False
+                if x == rows - 1 and y == cols - 1:
+                    return True
+                
+                for dx, dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+                    if 0 <= dx < rows and 0 <= dy < cols and abs(heights[dx][dy] - heights[x][y]) <= k and (dx, dy) not in visited:
+                        visited.add((dx, dy))
+                        queue.append((dx, dy))
             
-            return dfs(0,0)
-                            
-            
+            return False
         
-        left, right = 0, 1000000
-        
+        left, right = 0, 10 ** 6
+
         while left < right:
-            mid = left + (right-left) // 2
+            mid = left + (right - left) // 2
+            
             if good(mid):
                 right = mid
             else:
                 left = mid + 1
-            
+        
         return left
