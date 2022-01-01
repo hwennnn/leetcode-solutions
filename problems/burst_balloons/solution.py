2ff76b49bb7b7e3c36a54ed9097c3a02ec77f147
hwennnn <hwendev@gmail.com>
2022-01-01 09:18:05 +0000

    Sync LeetCode submission - Burst Balloons (python3)

--- problems/burst_balloons/solution.py
+++ problems/burst_balloons/solution.py
@@ -1,13 +1,14 @@
 class Solution:
-    def maxCoins(self, nums):
-        nums = [1] + [i for i in nums if i > 0] + [1]
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
         n = len(nums)
         dp = [[0] * n for _ in range(n)]
-
+        
         for k in range(2, n):
             for left in range(n - k):
                 right = left + k
-                for i in range(left + 1,right):
+                
+                for i in range(left + 1, right):
                     dp[left][right] = max(dp[left][right], nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right])
-                    
+        
         return dp[0][n - 1]
