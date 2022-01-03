ae925446c2b31fa2b4ec22676e295014b29b79f9
hwennnn <hwendev@gmail.com>
2022-01-03 02:57:31 +0000

    Sync LeetCode submission - Find the Town Judge (python3)

--- problems/find_the_town_judge/solution.py
+++ problems/find_the_town_judge/solution.py
@@ -1,13 +1,14 @@
 class Solution:
-    def findJudge(self, N: int, trust: List[List[int]]) -> int:
-        mp = [0] * (N+2)
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        trusted = [0] * (n + 1)
+        trust_other = [0] * (n + 1)
         
-        for a,b in trust:
-            mp[a] -= 1
-            mp[b] += 1
+        for u, v in trust:
+            trusted[v] += 1
+            trust_other[u] += 1
         
-        for i in range(1, N+1):
-            if mp[i] == N - 1: return i
+        for i in range(1, n + 1):
+            if trusted[i] == n - 1 and trust_other[i] == 0:
+                return i
         
         return -1
-            
