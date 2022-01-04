997291ce3b1363d8a77b025cac3b2bd0ea36e7d9
hwennnn <hwendev@gmail.com>
2022-01-04 07:21:02 +0000

    Sync LeetCode submission - Minimum XOR Sum of Two Arrays (python3)

--- problems/minimum_xor_sum_of_two_arrays/solution.py
+++ problems/minimum_xor_sum_of_two_arrays/solution.py
@@ -1,18 +1,17 @@
 class Solution:
     def minimumXORSum(self, nums1: List[int], nums2: List[int]) -> int:
-        n = len(nums2)
+        n = len(nums1)
         
         @cache
-        def go(mask):
-            i = bin(mask).count('1')
-            if i >= len(nums1): return 0
+        def dp(i, mask):
+            if i == n: return 0
             
-            count = float('inf')
+            res = float('inf')
             
             for j in range(n):
-                if mask & (1 << j) == 0:
-                    count = min(count, (nums1[i] ^ nums2[j]) + go(mask + (1 << j)))
-            
-            return count
+                if mask & (1 << j) > 0:
+                    res = min(res, dp(i + 1, mask ^ (1 << j)) + (nums1[i] ^ nums2[j]))
+                
+            return res
         
-        return go(0)
+        return dp(0, (1 << n) - 1)
