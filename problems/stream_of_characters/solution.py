9690ab9947ec2ea3bba63a0a0e534c8d5ee75d1a
hwennnn <hwendev@gmail.com>
2022-01-10 15:17:39 +0000

    Sync LeetCode submission - Stream of Characters (python3)

--- problems/stream_of_characters/solution.py
+++ problems/stream_of_characters/solution.py
@@ -1,43 +1,62 @@
-class TrieNode:
-    def __init__(self):
-        self.children = collections.defaultdict(TrieNode)
-        self.hasWord = False
-
+# ----------------------------------- Trie -----------------------------------
 class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-    
-    def insert(self, word):
-        curr = self.root
-        for c in word[::-1]:
-            curr = curr.children[c]
-        
-        curr.hasWord = True
-    
-    def hasWord(self, word):
-        curr = self.root
-        
-        for c in word[::-1]:
-            if c not in curr.children: return False
-            curr = curr.children[c]
-            if curr.hasWord:
-                return True
-        
-        return False
+    # https://github.com/cheran-senthil/PyRival/blob/master/pyrival/data_structures/Trie.py
+    def __init__(self, *words):
+        self.root = dict()
+        for word in words:
+            self.add(word)
+
+    def add(self, word):
+        current_dict = self.root
+        for letter in word:
+            current_dict = current_dict.setdefault(letter, dict())
+        current_dict["_end_"] = True
+
+    def __contains__(self, word):
+        exist = False
+        current_dict = self.root
+        for letter in word:
+            if letter not in current_dict:
+                break
+            current_dict = current_dict[letter]
+            exist |= "_end_" in current_dict
+        return exist
+
+    def delete(self, word, prune=False):
+        current_dict = self.root
+        nodes = [current_dict]
+        objects = []
+        for letter in word:
+            current_dict = current_dict[letter]
+            nodes.append(current_dict)
+            objects.append(current_dict)
+
+        del current_dict["_end_"]
+
+        if prune:
+            # https://leetcode.com/problems/maximum-genetic-difference-query/discuss/1344900/
+            for c, obj in zip(word[::-1], objects[:-1][::-1]):
+                if not obj[c]:
+                    del obj[c]
+                else:
+                    break
+
+        # assert word not in self  # confirm that the number has been removed
+
 
 class StreamChecker:
 
     def __init__(self, words: List[str]):
         self.trie = Trie()
-        self.q = ""
-        
         for word in words:
-            self.trie.insert(word)
+            self.trie.add(word[::-1])
+        self.curr = ""
 
     def query(self, letter: str) -> bool:
-        self.q += letter
+        self.curr = letter + self.curr
         
-        return self.trie.hasWord(self.q)
+        return self.curr in self.trie
+
 
 # Your StreamChecker object will be instantiated and called as such:
 # obj = StreamChecker(words)
