bb8163db799a8d8138368972b1e508d3ccd080fd
hwennnn <hwendev@gmail.com>
2022-01-10 06:44:55 +0000

    Sync LeetCode submission - Add Binary (python3)

--- problems/add_binary/solution.py
+++ problems/add_binary/solution.py
@@ -1,17 +1,14 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        i = len(a) - 1
-        j = len(b) - 1
         carry = 0
         res = []
         
-        while (i >= 0 or j >= 0 or carry):
-            s = carry
-            s += int(a[i]) if i >= 0 else 0
-            s += int(b[j]) if j >= 0 else 0
-            res.insert(0, str(s%2))
-            carry = s//2
-            i -= 1
-            j -= 1
+        for x, y in zip_longest(a[::-1], b[::-1], fillvalue = "0"):
+            curr = carry + int(x) + int(y)
+            carry, value = divmod(curr, 2)
+            res.append(str(value))
             
-        return "".join(res)
+        if carry:
+            res.append("1")
+        
+        return "".join(res)[::-1]
