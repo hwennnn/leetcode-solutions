4f5478e4932d312376fbdd66abcc22b2bda657e9
hwennnn <hwendev@gmail.com>
2022-01-18 04:25:15 +0000

    Sync LeetCode submission - Can Place Flowers (python3)

--- problems/can_place_flowers/solution.py
+++ problems/can_place_flowers/solution.py
@@ -1,22 +1,16 @@
 class Solution:
-    def canPlaceFlowers(self, arr: List[int], k: int) -> bool:
-        n = len(arr)
+    def canPlaceFlowers(self, flowerbed: List[int], k: int) -> bool:
+        n = len(flowerbed)
+        put = i = 0
         
-        for i in range(n):
-            if arr[i] == 0:
-                if i == 0 and i+1 < n and arr[i+1] == 0:
-                    arr[i] += 1
-                    k -= 1
-                    continue
-
-                if i == n-1 and arr[i-1] == 0:
-                    arr[i] += 1
-                    k -= 1
-                    continue
-
-                if 0 < i < n - 1 and arr[i] == arr[i-1] + arr[i+1]:
-                    arr[i] += 1
-                    k -= 1
+        while i < n:
+            if flowerbed[i] == 1:
+                i += 2
+            else:
+                if i == n - 1 or (i + 1 < n and flowerbed[i + 1] == 0):
+                    put += 1
+                    i += 2
+                else:
+                    i += 1
         
-        return k <= 0
-                
+        return put >= k
