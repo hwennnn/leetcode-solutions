76f149453fdae57354029e633e943fc75c604c8f
hwennnn <hwendev@gmail.com>
2022-01-31 02:25:31 +0000

    Sync LeetCode submission - Richest Customer Wealth (python3)

--- problems/richest_customer_wealth/solution.py
+++ problems/richest_customer_wealth/solution.py
@@ -1,8 +1,3 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        res = float('-inf')
-        
-        for a in accounts:
-            res = max(res, sum(a))
-        
-        return res
+        return max(sum(A) for A in accounts)
