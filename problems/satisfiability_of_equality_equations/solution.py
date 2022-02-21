d60a376ffbad28abe80ac0409f6cb82667396f7b
hwennnn <hwendev@gmail.com>
2022-02-21 06:26:27 +0000

    Sync LeetCode submission - Satisfiability of Equality Equations (python3)

--- problems/satisfiability_of_equality_equations/solution.py
+++ problems/satisfiability_of_equality_equations/solution.py
@@ -1,33 +1,19 @@
-class UnionFind:
-    def __init__(self):
-        self._parent = {}
-        self._size = {}
-
-    def union(self, a, b):
-        a, b = self.find(a), self.find(b)
-        if a == b:
-            return
-        if self._size[a] < self._size[b]:
-            a, b = b, a
-        self._parent[b] = a
-        self._size[a] += self._size[b]
-
-    def find(self, x):
-        if x not in self._parent:
-            self._parent[x] = x
-            self._size[x] = 1
-        while self._parent[x] != x:
-            self._parent[x] = self._parent[self._parent[x]]
-            x = self._parent[x]
-        return x
-
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-        N = len(equations)
-        uf = UnionFind()
-
-        for x, e, _ , y in equations:
-            if e == "=":
-                uf.union(x, y)
         
-        return not any(e == "!" and uf.find(x) == uf.find(y) for x, e, _, y in equations)
+        def find(x):
+            if u[x] != x:
+                u[x] = find(u[x])
+            
+            return u[x]
+        
+        def union(x, y):
+            u[find(u[x])] = find(u[y])
+        
+        u = {x : x for x in string.ascii_lowercase}
+        
+        for a, e, _, b in equations:
+            if e == "=":
+                union(a, b)
+        
+        return not any(e == "!" and find(a) == find(b) for a, e, _, b in equations)
