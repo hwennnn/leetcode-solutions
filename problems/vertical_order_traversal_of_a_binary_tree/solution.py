3829b61d8e7571ad916c588bed9b3e78b787bdf2
hwennnn <hwendev@gmail.com>
2022-02-21 08:29:33 +0000

    Sync LeetCode submission - Vertical Order Traversal of a Binary Tree (python3)

--- problems/vertical_order_traversal_of_a_binary_tree/solution.py
+++ problems/vertical_order_traversal_of_a_binary_tree/solution.py
@@ -5,29 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def verticalTraversal(self, root: TreeNode) -> List[List[int]]:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         mp = collections.defaultdict(list)
         
-        deq = collections.deque([(root, (0,0))])
+        queue = deque([(root, 0, 0)])
         
-        while deq:
-            l = len(deq)
+        while queue:
+            node, row, col = queue.popleft()
             
-            while l > 0:
-                node, (x, y) = deq.popleft()
-                mp[x].append((y, node.val))
-                
-                if node.left:
-                    deq.append((node.left, (x-1, y-1)))
-                
-                if node.right:
-                    deq.append((node.right, (x+1, y-1)))
-                    
-                l -= 1
+            mp[col].append((row, node.val))
+            
+            if node.left:
+                queue.append((node.left, row + 1, col - 1))
+            
+            if node.right:
+                queue.append((node.right, row + 1, col + 1))
         
+        n = len(mp)
         res = []
-        for k in sorted(mp):
-            values = [v[1] for v in sorted(mp[k], key = lambda x:(-x[0], x[1]))]
-            res.append(values)
+        keys = sorted(mp.keys())
+        
+        for key in keys:
+            res.append(col for _, col in sorted(mp[key], key = lambda x : (x[0], x[1])))
         
         return res
