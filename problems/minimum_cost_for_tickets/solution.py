57d33a4d8c88e0c6ca14513c4e2e58fc2bf367bd
hwennnn <hwendev@gmail.com>
2022-02-21 12:26:41 +0000

    Sync LeetCode submission - Minimum Cost For Tickets (python3)

--- problems/minimum_cost_for_tickets/solution.py
+++ problems/minimum_cost_for_tickets/solution.py
@@ -1,15 +1,19 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-
-        dp=[0 for i in range(days[-1]+1)]
+        res = 0
+        last7 = deque()
+        last30 = deque()
         
-        for i in range(days[-1]+1):
-            if i not in days:
-                dp[i]=dp[i-1]
-
-            else:
-                dp[i]=min(dp[max(0,i-7)]+costs[1],dp[max(0,i-1)]+costs[0],dp[max(0,i-30)]+costs[2])
-                
+        for day in days:
+            while last7 and day >= last7[0][0]:
+                last7.popleft()
+            
+            while last30 and day >= last30[0][0]:
+                last30.popleft()
+            
+            last7.append((day + 7, res + costs[1]))
+            last30.append((day + 30, res + costs[2]))
+            
+            res = min(res + costs[0], last7[0][1], last30[0][1])
         
-        return dp[-1]
-
+        return res
