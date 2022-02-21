20e619f93d5c9ef39a4b4351e05e0d99ff51a9e8
hwennnn <hwendev@gmail.com>
2022-02-21 07:52:49 +0000

    Sync LeetCode submission - Sum of Even Numbers After Queries (python3)

--- problems/sum_of_even_numbers_after_queries/solution.py
+++ problems/sum_of_even_numbers_after_queries/solution.py
@@ -1,23 +1,30 @@
 class Solution:
     def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
-        N = len(nums)
-        total = sum(x for x in nums if x % 2 == 0)
+        odd = even = 0
         res = []
         
-        for val, index in queries:
-            x = nums[index]
-            newNum = x + val
-            nums[index] = newNum
-            
+        for x in nums:
             if x % 2 == 0:
-                if newNum % 2 == 0:
-                    total += newNum - x
-                else:
-                    total -= x
+                even += x
             else:
-                if newNum % 2 == 0:
-                    total += newNum
+                odd += x
+        
+        for val, index in queries:
+            updated = nums[index] + val
+            if nums[index] % 2 == 0:
+                if updated % 2 == 0:
+                    even += val
+                else:
+                    even -= nums[index]
+                    odd += updated
+            else:
+                if updated % 2 == 0:
+                    odd -= nums[index]
+                    even += updated
+                else:
+                    odd += val
             
-            res.append(total)
+            res.append(even)
+            nums[index] = updated
         
         return res
