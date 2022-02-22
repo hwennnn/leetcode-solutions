afbbf5c6aa2da3f0a362dd4a10b514011b836b83
hwennnn <hwendev@gmail.com>
2022-02-22 13:43:07 +0000

    Sync LeetCode submission - Numbers With Same Consecutive Differences (python3)

--- problems/numbers_with_same_consecutive_differences/solution.py
+++ problems/numbers_with_same_consecutive_differences/solution.py
@@ -1,17 +1,21 @@
 class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
-        curr = range(1, 10)
+        res = set()
+        queue = deque([(x, 1) for x in range(1, 10)])
         
-        for _ in range(n - 1):
-            temp = set()
-            for x in curr:
-                y = x % 10
-                if y + k < 10:
-                    temp.add(x * 10 + y + k)
-                
-                if y - k >= 0:
-                    temp.add(x * 10 + y - k)
+        while queue:
+            x, length = queue.popleft()
             
-            curr = temp
+            if length == n:
+                res.add(x)
+                continue
             
-        return list(curr)
+            last = x % 10
+            
+            if last + k < 10:
+                queue.append((x * 10 + last + k, length + 1))
+            
+            if last - k >= 0:
+                queue.append((x * 10 + last - k, length + 1))
+        
+        return list(res)
