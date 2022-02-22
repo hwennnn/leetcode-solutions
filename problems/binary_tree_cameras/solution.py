873a4fc322d81ec94685719edb936657766a8e81
hwennnn <hwendev@gmail.com>
2022-02-22 15:50:31 +0000

    Sync LeetCode submission - Binary Tree Cameras (python3)

--- problems/binary_tree_cameras/solution.py
+++ problems/binary_tree_cameras/solution.py
@@ -5,16 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minCameraCover(self, root: TreeNode) -> int:
-        self.res = 0
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        res = 0
         
-        def dfs(node):
+        def go(node):
+            nonlocal res
+            
             if not node: return 2
-            left, right = dfs(node.left), dfs(node.right)
+            
+            left, right = go(node.left), go(node.right)
+            
             if left == 0 or right == 0:
-                self.res += 1
+                res += 1
                 return 1
             
             return 2 if left == 1 or right == 1 else 0
-            
-        return (dfs(root) == 0) + self.res
+        
+        return (go(root) == 0) + res
