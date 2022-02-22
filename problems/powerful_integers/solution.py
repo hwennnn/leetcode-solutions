fbc74b3b899e8136e17918b1dd10f94d3a4e6cb9
hwennnn <hwendev@gmail.com>
2022-02-22 12:04:38 +0000

    Sync LeetCode submission - Powerful Integers (python3)

--- problems/powerful_integers/solution.py
+++ problems/powerful_integers/solution.py
@@ -2,15 +2,10 @@ class Solution:
     def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
         res = set()
         
-        i = 1
-        while i < bound:
-            j = 1
-            while i + j <= bound:
-                res.add(i + j)
-                if y == 1: break
-                j *= y
-            
-            if x == 1: break
-            i *= x
+        for i in range(100):
+            for j in range(100):
+                z = x ** i + y ** j
+                if z <= bound:
+                    res.add(z)
         
         return list(res)
