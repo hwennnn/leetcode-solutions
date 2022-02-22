55297f47b0e30df7ba8c387f98d834898bff8d97
hwennnn <hwendev@gmail.com>
2022-02-22 16:01:07 +0000

    Sync LeetCode submission - N-Repeated Element in Size 2N Array (python3)

--- problems/n-repeated_element_in_size_2n_array/solution.py
+++ problems/n-repeated_element_in_size_2n_array/solution.py
@@ -1,4 +1,10 @@
 class Solution:
-    def repeatedNTimes(self, A: List[int]) -> int:
-        B = set(A)
-        return (sum(A) - sum(B)) // (len(A) - len(B))
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        m = len(nums)
+        n = m // 2
+        
+        count = Counter(nums)
+        
+        for k, v in count.items():
+            if v == n:
+                return k
