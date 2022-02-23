c00d5c4a8295563f356f1d4e5da9b53d46ab3a72
hwennnn <hwendev@gmail.com>
2022-02-23 16:43:05 +0000

    Sync LeetCode submission - Largest Component Size by Common Factor (python3)

--- problems/largest_component_size_by_common_factor/solution.py
+++ problems/largest_component_size_by_common_factor/solution.py
@@ -1,40 +1,38 @@
 class DSU:
-    def __init__(self, N):
-        self.A = list(range(N))
-    
+    def __init__(self, n):
+        self.graph = list(range(n))
+
     def find(self, x):
-        if x != self.A[x]:
-            self.A[x] = self.find(self.A[x])
-        
-        return self.A[x]
-    
+        if self.graph[x] != x:
+            self.graph[x] = self.find(self.graph[x])
+
+        return self.graph[x]
+
     def union(self, x, y):
         ux, uy = self.find(x), self.find(y)
-        self.A[ux] = uy
+        self.graph[ux] = uy
 
 class Solution:
     def largestComponentSize(self, nums: List[int]) -> int:
         n = len(nums)
-        primes = collections.defaultdict(list)
         dsu = DSU(n)
+        primes = defaultdict(list)
         
-        def getPrimes(x):
+        def getPrimesSet(x):
             for i in range(2, int(math.sqrt(x)) + 1):
                 if x % i == 0:
-                    return getPrimes(x // i) | set([i])
+                    return getPrimesSet(x // i) | set([i])
             
             return set([x])
         
         for i, x in enumerate(nums):
-            primes_set = getPrimes(x)
-            for q in primes_set:
-                primes[q].append(i)
+            p = getPrimesSet(x)
+            for prime in p:
+                primes[prime].append(i)
         
-        for _, v in primes.items():
-            for i in range(len(v) - 1):
-                dsu.union(v[i], v[i + 1])
+        for indexes in primes.values():
+            for x, y in zip(indexes, indexes[1:]):
+                dsu.union(x, y)
         
-        return max(Counter([dsu.find(x) for x in range(n)]).values())
-        
-        
-        
+        return max(Counter(dsu.find(i) for i in range(n)).values())
+            
