fa364e9c574d3c6a9483133acd9302e4df9e9f78
hwennnn <hwendev@gmail.com>
2022-02-23 05:48:19 +0000

    Sync LeetCode submission - Prison Cells After N Days (python3)

--- problems/prison_cells_after_n_days/solution.py
+++ problems/prison_cells_after_n_days/solution.py
@@ -1,35 +1,46 @@
 class Solution:
-    def prisonAfterNDays(self, cells: List[int], n: int) -> List[int]:
+    def prisonAfterNDays(self, cells: List[int], days: int) -> List[int]:
         
-        def go(cells):
-            res = [0] * len(cells)
-            for i in range(1, len(cells) - 1):
-                if cells[i - 1] == cells[i + 1]:
-                    res[i] = 1
-                else:
-                    res[i] = 0
+        def g(cells):
+            masks = 0
             
-            return res
+            for cell, occupied in enumerate(cells):
+                if occupied:
+                    masks |= (1 << cell)
+            
+            return masks
         
-        visited = set()
+        def transform(cells):
+            new_cells = [0] * 8
+            
+            for i in range(1, 7):
+                if cells[i - 1] == cells[i + 1]:
+                    new_cells[i] = 1
+            
+            return new_cells
+        
+        original = cells[:]
+        seen = set()
         cycle = 0
         hasCycle = False
-        for _ in range(n):
-            nextCells = go(cells)
+        
+        for _ in range(days):
+            next_cells = transform(cells)
+            masks = g(next_cells)
             
-            if tuple(nextCells) in visited: 
+            if masks in seen:
                 hasCycle = True
                 break
             else:
-                visited.add(tuple(nextCells))
+                seen.add(masks)
                 cycle += 1
             
-            cells = nextCells
+            cells = next_cells
         
         if hasCycle:
-            count = n % cycle
-            for _ in range(count):
-                cells = go(cells)
-        
-        return cells
+            count = days % cycle
             
+            for _ in range(count):
+                cells = transform(cells)
+            
+        return cells
