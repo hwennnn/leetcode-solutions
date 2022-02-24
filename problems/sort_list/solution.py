0e7fc202bd1d2c909cea137cfac89ceaee5b19fd
hwennnn <hwendev@gmail.com>
2022-02-24 07:34:34 +0000

    Sync LeetCode submission - Sort List (python3)

--- problems/sort_list/solution.py
+++ problems/sort_list/solution.py
@@ -4,24 +4,31 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def sortList(self, head: ListNode) -> ListNode:
-        if not head: return None
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next: return head
         
-        arr = []
-        
-        while head:
-            arr.append(head.val)
-            head = head.next
+        def merge(head1, head2):
+            curr = res = ListNode(-1)
             
-        arr.sort()
-        res = node = ListNode()
+            while head1 and head2:
+                if head1.val < head2.val:
+                    curr.next = ListNode(head1.val)
+                    head1 = head1.next
+                else:
+                    curr.next = ListNode(head2.val)
+                    head2 = head2.next
+                
+                curr = curr.next
+            
+            curr.next = head1 or head2
+            
+            return res.next
         
-        for i,v in enumerate(arr):
-            tmp = node
-            tmp.val = v
-            if i != len(arr) - 1:
-                tmp.next = ListNode()
-            node = tmp
-            node = node.next
+        pre, slow, fast = None, head, head
         
-        return res
+        while fast and fast.next:
+            pre, slow, fast = slow, slow.next, fast.next.next
+        
+        pre.next = None
+        
+        return merge(self.sortList(head), self.sortList(slow))
