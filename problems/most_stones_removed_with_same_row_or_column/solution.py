f091582ae308e11b0801bf7f76a3fa94572647e0
hwennnn <hwendev@gmail.com>
2022-02-24 14:03:53 +0000

    Sync LeetCode submission - Most Stones Removed with Same Row or Column (python3)

--- problems/most_stones_removed_with_same_row_or_column/solution.py
+++ problems/most_stones_removed_with_same_row_or_column/solution.py
@@ -1,47 +1,25 @@
-class DSU:
-    def __init__(self, n):
-        self.graph = list(range(n))
-
-    def find(self, x):
-        if self.graph[x] != x:
-            self.graph[x] = self.find(self.graph[x])
-
-        return self.graph[x]
-
-    def union(self, x, y):
-        ux, uy = self.find(x), self.find(y)
-        self.graph[ux] = uy
-
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)
-
-
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
         n = len(stones)
+        visited = set()
+        res = 0
         rows = defaultdict(list)
         cols = defaultdict(list)
         
-        for i, (x, y) in enumerate(stones):
-            rows[x].append(i)
-            cols[y].append(i)
+        for x, y in stones:
+            rows[x].append((x, y))
+            cols[y].append((x, y))
         
-        dsu = DSU(n)
+        def go(x, y):
+            visited.add((x, y))
+            
+            for nx, ny in rows[x] + cols[y]:
+                if (nx, ny) not in visited:
+                    go(nx, ny)
         
-        for row in rows.values():
-            for i in range(len(row)):
-                for j in range(i + 1, len(row)):
-                    dsu.union(row[i], row[j])
+        for x, y in stones:
+            if (x, y) not in visited:
+                go(x, y)
+                res += 1
         
-        for col in cols.values():
-            for i in range(len(col)):
-                for j in range(i + 1, len(col)):
-                    dsu.union(col[i], col[j])
-        
-        parents = set()
-        
-        for node in range(n):
-            p = dsu.find(node)
-            parents.add(p)
-        
-        return n - len(parents)
+        return n - res
