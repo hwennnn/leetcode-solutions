56c01d7984e9bbe4580773f06e7a63fb64e54c7e
hwennnn <hwendev@gmail.com>
2022-02-25 06:49:17 +0000

    Sync LeetCode submission - Valid Mountain Array (python3)

--- problems/valid_mountain_array/solution.py
+++ problems/valid_mountain_array/solution.py
@@ -1,16 +1,14 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
         n = len(arr)
-        
         if n < 3: return False
         
-        i,j = 0, n-1
+        i, j = 0, n - 1
         
-        while i + 1 < n and arr[i] < arr[i+1]:
+        while i + 1 < n and arr[i] < arr[i + 1]: 
             i += 1
         
-        while j > 0 and arr[j] < arr[j-1]:
+        while j > 0 and arr[j] < arr[j - 1]:
             j -= 1
         
-        return i > 0 and i == j and j < n - 1
-        
+        return i == j and j != n - 1 and i != 0
