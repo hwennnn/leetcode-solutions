e95b8ed8f24df0eea02727f002c6dd6b05c8cdc4
hwennnn <hwendev@gmail.com>
2022-02-28 04:08:46 +0000

    Sync LeetCode submission - Flood Fill (python3)

--- problems/flood_fill/solution.py
+++ problems/flood_fill/solution.py
@@ -1,16 +1,17 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        if image[sr][sc] == newColor: return image
-        
         rows, cols = len(image), len(image[0])
-
-        def dfs(x, y, color):
+        original = image[sr][sc]
+        
+        if original == newColor: return image
+        
+        def dfs(x, y):
             image[x][y] = newColor
             
             for dx, dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-                if 0 <= dx < rows and 0 <= dy < cols and image[dx][dy] == color:
-                    dfs(dx, dy, color)
+                if 0 <= dx < rows and 0 <= dy < cols and image[dx][dy] == original:
+                    dfs(dx, dy)
         
-        dfs(sr, sc, image[sr][sc])
+        dfs(sr, sc)
         
         return image
