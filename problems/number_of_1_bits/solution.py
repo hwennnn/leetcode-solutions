e548eb577baff1586d011e2033f6473b862bbb91
hwennnn <hwendev@gmail.com>
2022-03-01 05:15:53 +0000

    Sync LeetCode submission - Number of 1 Bits (python3)

--- problems/number_of_1_bits/solution.py
+++ problems/number_of_1_bits/solution.py
@@ -3,9 +3,7 @@ class Solution:
         res = 0
         
         while n > 0:
-            if n & 1 == 1:
-                res += 1
-            
+            res += int(n & 1)
             n >>= 1
         
         return res
