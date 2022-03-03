819170949d7c5be0fad9056712956da7eb3e6c6e
hwennnn <hwendev@gmail.com>
2022-03-03 05:34:53 +0000

    Sync LeetCode submission - Can Make Arithmetic Progression From Sequence (python3)

--- problems/can_make_arithmetic_progression_from_sequence/solution.py
+++ problems/can_make_arithmetic_progression_from_sequence/solution.py
@@ -1,32 +1,11 @@
 class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
-        
         n = len(arr)
-        if n==2: return True
+        arr.sort()
+        diff = arr[1] - arr[0]
         
-        def checkAcc(arr,n):
-            diff = None
-            arr.sort()
-            for i in range(1, n):
-                count = abs(arr[i]-arr[i-1])
-                if i != 1 and diff != count:
-                    return False
-                diff = count
-
-            return True
+        for i in range(2, n):
+            d = arr[i] - arr[i - 1]
+            if d != diff: return False
         
-        def checkDesc(arr,n):
-            diff = None
-            arr.sort(reverse = True)
-            for i in range(1, n):
-                count = abs(arr[i]-arr[i-1])
-                if i != 1 and diff != count:
-                    return False
-                diff = count
-
-            return True
-        
-        return checkAcc(arr,n) or checkDesc(arr,n)
-            
-            
-            
+        return True
