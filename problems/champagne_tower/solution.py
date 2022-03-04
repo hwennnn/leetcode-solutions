aaac9fb063101a7694169203fa234347009cc54a
hwennnn <hwendev@gmail.com>
2022-03-04 06:33:56 +0000

    Sync LeetCode submission - Champagne Tower (python3)

--- problems/champagne_tower/solution.py
+++ problems/champagne_tower/solution.py
@@ -1,13 +1,15 @@
 class Solution:
-    def champagneTower(self, p, qr, qg):
-        res = [[0.0]*i for i in range(1, qr + 2)]
-        res[0][0] = p
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        A = [[0.0] * col for col in range(1, query_row + 2)]
+        A[0][0] = poured
         
-        for i in range(qr):
-            for j in range(len(res[i])):
-                if res[i][j] > 1:
-                    res[i+1][j] += (res[i][j] - 1) / 2
-                    res[i+1][j+1] += (res[i][j] - 1) / 2
-                    res[i][j] = 1
-        
-        return res[qr][qg] if res[qr][qg] < 1 else 1
+        for i in range(query_row):
+            for j in range(len(A[i])):
+                if A[i][j] > 1:
+                    half = (A[i][j] - 1) / 2
+                    A[i + 1][j] += half
+                    A[i + 1][j + 1] += half
+                    A[i][j] = 1
+                    
+        res = A[query_row][query_glass]
+        return res if res < 1 else 1
