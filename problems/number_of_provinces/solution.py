fbd6d1908dd1e2af4cfa8269ceb92cdb20b69855
hwennnn <hwendev@gmail.com>
2022-03-07 05:06:58 +0000

    Sync LeetCode submission - Number of Provinces (python3)

--- problems/number_of_provinces/solution.py
+++ problems/number_of_provinces/solution.py
@@ -1,18 +1,26 @@
 class Solution:
-    def findCircleNum(self, graphs: List[List[int]]) -> int:
-        n = len(graphs)
-        seen = set()
-        
-        def dfs(i):
-            for nei, adj in enumerate(graphs[i]):
-                if adj and nei not in seen:
-                    seen.add(nei)
-                    dfs(nei)
-        
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        graph = [[] for _ in range(n)]
+        visited = [False] * n
         res = 0
+        
+        for node, relations in enumerate(isConnected):
+            for nei, has in enumerate(relations):
+                if node == nei: continue
+                if has:
+                    graph[node].append(nei)
+        
+        def go(node):
+            visited[node] = True
+            
+            for nei in graph[node]:
+                if not visited[nei]:
+                    go(nei)
+        
         for i in range(n):
-            if i not in seen:
+            if not visited[i]:
                 res += 1
-                dfs(i)
+                go(i)
         
         return res
