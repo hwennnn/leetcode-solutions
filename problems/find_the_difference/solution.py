d6737e5832dfe592c9172a9bc297872f72bf6f79
hwennnn <hwendev@gmail.com>
2022-03-07 04:35:56 +0000

    Sync LeetCode submission - Find the Difference (python3)

--- problems/find_the_difference/solution.py
+++ problems/find_the_difference/solution.py
@@ -1,8 +1,3 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        res = 0
-        
-        for c in s+t:
-            res ^= ord(c)
-            
-        return chr(res)
+        return "".join(Counter(t) - Counter(s))
