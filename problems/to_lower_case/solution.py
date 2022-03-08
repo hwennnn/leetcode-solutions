648b35edf16e095692a82e880bc7b5d54ebb84ff
hwennnn <hwendev@gmail.com>
2022-03-08 09:01:27 +0000

    Sync LeetCode submission - To Lower Case (python3)

--- problems/to_lower_case/solution.py
+++ problems/to_lower_case/solution.py
@@ -1,13 +1,3 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        res = ''
-        
-        for c in s:
-            if ord('A') <= ord(c) <= ord('Z'):
-                v = (ord(c) - ord('A'))
-                res += chr(ord('a') + v)
-            else:
-                res += c
-
-        
-        return res
+        return s.lower()
