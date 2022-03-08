e45da769ef1f24838a7ff6ef756bdf8433e053b8
hwennnn <hwendev@gmail.com>
2022-03-08 09:08:48 +0000

    Sync LeetCode submission - Decrypt String from Alphabet to Integer Mapping (python3)

--- problems/decrypt_string_from_alphabet_to_integer_mapping/solution.py
+++ problems/decrypt_string_from_alphabet_to_integer_mapping/solution.py
@@ -1,17 +1,17 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        res = []
         n = len(s)
         i = 0
+        res = []
         
         while i < n:
-            
-            if i + 2 < n and s[i+2] == "#" and 10 <= int(s[i:i+2]) <= 26:
-                res.append(chr(ord('a') + int(s[i:i+2]) - 1))
+            if i + 2 < n and s[i + 2] == "#":
+                c = int(s[i : i + 2]) - 1
+                res.append(chr(ord('a') + c))
                 i += 3
-            
             else:
-                res.append(chr(ord('a') + int(s[i]) - 1))
+                c = int(s[i]) - 1
+                res.append(chr(ord('a') + c))
                 i += 1
         
         return "".join(res)
