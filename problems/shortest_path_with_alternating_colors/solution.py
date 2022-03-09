40630dce4896caf2aac979fb184f51303a453c4d
hwennnn <hwendev@gmail.com>
2022-03-09 14:24:12 +0000

    Sync LeetCode submission - Shortest Path with Alternating Colors (python3)

--- problems/shortest_path_with_alternating_colors/solution.py
+++ problems/shortest_path_with_alternating_colors/solution.py
@@ -1,17 +1,40 @@
 class Solution:
-    def shortestAlternatingPaths(self, n, red_edges, blue_edges):
-        G = [[[], []] for i in range(n)]
-        for i, j in red_edges: G[i][0].append(j)
-        for i, j in blue_edges: G[i][1].append(j)
-            
-        res = [[0, 0]] + [[n * 2, n * 2] for i in range(n - 1)]
-        bfs = [[0, 0], [0, 1]]
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        red, blue = defaultdict(list), defaultdict(list)
+        queue = collections.deque()
+        visited = set()
+        res = [-1] * n
+        res[0] = 0
         
-        for i, c in bfs:
-            for j in G[i][c]:
-                if res[j][c] == n * 2:
-                    res[j][c] = res[i][1 - c] + 1
-                    bfs.append([j, 1 - c])
-                    
-        return [x if x < n * 2 else -1 for x in map(min, res)]
+        for x, y in redEdges:
+            red[x].append(y)
+            if x == 0:
+                queue.append((y, 1, True))
+        
+        for x, y in blueEdges:
+            blue[x].append(y)
+            if x == 0:
+                queue.append((y, 1, False))
+        
+        while queue:
+            node, distance, isRed = queue.popleft()
+            
+            if res[node] == -1 or distance < res[node]:
+                res[node] = distance
+            
+            if isRed:
+                for nei in blue[node]:
+                    p = (nei, True)
+                    if p not in visited:
+                        visited.add(p)
+                        queue.append((nei, distance + 1, False))
+            else:
+                for nei in red[node]:
+                    p = (nei, False)
+                    if p not in visited:
+                        visited.add(p)
+                        queue.append((nei, distance + 1, True))
+        
+        return res
+        
         
