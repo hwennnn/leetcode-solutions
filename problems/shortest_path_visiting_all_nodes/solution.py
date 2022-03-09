c24a7e66aeca5ac5da6bca12df886a810ecfca4b
hwennnn <hwendev@gmail.com>
2022-03-09 13:40:08 +0000

    Sync LeetCode submission - Shortest Path Visiting All Nodes (python3)

--- problems/shortest_path_visiting_all_nodes/solution.py
+++ problems/shortest_path_visiting_all_nodes/solution.py
@@ -1,33 +1,20 @@
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
         n = len(graph)
-        visited = set()
-        queue = collections.deque()
-        full = (1 << n) - 1
-        
-        for i in range(n):
-            mask = 1 << i
-            visited.add((i, mask))
-            queue.append((i, mask))
-        
-        level = 0
+        visited = set([(i, 1 << i) for i in range(n)])
+        queue = deque([(i, 1 << i, 0) for i in range(n)])
+        full_mask = (1 << n) - 1
         
         while queue:
-            n = len(queue)
+            node, mask, count = queue.popleft()
             
-            for _ in range(n):
-                i, mask = queue.popleft()
-                
-                if mask == full: return level
-                
-                for nei in graph[i]:
-                    new_mask = mask | (1 << nei)
-                    
-                    if (nei, new_mask) not in visited:
-                        visited.add((nei, new_mask))
-                        queue.append((nei, new_mask))
+            if mask == full_mask:
+                return count
             
-            level += 1
+            for nei in graph[node]:
+                new_mask = mask | (1 << nei)
+                if (nei, new_mask) in visited: continue
+                visited.add((nei, new_mask))
+                queue.append((nei, new_mask, count + 1))
         
         return -1
-            
