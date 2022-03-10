ba7a639af21000582dbaf95c2928b0eb8a0e7924
hwennnn <hwendev@gmail.com>
2022-03-10 07:11:51 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- problems/valid_anagram/solution.py
+++ problems/valid_anagram/solution.py
@@ -1,15 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        c1 = [0] * 26
-        c2 = [0] * 26
-        
-        for c in s:
-            c1[ord(c) - ord('a')] += 1
-        
-        for c in t:
-            c2[ord(c) - ord('a')] += 1
-        
-        for i in range(26):
-            if c1[i] != c2[i]: return False
-        
-        return True
+        return Counter(s) == Counter(t)
