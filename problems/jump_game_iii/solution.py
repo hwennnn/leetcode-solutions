c8a5221d6ab58ffc44bfb6fc0f43a317baa2bca8
hwennnn <hwendev@gmail.com>
2022-03-10 07:22:55 +0000

    Sync LeetCode submission - Jump Game III (python3)

--- problems/jump_game_iii/solution.py
+++ problems/jump_game_iii/solution.py
@@ -1,21 +1,18 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-        initial = [start + arr[start], start - arr[start]]
-        visited = set(initial)
-        deq = deque(initial)
         n = len(arr)
+        queue = deque([start])
+        visited = [False] * n
+        visited[start] = True
         
-        while deq:
-            curr = deq.popleft()
+        while queue:
+            index = queue.popleft()
             
-            if not 0 <= curr < n: continue
-                
-            if arr[curr] == 0: return True
+            if arr[index] == 0: return True
             
-            for x in (curr + arr[curr], curr - arr[curr]):
-                if 0 <= x < n and x not in visited:
-                    deq.append(x)
-                    visited.add(x)
-
+            for j in [(index + arr[index]), (index - arr[index])]:
+                if 0 <= j < n and not visited[j]:
+                    visited[j] = True
+                    queue.append(j)
         
         return False
