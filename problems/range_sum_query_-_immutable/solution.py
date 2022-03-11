ad8aa9170e96fd09cdf59821aef508c7f1de4677
hwennnn <hwendev@gmail.com>
2022-03-11 05:26:57 +0000

    Sync LeetCode submission - Range Sum Query - Immutable (python3)

--- problems/range_sum_query_-_immutable/solution.py
+++ problems/range_sum_query_-_immutable/solution.py
@@ -1,10 +1,7 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.prefix = [0]
-        for x in nums:
-            self.prefix.append(self.prefix[-1] + x)
-        
+        self.prefix = [0] + list(accumulate(nums))
 
     def sumRange(self, left: int, right: int) -> int:
         return self.prefix[right + 1] - self.prefix[left]
