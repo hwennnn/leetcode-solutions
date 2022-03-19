c510eb28d09f09282ea4dfdfb02d41f605be1bb6
hwennnn <hwendev@gmail.com>
2022-03-19 04:55:05 +0000

    Sync LeetCode submission - Maximum Frequency Stack (python3)

--- problems/maximum_frequency_stack/solution.py
+++ problems/maximum_frequency_stack/solution.py
@@ -1,26 +1,23 @@
 class FreqStack:
 
     def __init__(self):
-        self.freq = collections.Counter()
-        self.m = collections.defaultdict(list)
-        self.maxf = 0
+        self.count = defaultdict(int)
+        self.pq = []
+        self.index = 0
 
-    def push(self, x: int) -> None:
-        freq, m = self.freq, self.m
-        freq[x] += 1
-        self.maxf = max(self.maxf, freq[x])
-        self.m[freq[x]].append(x)
-        
+    def push(self, val: int) -> None:
+        self.count[val] += 1
+        heapq.heappush(self.pq, (-self.count[val], -self.index, val))
+        self.index += 1
 
     def pop(self) -> int:
-        freq, m, maxf = self.freq, self.m, self.maxf
-        x = m[maxf].pop()
-        if not m[maxf]: self.maxf = maxf - 1
-        freq[x] -= 1
-        return x
+        _, _, val = heapq.heappop(self.pq)
+        self.count[val] -= 1
+        
+        return val
 
 
 # Your FreqStack object will be instantiated and called as such:
 # obj = FreqStack()
-# obj.push(x)
+# obj.push(val)
 # param_2 = obj.pop()
