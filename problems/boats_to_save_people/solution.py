45491dcaf336407c6f224566ecedd93ec40dd472
hwennnn <hwendev@gmail.com>
2022-03-24 05:59:27 +0000

    Sync LeetCode submission - Boats to Save People (python3)

--- problems/boats_to_save_people/solution.py
+++ problems/boats_to_save_people/solution.py
@@ -1,20 +1,17 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         n = len(people)
-        if n == 1: return 1
-        
-        people.sort(reverse = 1)
-        i, j = 0, n-1
-        boat = 0
+        people.sort()
+        res = 0
+        i, j = 0, n - 1
         
         while i <= j:
             if i != j and people[i] + people[j] <= limit:
+                res += 1
                 i += 1
                 j -= 1
-                boat += 1
             else:
-                i += 1
-                boat += 1
-
-        return boat
+                res += 1
+                j -= 1
         
+        return res
