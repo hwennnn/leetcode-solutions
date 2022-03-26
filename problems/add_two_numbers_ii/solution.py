9fe349cd9b42f25c9c04e69b2c6bc709ba8af903
hwennnn <hwendev@gmail.com>
2022-03-26 05:08:11 +0000

    Sync LeetCode submission - Add Two Numbers II (python3)

--- problems/add_two_numbers_ii/solution.py
+++ problems/add_two_numbers_ii/solution.py
@@ -4,9 +4,9 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         s1, s2 = [], []
+        curr = res = ListNode(-1)
         
         while l1:
             s1.append(l1.val)
@@ -16,20 +16,30 @@ class Solution:
             s2.append(l2.val)
             l2 = l2.next
         
-        res = ListNode(-1)
         carry = 0
         
         while s1 or s2 or carry:
             if s1:
                 carry += s1.pop()
+            
             if s2:
                 carry += s2.pop()
-                
-            carry, val = divmod(carry,10)
-            res.next, res.next.next = ListNode(val), res.next
             
+            curr.next = ListNode(carry % 10)
+            curr = curr.next
+            
+            carry //= 10
         
-        return res.next
-                
+        def reverseLL(head):
+            prev = None
             
+            while head:
+                nxt = head.next
+                head.next = prev
+                prev = head
+                head = nxt
+            
+            return prev
+        
+        return reverseLL(res.next)
         
