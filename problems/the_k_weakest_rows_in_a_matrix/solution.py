fd5e5c33742ee555cfffbe4b96221fd9249a6602
hwennnn <hwendev@gmail.com>
2022-03-27 04:43:08 +0000

    Sync LeetCode submission - The K Weakest Rows in a Matrix (python3)

--- problems/the_k_weakest_rows_in_a_matrix/solution.py
+++ problems/the_k_weakest_rows_in_a_matrix/solution.py
@@ -1,8 +1,31 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        res = [(sum(mat[i]), i) for i in range(len(mat))]
-        res.sort()
+        heap = []
         
-        return [res[i][1] for i in range(k)]
+        def countSoldier(row):
+            left, right = 0, len(row)
+            
+            while left < right:
+                mid = left + (right - left) // 2
+                
+                if row[mid] == 1:
+                    left = mid + 1
+                else:
+                    right = mid
+            
+            return left
         
+        for index, row in enumerate(mat):
+            curr = (-countSoldier(row), -index)
+            if len(heap) == k:
+                heapq.heappushpop(heap, curr)
+            else:
+                heapq.heappush(heap, curr)
         
+        res = []
+        
+        for _ in range(k):
+            _, index = heapq.heappop(heap)
+            res.append(-index)
+        
+        return res[::-1]
