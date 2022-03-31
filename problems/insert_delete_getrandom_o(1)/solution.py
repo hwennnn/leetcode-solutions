5740bd35d9936c5c3724751b774612e78eb06669
hwennnn <hwendev@gmail.com>
2022-03-31 04:08:09 +0000

    Sync LeetCode submission - Insert Delete GetRandom O(1) (python3)

--- problems/insert_delete_getrandom_o(1)/solution.py
+++ problems/insert_delete_getrandom_o(1)/solution.py
@@ -1,26 +1,24 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.mp = {}
-        self.A = []
+        self.s = set()
 
     def insert(self, val: int) -> bool:
-        if val in self.mp: return False
-        self.A.append(val)
-        self.mp[val] = len(self.A) - 1
+        if val in self.s: return False
+        self.s.add(val)
+        
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.mp: return False
-        last = self.A[-1]
-        self.mp[last] = self.mp[val]
-        self.A[self.mp[val]] = last
-        self.A.pop()
-        del self.mp[val]
+        if val not in self.s: return False
+        self.s.remove(val)
+        
         return True
 
     def getRandom(self) -> int:
-        return random.choice(self.A)
+        r = randint(0, len(self.s) - 1)
+        
+        return list(self.s)[r]
 
 
 # Your RandomizedSet object will be instantiated and called as such:
