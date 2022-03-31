3a78d74e6d89ceed465f7a65936c4229e7dba046
hwennnn <hwendev@gmail.com>
2022-03-31 04:25:25 +0000

    Sync LeetCode submission - My Calendar I (python3)

--- problems/my_calendar_i/solution.py
+++ problems/my_calendar_i/solution.py
@@ -1,39 +1,17 @@
-class BST:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-        self.left = None
-        self.right = None
-        
+from sortedcontainers import SortedList
+
 class MyCalendar:
 
     def __init__(self):
-        self.root = None
-    
-    def bookHelper(self, start, end, node):
-        if start >= node.end:
-            if node.right:
-                return self.bookHelper(start, end, node.right)
-            else:
-                node.right = BST(start, end)
-                return True
-        elif end <= node.start:
-            if node.left:
-                return self.bookHelper(start, end, node.left)
-            else:
-                node.left = BST(start, end)
-                return True
+        self.sl = SortedList([(float('-inf'), float('-inf')), (float('inf'), float('inf'))])
         
-        return False
-        
-
     def book(self, start: int, end: int) -> bool:
+        index = self.sl.bisect_left((start, end))
         
-        if not self.root:
-            self.root = BST(start, end)
-            return True
-        
-        return self.bookHelper(start, end, self.root)
+        if start < self.sl[index - 1][1] or end > self.sl[index][0]: return False
+
+        self.sl.add((start, end))    
+        return True
 
 
 # Your MyCalendar object will be instantiated and called as such:
