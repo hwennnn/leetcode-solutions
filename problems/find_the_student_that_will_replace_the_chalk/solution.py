1b2333a5da5ef0eacb3f1c3f4097fef55e2559f0
hwennnn <hwendev@gmail.com>
2022-04-19 13:13:32 +0000

    Sync LeetCode submission - Find the Student that Will Replace the Chalk (python3)

--- problems/find_the_student_that_will_replace_the_chalk/solution.py
+++ problems/find_the_student_that_will_replace_the_chalk/solution.py
@@ -1,11 +1,9 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
         n = len(chalk)
-        ssum = k % (sum(chalk))
-
-        for i,x in enumerate(chalk):
-            if ssum < x: return i
-            
-            ssum -= x
+        total = sum(chalk)
+        k %= total
         
-        
+        for i, x in enumerate(chalk):
+            if k < x: return i
+            k -= x
