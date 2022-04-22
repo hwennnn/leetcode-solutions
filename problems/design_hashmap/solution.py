a82c2669bf2d2a51bd8ad06c093c5811d5b93267
hwennnn <hwendev@gmail.com>
2022-04-22 05:24:27 +0000

    Sync LeetCode submission - Design HashMap (python3)

--- problems/design_hashmap/solution.py
+++ problems/design_hashmap/solution.py
@@ -1,28 +1,15 @@
 class MyHashMap:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.mp = [-1] * (10 ** 6 + 1)
-        
 
     def put(self, key: int, value: int) -> None:
-        """
-        value will always be non-negative.
-        """
         self.mp[key] = value
 
     def get(self, key: int) -> int:
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        """
         return self.mp[key]
 
     def remove(self, key: int) -> None:
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        """
         self.mp[key] = -1
 
 
