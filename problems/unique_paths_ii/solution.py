8dbd94ee5a6e0d5e2f4dfdd29008cbec5410e064
hwennnn <hwendev@gmail.com>
2022-05-20 06:11:44 +0000

    Sync LeetCode submission - Unique Paths II (python3)

--- problems/unique_paths_ii/solution.py
+++ problems/unique_paths_ii/solution.py
@@ -4,14 +4,14 @@ class Solution:
         dp = [[0] * cols for _ in range(rows)]
         dp[0][0] = 1 - grid[0][0]
         
-        for i in range(1, rows):
-            dp[i][0] = dp[i - 1][0] * (1 - grid[i][0])
-        
         for j in range(1, cols):
-            dp[0][j] = dp[0][j - 1] * (1 - grid[0][j])
+            dp[0][j] += dp[0][j - 1] * (1 - grid[0][j])
         
+        for i in range(1, rows):
+            dp[i][0] += dp[i - 1][0] * (1 - grid[i][0])
+            
         for i in range(1, rows):
             for j in range(1, cols):
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) * (1 - grid[i][j])
+                dp[i][j] += (dp[i - 1][j] + dp[i][j - 1]) * (1 - grid[i][j])
         
         return dp[-1][-1]
