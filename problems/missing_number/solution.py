197f79096f28fa78158c057c87fbecf8fcfa59ed
hwennnn <hwendev@gmail.com>
2022-05-28 06:27:34 +0000

    Sync LeetCode submission - Missing Number (python3)

--- problems/missing_number/solution.py
+++ problems/missing_number/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        total = (n*(n+1)) // 2
-        s = sum(nums)
+        total = n * (n + 1) // 2
         
-        return total - s
+        return total - sum(nums)
