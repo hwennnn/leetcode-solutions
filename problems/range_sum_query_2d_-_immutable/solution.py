080b33ec593d0fa93c77024a28349af95e2a068e
hwennnn <hwendev@gmail.com>
2022-06-03 05:02:22 +0000

    Sync LeetCode submission - Range Sum Query 2D - Immutable (python3)

--- problems/range_sum_query_2d_-_immutable/solution.py
+++ problems/range_sum_query_2d_-_immutable/solution.py
@@ -10,7 +10,7 @@ class NumMatrix:
                 self.prefix[i + 1][j + 1] = self.prefix[i + 1][j] + self.prefix[i][j + 1] - self.prefix[i][j] + matrix[i][j]
         
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return (self.prefix[row2 + 1][col2 + 1] - self.prefix[row1][col2 + 1]) - (self.prefix[row2 + 1][col1] - self.prefix[row1][col1])
+        return self.prefix[row2 + 1][col2 + 1] - self.prefix[row1][col2 + 1] - self.prefix[row2 + 1][col1] + self.prefix[row1][col1]
 
 
 # Your NumMatrix object will be instantiated and called as such:
