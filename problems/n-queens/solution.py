9e7cbe535eff4c7276bf8bb0651755b81e3b53b3
hwennnn <hwendev@gmail.com>
2022-06-04 14:15:31 +0000

    Sync LeetCode submission - N-Queens (python3)

--- problems/n-queens/solution.py
+++ problems/n-queens/solution.py
@@ -1,28 +1,37 @@
 class Solution:
-    def solveNQueens(self, n):
-        
-        def dfs(r):
-            if r == n:
-                res.append([''.join(row) for row in b])
-                return
-            for c in range(n):
-                if isValid(r, c):
-                    b[r][c] = 'Q'
-                    dfs(r + 1)      # fill row by row
-                    b[r][c] = '.'
-        
-        def isValid(r, c):
-            for i in range(r):
-                for j in range(n):
-                    if b[i][j] == 'Q' and (c == j or        # same column
-                                           i+j == r+c or    # 45 degree line
-                                           i-j == r-c):     # 135 degree line
-                        return False
-            return True
-                        
-        
-        b = [['.'] * n for _ in range(n)]
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["."] * n for _ in range(n)]
+        cols, diag1, diag2 = set(), set(), set()
         res = []
-        dfs(0)      # start from row 0
+        
+        def place(row, col):
+            board[row][col] = "Q"
+            cols.add(col)
+            diag1.add(row - col)
+            diag2.add(row + col)
+        
+        def discard(row, col):
+            board[row][col] = "."
+            cols.remove(col)
+            diag1.remove(row - col)
+            diag2.remove(row + col)
+        
+        def ok(row, col):
+            return col not in cols and (row - col) not in diag1 and (row + col) not in diag2
+        
+        def go(row):
+            if row == n:
+                nonlocal res
+                
+                res.append(["".join(b) for b in board])
+                return
+            
+            for col in range(n):
+                if ok(row, col):
+                    place(row, col)
+                    go(row + 1)
+                    discard(row, col)
+        
+        go(0)
         
         return res
