78e8d27b54264b708f5c19581538f207563d7d75
hwennnn <hwendev@gmail.com>
2022-06-05 04:48:40 +0000

    Sync LeetCode submission - N-Queens II (python3)

--- problems/n-queens_ii/solution.py
+++ problems/n-queens_ii/solution.py
@@ -1,19 +1,22 @@
 class Solution:
-    def totalNQueens(self, n: int, diag1 = set(), diag2 = set(), cols = set(), row = 0) -> int:
-        if row == n: return 1
+    def totalNQueens(self, n: int) -> int:
+        cols, diag1, diag2 = set(), set(), set()
         
-        count = 0
+        def go(row):
+            if row == n: return 1
+            
+            res = 0
+            
+            for col in range(n):
+                if col not in cols and row - col not in diag1 and row + col not in diag2:
+                    cols.add(col)
+                    diag1.add(row - col)
+                    diag2.add(row + col)
+                    res += go(row + 1)
+                    cols.remove(col)
+                    diag1.remove(row - col)
+                    diag2.remove(row + col)
+            
+            return res
         
-        for col in range(n):
-            if row+col in diag1 or row-col in diag2 or col in cols: continue
-            diag1.add(row+col)
-            diag2.add(row-col)
-            cols.add(col)
-            
-            count += self.totalNQueens(n, diag1, diag2, cols, row + 1)
-            
-            diag1.remove(row+col)
-            diag2.remove(row-col)
-            cols.remove(col)
-            
-        return count
+        return go(0)
