b6273ffcc23f6346e9b8b5d4f9731c0395e36bde
hwennnn <hwendev@gmail.com>
2022-06-06 04:52:17 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (python3)

--- problems/intersection_of_two_linked_lists/solution.py
+++ problems/intersection_of_two_linked_lists/solution.py
@@ -5,12 +5,11 @@
 #         self.next = None
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         currA, currB = headA, headB
         
         while currA != currB:
             currA = currA.next if currA else headB
             currB = currB.next if currB else headA
-        
+            
         return currA
