28d00509026139465fb1eac4190c863477d5ac58
hwennnn <hwendev@gmail.com>
2022-06-11 04:03:31 +0000

    Sync LeetCode submission - Minimum Operations to Reduce X to Zero (python3)

--- problems/minimum_operations_to_reduce_x_to_zero/solution.py
+++ problems/minimum_operations_to_reduce_x_to_zero/solution.py
@@ -2,20 +2,21 @@ class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
         n = len(nums)
         total = sum(nums)
+        
         if total == x: return n
         
-        target = total - x
-        mp = collections.defaultdict(int)
-        mp[0] = -1
-        c = 0
         res = float('-inf')
+        target = total - x
+        mp = {0:-1}
+        curr = 0
         
-        for i,num in enumerate(nums):
-            c += num
-            if c - target in mp:
-                res = max(res, i - mp[c-target])
+        for i, x in enumerate(nums):
+            curr += x
             
-            if c not in mp:
-                mp[c] = i
+            if curr - target in mp:
+                res = max(res, i - mp[curr - target])
+            
+            if curr not in mp:
+                mp[curr] = i
         
-        return n - res if res != float('-inf') else -1
+        return -1 if res == float('-inf') else n - res
