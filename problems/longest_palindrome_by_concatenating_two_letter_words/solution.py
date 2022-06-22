4740766cf5c0f8076cdd6f18f341ecd4f1ecff79
hwennnn <hwendev@gmail.com>
2022-06-22 13:17:50 +0000

    Sync LeetCode submission - Longest Palindrome by Concatenating Two Letter Words (python3)

--- problems/longest_palindrome_by_concatenating_two_letter_words/solution.py
+++ problems/longest_palindrome_by_concatenating_two_letter_words/solution.py
@@ -1,25 +1,26 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         count = Counter()
-        res = leftover = 0
+        res = 0
+        middle = 0
         
         for word in words:
-            if word[0] == word[1]:
+            if word == word[::-1]:
                 if count[word] > 0:
-                    leftover -= 1
-                    res += 4
                     count[word] -= 1
+                    res += 4
+                    middle -= 1
                 else:
+                    middle += 1
                     count[word] += 1
-                    leftover += 1
             else:
                 if count[word[::-1]] > 0:
-                    res += 4
                     count[word[::-1]] -= 1
+                    res += 4
                 else:
                     count[word] += 1
         
-        if leftover > 0:
+        if middle > 0:
             res += 2
         
         return res
