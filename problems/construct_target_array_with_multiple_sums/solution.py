f66b94866836ff55086ca7db3de46bbf4b33b4c0
hwennnn <hwendev@gmail.com>
2022-06-24 03:18:01 +0000

    Sync LeetCode submission - Construct Target Array With Multiple Sums (python3)

--- problems/construct_target_array_with_multiple_sums/solution.py
+++ problems/construct_target_array_with_multiple_sums/solution.py
@@ -1,15 +1,18 @@
 class Solution:
-    def isPossible(self, A):
-        total = sum(A)
-        A = [-a for a in A]
-        heapq.heapify(A)
-        while True:
-            a = -heapq.heappop(A)
-            total -= a
-            if a == 1 or total == 1: return True
-            if a < total or total == 0 or a % total == 0:
-                return False
-            a %= total
-            total += a
-            heapq.heappush(A, -a)
+    def isPossible(self, target: List[int]) -> bool:
+        total = sum(target)
+        heap = [-x for x in target]
+        heapify(heap)
         
+        while True:
+            x = -heappop(heap)
+            total -= x
+            
+            if x == 1 or total == 1: return True
+            
+            if x < total or total == 0 or x % total == 0: return False
+            
+            x %= total
+            total += x
+            
+            heappush(heap, -x)
