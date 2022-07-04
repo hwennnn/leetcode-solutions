5f0fd6907f56768a52fd828a09a4135b4166224d
hwennnn <hwendev@gmail.com>
2022-07-04 03:38:41 +0000

    Sync LeetCode submission - Insert Interval (python3)

--- problems/insert_interval/solution.py
+++ problems/insert_interval/solution.py
@@ -1,57 +1,15 @@
 class Solution:
-    def insert(self, intervals, newInterval):
-        """
-        Given an ordered list of intervals and a new interval,
-        this program inserts the new interval into the ordered
-        list of intervals while incorporating mergers of intervals
-        where appropriate.
-
-        :param intervals: array of intervals where each interval
-                          is a [start, end] pair and the intervals
-                          are sorted by their start points
-        :type intervals: list[list[int]]
-        :param newInterval: interval to be inserted into intervals
-        :type newInterval: list[int]
-        :return: updated array of intervals
-        :rtype: list[list[int]]
-        """
-        new_intervals_front = []
-        new_intervals_back = []
-        new_begin, new_end = newInterval
-        for ibegin, iend in intervals:
-            if iend < new_begin:
-                """
-                [ibegin, iend] occurs before newInterval without
-                overlap.
-                """
-                new_intervals_front.append( [ibegin, iend] )
-            elif ibegin < new_begin:
-                if iend <= new_end:
-                    """
-                    [ibegin, iend] precedes and overlaps newInterval,
-                    but does not exceed newInterval
-                    """
-                    new_begin = ibegin
-                else:
-                    """
-                    newInterval resides within [ibegin, iend]
-                    """
-                    return intervals
-            elif ibegin >= new_begin and iend <= new_end:
-                """
-                [ibegin, iend] resides within newInterval
-                """
-                pass
-            elif new_begin <= ibegin <= new_end and iend > new_end:
-                """
-                [ibegin, iend] overlaps and exceeds newInterval
-                """
-                new_end = iend
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        sx, sy = newInterval
+        left, right = [], []
+        
+        for x, y in intervals:
+            if y < sx:
+                left.append([x, y])
+            elif x > sy:
+                right.append([x, y])
             else:
-                """
-                [ibegin, iend] exceeds, but does not overlap
-                newInterval
-                """
-                new_intervals_back.append( [ibegin, iend] )
-        insert_interval = [new_begin, new_end]
-        return new_intervals_front + [insert_interval] + new_intervals_back
+                sx = min(sx, x)
+                sy = max(sy, y)
+                
+        return left + [[sx, sy]] + right
