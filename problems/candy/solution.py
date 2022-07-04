2fc22c19facf9c8f5d1f0f60a09030b9b88f52a1
hwennnn <hwendev@gmail.com>
2022-07-04 03:09:26 +0000

    Sync LeetCode submission - Candy (python3)

--- problems/candy/solution.py
+++ problems/candy/solution.py
@@ -1,17 +1,19 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
-        res = [-1] * n
-
-        for _, index in sorted((rating, index) for index,rating in enumerate(ratings)):
-            current = 1
-            
-            if index - 1 >= 0 and ratings[index] > ratings[index - 1]:
-                current = max(current, res[index - 1] + 1)
-            
-            if index + 1 < n and ratings[index] > ratings[index + 1]:
-                current = max(current, res[index + 1] + 1)
-            
-            res[index] = current
         
-        return sum(res)
+        A = [(x, i) for i, x in enumerate(ratings)]
+        A.sort()
+        candies = [1] * n
+        
+        for x, i in A:
+            current = candies[i]
+            
+            if i - 1 >= 0 and ratings[i - 1] > x:
+                candies[i - 1] = max(candies[i - 1], current + 1)
+            
+            if i + 1 < n and ratings[i + 1] > x:
+                candies[i + 1] = max(candies[i + 1], current + 1)
+        
+        return sum(candies)
+        
