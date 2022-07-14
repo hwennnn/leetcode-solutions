9008d4bbf314315babf5706daf8306da8b033eaf
hwennnn <hwendev@gmail.com>
2022-07-14 03:50:54 +0000

    Sync LeetCode submission - Construct Binary Tree from Preorder and Inorder Traversal (python3)

--- problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.py
+++ problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.py
@@ -5,21 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         
-        def helper(preStart, inStart, inEnd):
+        def go(preStart, inStart, inEnd):
             if inStart > inEnd or preStart >= len(preorder): return None
             
             root = TreeNode(preorder[preStart])
             
-            root_index = 0
-            for i in range(inStart, inEnd + 1):
-                if inorder[i] == root.val:
-                    root_index = i
+            rootIndex = 0
+            for index in range(inStart, inEnd + 1):
+                if inorder[index] == root.val:
+                    rootIndex = index
             
-            root.left = helper(preStart + 1, inStart, root_index - 1)
-            root.right = helper(preStart + root_index - inStart + 1, root_index + 1, inEnd)
+            root.left = go(preStart + 1, inStart, rootIndex - 1)
+            root.right = go(preStart + rootIndex - inStart + 1, rootIndex + 1, inEnd)
             
             return root
         
-        return helper(0, 0, len(inorder) - 1)
+        return go(0, 0, len(inorder) - 1)
