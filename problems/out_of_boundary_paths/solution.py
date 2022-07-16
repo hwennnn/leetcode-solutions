b6c2db80111350f319c4889a404c5a696a23548e
hwennnn <hwendev@gmail.com>
2022-07-16 04:06:24 +0000

    Sync LeetCode submission - Out of Boundary Paths (python3)

--- problems/out_of_boundary_paths/solution.py
+++ problems/out_of_boundary_paths/solution.py
@@ -1,17 +1,20 @@
 class Solution:
-    def findPaths(self, rows: int, cols: int, maxMove: int, startRow: int, startColumn: int) -> int:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        M = 10 ** 9 + 7
         
         @cache
         def go(x, y, moves):
-            if not (0 <= x < rows and 0 <= y < cols): return 1
+            if x < 0 or x >= m or y < 0 or y >= n:
+                return 1
             
             if moves == maxMove: return 0
             
             res = 0
             
-            for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
-                res += go(dx, dy, moves + 1)
+            for dx, dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+                res += go(dx, dy, moves + 1)    
+                res %= M
             
             return res
         
-        return go(startRow, startColumn, 0) % (10 ** 9 + 7)
+        return go(startRow, startColumn, 0)
