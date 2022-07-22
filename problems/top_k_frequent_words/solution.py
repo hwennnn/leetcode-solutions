f81c12449b77e208eb9c85d86b8876fc5c36b0be
hwennnn <hwendev@gmail.com>
2022-07-22 01:11:42 +0000

    Sync LeetCode submission - Top K Frequent Words (python3)

--- problems/top_k_frequent_words/solution.py
+++ problems/top_k_frequent_words/solution.py
@@ -1,29 +1,7 @@
-class Word:
-    def __init__(self, word, freq):
-        self.word = word
-        self.freq = freq
-    
-    def __lt__(self, other):
-        if self.freq == other.freq:
-            return self.word > other.word
-        
-        return self.freq < other.freq
-    
-    def __eq__(self, other):
-        return self.word == other.word and self.freq == other.freq
-    
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        counter = collections.Counter(words)
-        heap = []
+        counter = Counter(words)
+        A = list(counter.items())
+        A.sort(key = lambda x: (-x[1], x[0]))
         
-        for word, freq in counter.items():
-            heapq.heappush(heap, Word(word, freq))
-            if len(heap) > k:
-                heapq.heappop(heap)
-        
-        res = []
-        for _ in range(k):
-            res.append(heapq.heappop(heap).word)
-        
-        return res[::-1]
+        return [x for x, _ in A[:k]]
