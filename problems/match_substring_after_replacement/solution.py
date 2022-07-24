d7e99735a429f8cb957c0dd40bcab63dc1ab7ae9
hwennnn <hwendev@gmail.com>
2022-07-24 10:52:44 +0000

    Sync LeetCode submission - Match Substring After Replacement (python3)

--- problems/match_substring_after_replacement/solution.py
+++ problems/match_substring_after_replacement/solution.py
@@ -2,18 +2,24 @@ class Solution:
     def matchReplacement(self, s: str, sub: str, mappings: List[List[str]]) -> bool:
         mp = defaultdict(set)
         n, m = len(s), len(sub)
-
+        
         for a, b in mappings:
             mp[b].add(a)
+        
+        def go(index, curr):
+            if curr == m:
+                return True
             
-        for i in range(n):
-            if i + m > n: return False
+            if index == n:
+                return False
             
-            for j in range(m):
-                if not (sub[j] == s[i + j] or sub[j] in mp[s[i + j]]):
-                    break
-                
-                if j == m - 1:
-                    return True
+            if sub[curr] == s[index] or sub[curr] in mp[s[index]]:
+                return go(index + 1, curr + 1)
+            
+            return False
+            
+        for i, x in enumerate(s):
+            if go(i, 0):
+                return True
         
         return False
