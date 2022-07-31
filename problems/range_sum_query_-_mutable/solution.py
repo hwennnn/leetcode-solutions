a622d177d82fa640d76c4aca24f0c54178087eae
hwennnn <hwendev@gmail.com>
2022-07-31 02:06:56 +0000

    Sync LeetCode submission - Range Sum Query - Mutable (python3)

--- problems/range_sum_query_-_mutable/solution.py
+++ problems/range_sum_query_-_mutable/solution.py
@@ -1,63 +1,40 @@
-class SegmentTree:
-    
-    def __init__(self, start, end):
-        self.left = None
-        self.right = None
-        self.start = start
-        self.end = end
-        self.total = 0
+class BinaryIndexedTree:
+    def __init__(self, size):
+        self._size = size + 1
+        self._data = [0] * self._size
 
+    @staticmethod
+    def from_list(nums):
+        tree = BinaryIndexedTree(len(nums))
+        for index, num in enumerate(nums):
+            tree.update(index, num)
+        return tree
+    
+    def update(self, index, value):
+        index += 1
+        while index < self._size:
+            self._data[index] += value
+            index += index & -index
+    
+    def query(self, index):
+        index += 1
+        result = 0
+        while index > 0:
+            result += self._data[index]
+            index -= index & -index
+        return result
+    
+    def range_sum(self, left, right):
+        return self.query(right) - self.query(left - 1)
+    
 class NumArray:
-
     def __init__(self, nums: List[int]):
-        self.root = self.buildTree(nums, 0, len(nums) - 1)
-    
-    def buildTree(self, nums, start, end):
-        if start > end: return None
-        
-        node = SegmentTree(start, end)
-        
-        if start == end:
-            node.total = nums[start]
-        else:
-            mid = start + (end - start) // 2
-            node.left = self.buildTree(nums, start, mid)
-            node.right = self.buildTree(nums, mid + 1, end)
-            node.total = node.left.total + node.right.total
-        
-        return node
+        self._tree = BinaryIndexedTree.from_list(nums)
+        self._nums = nums
 
     def update(self, index: int, val: int) -> None:
-        self.updateR(self.root, index, val)
-    
-    def updateR(self, node, index, val):
-        
-        if node.start == node.end:
-            node.total = val
-        else:
-            mid = node.start + (node.end - node.start) // 2
-            if index <= mid:
-                self.updateR(node.left, index, val)
-            else:
-                self.updateR(node.right, index, val)
-            
-            node.total = node.left.total + node.right.total
+        self._tree.update(index, val - self._nums[index])
+        self._nums[index] = val
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.sumRangeR(self.root, left, right)
-    
-    def sumRangeR(self, node, left, right):   
-        if not node or node.start > right or node.end < left: return 0
-
-        if node.start >= left and node.end <= right: return node.total
-        
-        l = self.sumRangeR(node.left, left, right)
-        r = self.sumRangeR(node.right, left, right)
-        
-        return l + r
-
-
-# Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# obj.update(index,val)
-# param_2 = obj.sumRange(left,right)
+        return self._tree.range_sum(left, right)
