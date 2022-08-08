eef8e25b0df80c5df9fddf777d3c2978d971019b
hwennnn <hwendev@gmail.com>
2022-08-08 15:03:54 +0000

    Sync LeetCode submission - Check if There is a Valid Partition For The Array (python3)

--- problems/check_if_there_is_a_valid_partition_for_the_array/solution.py
+++ problems/check_if_there_is_a_valid_partition_for_the_array/solution.py
@@ -1,22 +1,23 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
-        N = len(nums)
+        n = len(nums)
         
         @cache
         def go(index):
-            if index == N: return True
+            if index >= n:
+                return True
             
-            res = False
+            valid = False
             
-            if index + 1 < N and nums[index] == nums[index + 1]:
-                res |= go(index + 2)
-            
-            if index + 2 < N and nums[index] == nums[index + 1] == nums[index + 2]:
-                res |= go(index + 3)
+            if index + 1 < n and nums[index] == nums[index + 1]:
+                valid |= go(index + 2)
                 
-            if index + 2 < N and nums[index] + 1 == nums[index + 1] and nums[index + 1] + 1 == nums[index + 2]:
-                res |= go(index + 3)
+            if index + 2 < n and nums[index] == nums[index + 1] == nums[index + 2]:
+                valid |= go(index + 3)
+                
+            if index + 2 < n and nums[index] + 1 == nums[index + 1] and nums[index + 1] + 1 == nums[index + 2]:
+                valid |= go(index + 3)
             
-            return res
-        
+            return valid
+            
         return go(0)
