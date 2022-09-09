7700b5e4e89c7d4afa02c4e256c36c2baaa45b6a
hwennnn <hwendev@gmail.com>
2022-09-09 03:31:42 +0000

    Sync LeetCode submission - The Number of Weak Characters in the Game (python3)

--- problems/the_number_of_weak_characters_in_the_game/solution.py
+++ problems/the_number_of_weak_characters_in_the_game/solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def numberOfWeakCharacters(self, A: List[List[int]]) -> int:
-        A.sort(key = lambda x:(x[0], -x[1]))
-        stack = []
+        A.sort(key = lambda x : (x[0], -x[1]))
         res = 0
+        stack = []
         
-        for a,b in A:
+        for a, b in A:
             while stack and a > stack[-1][0] and b > stack[-1][1]:
-                stack.pop()
                 res += 1
+                stack.pop()
             
             stack.append((a, b))
         
