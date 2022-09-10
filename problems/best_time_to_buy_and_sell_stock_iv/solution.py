8ff07586dcf3c3e7b1b4d0d7e18b1cfadfa0c2f9
hwennnn <hwendev@gmail.com>
2022-09-10 12:35:06 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock IV (python3)

--- problems/best_time_to_buy_and_sell_stock_iv/solution.py
+++ problems/best_time_to_buy_and_sell_stock_iv/solution.py
@@ -1,19 +1,23 @@
 class Solution:
-    def maxProfit(self, k, prices):
-        if k >= int(len(prices) / 2):
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        
+        if k >= n // 2:
             res = 0
-            for i in range(1, len(prices)):
-                if prices[i] > prices[i-1]:
-                    res += prices[i] - prices[i-1]
+            
+            for i in range(1, n):
+                if prices[i] > prices[i - 1]:
+                    res += prices[i] - prices[i - 1]
+            
             return res
-        if len(prices) == 0 or k == 0:
-            return 0
-        dp = [[[0,0] for i in range(len(prices)+1)] for j in range(k+1)]
-        for i in range(k+1):
-            dp[i][0][0] = -prices[0]
-        for i in range(1,k+1):
-            for p in range(1,len(prices)+1):
-                dp[i][p][0] = max(dp[i][p-1][0], dp[i-1][p-1][1] - prices[p-1])
-                dp[i][p][1] = max(dp[i][p-1][1], dp[i][p-1][0] + prices[p-1], 0)   
-
-        return dp[-1][-1][-1]
+        
+        dp = [[0] * (n) for _ in range(k + 1)]
+        
+        for i in range(1, k + 1):
+            tempMax = -prices[0]
+            
+            for j in range(1, n):
+                dp[i][j] = max(dp[i][j - 1], prices[j] + tempMax)
+                tempMax = max(tempMax, dp[i - 1][j - 1] - prices[j])
+        
+        return dp[k][n - 1]
