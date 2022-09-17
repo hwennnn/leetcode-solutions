7753350916ca9c22c47cd763d294f035b349b889
hwennnn <hwendev@gmail.com>
2022-09-17 03:41:06 +0000

    Sync LeetCode submission - Palindrome Pairs (python3)

--- problems/palindrome_pairs/solution.py
+++ problems/palindrome_pairs/solution.py
@@ -1,57 +1,22 @@
-class TrieNode:
-    def __init__(self):
-        self.pos = -1
-        self.nodes = [None] * 26
-        self.palins = []
-
-class Solution:    
-    def add(self, root, word, pos):
-        for i in range(len(word) - 1, -1, -1):
-            if self.isPalindrome(word, 0, i):
-                root.palins.append(pos)
-        
-            p = ord(word[i]) - ord('a')
-            if not root.nodes[p]:
-                root.nodes[p] = TrieNode()
-            
-            root = root.nodes[p]
-        
-        root.pos = pos
-        root.palins.append(pos)
-    
-    def isPalindrome(self, word, i, j):
-        while i < j and word[i] == word[j]:
-            i += 1; j -= 1
-            
-        return i >= j
-    
-    def search(self, root, word, pos, res):
-        n = len(word)
-        j = 0
-        
-        while root and j < n:
-            if root.pos >= 0 and pos != root.pos and self.isPalindrome(word, j, n - 1):
-                res.append([pos, root.pos])
-            
-            p = ord(word[j]) - ord('a')
-            root = root.nodes[p]
-            j += 1
-        
-        if root and root.palins:
-            for j in root.palins:
-                if j != pos:
-                    res.append([pos, j])
-        
-    
+class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
-        n = len(words)
-        root = TrieNode()
+        N = len(words)
+        mp = {x : i for i, x in enumerate(words)}
         res = []
         
-        for i in range(n):
-            self.add(root, words[i], i)
-        
-        for i in range(n):
-            self.search(root, words[i], i, res)
-        
+        for index, word in enumerate(words):
+            for j in range(len(word) + 1):
+                prefix = word[:j]
+                suffix = word[j:]
+                
+                if prefix == prefix[::-1]:
+                    suffixR = suffix[::-1]
+                    if word != suffixR and suffixR in mp:
+                        res.append([mp[suffixR], index])
+                
+                if j != len(word) and suffix == suffix[::-1]:
+                    prefixR = prefix[::-1]
+                    if word != prefixR and prefixR in mp:
+                        res.append([index, mp[prefixR]])
+                
         return res
