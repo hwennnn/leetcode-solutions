e8ec3532a13a02df582c20dffd6f2d843303fc80
hwennnn <hwendev@gmail.com>
2022-10-20 03:24:42 +0000

    Sync LeetCode submission - Integer to Roman (python3)

--- problems/integer_to_roman/solution.py
+++ problems/integer_to_roman/solution.py
@@ -1,11 +1,11 @@
 class Solution:
-    def intToRoman(self, num):
-        mp = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
-        nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-        result = ""
+    def intToRoman(self, num: int) -> str:
+        res = ""
+        mp = [("I", 1), ("IV", 4), ("V", 5), ("IX", 9), ("X", 10), ("XL", 40), ("L", 50), ("XC", 90), ("C", 100), ("CD", 400), ("D", 500), ("CM", 900), ("M", 1000)]
+
+        for symbol, x in mp[::-1]:
+            if num >= x:
+                res += symbol * (num // x)
+                num %= x
         
-        for letter, n in zip(mp, nums):
-            result += letter * (num // n)
-            num %= n
-            
-        return result
+        return res
