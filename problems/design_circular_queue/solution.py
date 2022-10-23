2bdbee9b5aa82ba866ef34c8071dca51393a5534
hwennnn <hwendev@gmail.com>
2022-10-23 06:17:10 +0000

    Sync LeetCode submission - Design Circular Queue (python3)

--- problems/design_circular_queue/solution.py
+++ problems/design_circular_queue/solution.py
@@ -1,43 +1,47 @@
 class MyCircularQueue:
 
-    def __init__(self, k):
+    def __init__(self, k: int):
+        self.k = k
         self.size = 0
-        self.max_size = k
-        self.t = [0]*k
-        self.front = self.rear = -1
+        self.maxSize = k
+        self.A = [0] * k
+        self.front = 0
+        self.rear = -1
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull(): return False
         
-    def enQueue(self, value):
-        if self.size == self.max_size: return False
-        else:
-            if self.rear == -1:
-                self.rear = self.front = 0
-            else:
-                self.rear = (self.rear + 1)%self.max_size
-            self.t[self.rear] = value
-            self.size += 1
-            return True
+        self.rear = (self.rear + 1) % self.k
+        self.A[self.rear] = value
+        self.size += 1
         
-    def deQueue(self):
-        if self.size == 0: return False
-        if self.front == self.rear:
-            self.front = self.rear = -1
-        else:
-            self.front = (self.front + 1)%self.max_size
-        self.size -= 1
         return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty(): return False
         
+        self.front = (self.front + 1) % self.k
+        self.size -= 1
+        
+        return True
 
-    def Front(self):
-        return self.t[self.front] if self.size != 0 else -1
+    def Front(self) -> int:
+        if self.isEmpty(): return -1
+        
+        return self.A[self.front]
 
-    def Rear(self):
-        return self.t[self.rear] if self.size != 0 else -1
+    def Rear(self) -> int:
+        if self.isEmpty(): return -1
+        
+        return self.A[self.rear]
 
-    def isEmpty(self):
+    def isEmpty(self) -> bool:
         return self.size == 0
 
-    def isFull(self):
-        return self.size == self.max_size
+    def isFull(self) -> bool:
+        return self.size == self.maxSize
+
+
 # Your MyCircularQueue object will be instantiated and called as such:
 # obj = MyCircularQueue(k)
 # param_1 = obj.enQueue(value)
