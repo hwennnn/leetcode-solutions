03fc385d004189c859cce7c619f2d7206ad8e082
hwennnn <hwendev@gmail.com>
2022-10-26 03:57:01 +0000

    Sync LeetCode submission - Continuous Subarray Sum (python3)

--- problems/continuous_subarray_sum/solution.py
+++ problems/continuous_subarray_sum/solution.py
@@ -1,19 +1,14 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        
-        prefix = {0: -1}
-        c = 0
+        mp = {0:-1}
+        s = 0
         
         for i, x in enumerate(nums):
-            c += x
-            if k != 0:
-                c %= k
+            s = (s + x) % k
+            if s not in mp:
+                mp[s] = i
             
-            if c in prefix:
-                if i - prefix[c] > 1:
-                    return True
-            else:
-                prefix[c] = i
-    
-        return False
+            if i - mp[s] >= 2:
+                return True
         
+        return False
