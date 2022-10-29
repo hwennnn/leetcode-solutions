c5f157f63a5b6d699baeb0388c07af11e14a3bb3
hwennnn <hwendev@gmail.com>
2022-10-29 05:07:24 +0000

    Sync LeetCode submission - Earliest Possible Day of Full Bloom (python3)

--- problems/earliest_possible_day_of_full_bloom/solution.py
+++ problems/earliest_possible_day_of_full_bloom/solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
-        A = sorted(zip(plantTime, growTime), key = lambda x:-x[1])
-        
-        days = res = 0
+        A = sorted([(plant, grow) for plant, grow in zip(plantTime, growTime)], key = lambda x:-x[1])
+        res = 0
+        days = 0
         
         for plant, grow in A:
             days += plant
-            
             res = max(res, days + grow)
         
         return res
