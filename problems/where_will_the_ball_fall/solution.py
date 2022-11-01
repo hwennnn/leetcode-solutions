e6c36f06ee3082b040f5920b47f3d34cf7c790fb
hwennnn <hwendev@gmail.com>
2022-11-01 11:56:51 +0000

    Sync LeetCode submission - Where Will the Ball Fall (python3)

--- problems/where_will_the_ball_fall/solution.py
+++ problems/where_will_the_ball_fall/solution.py
@@ -2,20 +2,18 @@ class Solution:
     def findBall(self, grid: List[List[int]]) -> List[int]:
         rows, cols = len(grid), len(grid[0])
         
-        def go(row, col):
-            if row == rows: return col
-            
-            if 0 <= row < rows and 0 <= col < cols:
-                if grid[row][col] == 1 and col == cols - 1: return -1
-                
-                elif grid[row][col] == 1 and col + 1 < cols and grid[row][col + 1] == -1: return -1
-                
-                elif grid[row][col] == -1 and col - 1 < 0: return -1
-                
-                elif grid[row][col] == -1 and col - 1 >= 0 and grid[row][col - 1] == 1: return -1
-            
-                return go(row + 1, col + grid[row][col])
-            
-            return -1
+        def go(x, y):
+            if x == rows: return y
 
-        return [go(0, j) for j in range(cols)]
+            if grid[x][y] == 1:
+                if y == cols - 1 or grid[x][y + 1] == -1:
+                    return -1
+                
+                return go(x + 1, y + 1)
+            else:
+                if y == 0 or grid[x][y - 1] == 1:
+                    return -1
+                
+                return go(x + 1, y - 1)
+            
+        return [go(0, y) for y in range(cols)]
