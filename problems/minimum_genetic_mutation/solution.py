e808359530a8d2e2749614bd2c3503e39d83bc52
hwennnn <hwendev@gmail.com>
2022-11-02 03:41:16 +0000

    Sync LeetCode submission - Minimum Genetic Mutation (python3)

--- problems/minimum_genetic_mutation/solution.py
+++ problems/minimum_genetic_mutation/solution.py
@@ -1,19 +1,26 @@
 class Solution:
     def minMutation(self, start: str, end: str, bank: List[str]) -> int:
-        visited = set([start])
-        queue = deque([(start, 0)])
+        seen = set([start])
         bank = set(bank)
-        
-        while queue:
-            word, count = queue.popleft()
+        dq = deque([start])
+        count = 0
+
+        while dq:
+            N = len(dq)
+
+            for _ in range(N):
+                curr = dq.popleft()
+
+                if curr == end: return count
+
+                for i in range(8):
+                    for char in "ACGT":
+                        if char == curr[i]: continue
+                        newWord = curr[:i] + char + curr[i + 1:]
+                        if newWord in bank and newWord not in seen:
+                            dq.append(newWord)
+                            seen.add(newWord)
             
-            if word == end: return count
-            
-            for i in range(8):
-                for char in "ACGT":
-                    w = word[:i] + char + word[i + 1:]
-                    if w not in visited and w in bank:
-                        visited.add(w)
-                        queue.append((w, count + 1))
+            count += 1
         
         return -1
