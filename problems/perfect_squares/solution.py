4d4337225d330a0367e1987869c2379344d1c37d
hwennnn <hwendev@gmail.com>
2022-11-22 02:28:31 +0000

    Sync LeetCode submission - Perfect Squares (python3)

--- problems/perfect_squares/solution.py
+++ problems/perfect_squares/solution.py
@@ -1,16 +1,18 @@
 class Solution:
-    def numSquares(self, n: int) -> int:
-        dp = [float('inf')] * (n + 1)
+    def numSquares(self, N: int) -> int:
+        dp = [inf] * (N + 1)
         dp[0] = 0
-        
-        for i in range(1, n + 1):
-            mmin = float('inf')
-            j = 1
-            
-            while i - j * j >= 0:
-                mmin = min(mmin, dp[i - j * j] + 1)
-                j += 1
-                
-            dp[i] = mmin
 
-        return dp[n]
+        for i in range(1, N + 1):
+            curr = inf
+            j = 1
+
+            while i - j * j >= 0:
+                k = dp[i - j * j] + 1
+                if k < curr:
+                    curr = k
+                j += 1
+            
+            dp[i] = curr
+
+        return dp[N]
