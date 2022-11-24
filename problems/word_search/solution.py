ef7f673cb2adce25148224e572428a177474403c
hwennnn <hwendev@gmail.com>
2022-11-24 12:11:54 +0000

    Sync LeetCode submission - Word Search (python3)

--- problems/word_search/solution.py
+++ problems/word_search/solution.py
@@ -1,25 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
-        n = len(word)
-        
-        def backtrack(x, y, i, curr, visited):
-            if curr == word:
-                return True
-            
-            for dx,dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-                if 0 <= dx < rows and 0 <= dy < cols and board[dx][dy] == word[i] and (dx, dy) not in visited:
-                    visited.add((dx, dy))
-                    if backtrack(dx, dy, i + 1, curr + board[dx][dy], visited):
-                        return True
-                    visited.remove((dx, dy))
-            
+        N = len(word)
+        visited = [[False] * cols for _ in range(rows)]
+
+        def go(x, y, index):
+            if index == N: return True
+
+            for dx, dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+                if 0 <= dx < rows and 0 <= dy < cols and not visited[dx][dy] and board[dx][dy] == word[index]:
+                    visited[dx][dy] = True
+                    if go(dx, dy, index + 1): return True
+                    visited[dx][dy] = False
+
             return False
         
-        for x in range(rows):
-            for y in range(cols):
-                if board[x][y] == word[0]:
-                    if backtrack(x, y, 1, word[0], set([(x, y)])):
-                        return True
-            
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == word[0]:
+                    visited[i][j] = True
+                    if go(i, j, 1): return True
+                    visited[i][j] = False
+        
         return False
