eec1d087c255664c9429ef42c7303b9a80e53caa
hwennnn <hwendev@gmail.com>
2022-12-03 02:05:01 +0000

    Sync LeetCode submission - Sort Characters By Frequency (python3)

--- problems/sort_characters_by_frequency/solution.py
+++ problems/sort_characters_by_frequency/solution.py
@@ -1,15 +1,9 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        n = len(s)
-        buckets = [[] for _ in range(n + 1)]
-        res = ''
+        A = Counter(s)
+        res = []
+
+        for k, v in sorted(A.items(), key = lambda x:-x[1]):
+            res += [k] * v
         
-        counter = collections.Counter(s)
-        for k,v in counter.items():
-            buckets[v].append(k)
-        
-        for i in range(n, -1, -1):
-            for c in buckets[i]:
-                res += c * i
-        
-        return res
+        return "".join(res)
