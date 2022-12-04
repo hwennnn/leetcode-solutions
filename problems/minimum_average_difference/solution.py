f4e791f78c909b62d9cea362c84114ddd5433ab2
hwennnn <hwendev@gmail.com>
2022-12-04 01:50:03 +0000

    Sync LeetCode submission - Minimum Average Difference (python3)

--- problems/minimum_average_difference/solution.py
+++ problems/minimum_average_difference/solution.py
@@ -1,26 +1,21 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
-        mmin = float('inf')
-        res = -1
-        n = len(nums)
-        curr = 0
-        s = sum(nums)
-        
+        N = len(nums)
+        left = 0
+        right = sum(nums)
+        minAverage = inf
+        index = -1
 
         for i, x in enumerate(nums):
-            left = i + 1
-            right = n - i - 1
-            
-            curr += x
-            s -= x
+            left += x
+            right -= x
 
-            if i == n - 1:
-                ans = abs(floor(curr / left))
-            else:
-                ans = abs(floor(curr / left) - floor(s / right))
-            # print(i, ans)
-            if ans < mmin:
-                mmin = ans
-                res = i
+            leftAverage = left // (i + 1)
+            rightAverage = right // (N - i - 1) if i < N - 1 else 0
+            average = abs(leftAverage - rightAverage)
+
+            if average < minAverage:
+                minAverage = average
+                index = i
         
-        return res
+        return index
