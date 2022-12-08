6e1cde97acfe03cbfc5f0e8892a19f3a663b278a
hwennnn <hwendev@gmail.com>
2022-12-08 01:33:04 +0000

    Sync LeetCode submission - Leaf-Similar Trees (python3)

--- problems/leaf-similar_trees/solution.py
+++ problems/leaf-similar_trees/solution.py
@@ -1,15 +1,25 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
-        return self.findleaf(root1) == self.findleaf(root2)
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         
-    def findleaf(self, root):
-        if not root: return []
-        if not (root.left or root.right): return [root.val]
-        return self.findleaf(root.left) + self.findleaf(root.right)
+        def getLeaves(root):
+            res = []
+
+            def dfs(node):
+                if not node: return
+
+                if not node.left and not node.right:
+                    res.append(node.val)
+                else:
+                    dfs(node.left)
+                    dfs(node.right)
+
+            dfs(root)
+            return res
+        
+        return getLeaves(root1) == getLeaves(root2)
