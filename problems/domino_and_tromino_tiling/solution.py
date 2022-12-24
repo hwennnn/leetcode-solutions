7e83b3efb72eff8299140c9c613f78b43435da40
hwennnn <hwendev@gmail.com>
2022-12-24 03:08:56 +0000

    Sync LeetCode submission - Domino and Tromino Tiling (python3)

--- problems/domino_and_tromino_tiling/solution.py
+++ problems/domino_and_tromino_tiling/solution.py
@@ -1,5 +1,8 @@
 class Solution:
     def numTilings(self, N):
         a, b, c = 0, 1, 1
-        for i in range(N - 1): a, b, c = b, c, (c + c + a) % int(1e9 + 7)
+
+        for i in range(N - 1): 
+            a, b, c = b, c, (c + c + a) % (10 ** 9 + 7)
+            
         return c
