75f850867984d34ccc2e247e6346d3c91b0f7112
hwennnn <hwendev@gmail.com>
2022-12-30 10:41:53 +0000

    Sync LeetCode submission - All Paths From Source to Target (python3)

--- problems/all_paths_from_source_to_target/solution.py
+++ problems/all_paths_from_source_to_target/solution.py
@@ -1,16 +1,18 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        n = len(graph)
+        N = len(graph)
         res = []
-        
-        def go(node, path):
-            if node == n - 1:
+
+        def dfs(node, path):
+            nonlocal res
+
+            if node == N - 1:
                 res.append(path)
                 return
             
             for nei in graph[node]:
-                go(nei, path + [nei])
+                dfs(nei, path + [nei])
         
-        go(0, [0])
-        
+        dfs(0, [0])
+
         return res
