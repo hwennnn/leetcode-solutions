3a9d963b5d4b1bc48af828178f7761d7371660e9
hwennnn <hwendev@gmail.com>
2022-12-31 02:44:48 +0000

    Sync LeetCode submission - Unique Paths III (python3)

--- problems/unique_paths_iii/solution.py
+++ problems/unique_paths_iii/solution.py
@@ -1,32 +1,30 @@
 class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        available = 0
-        sx = sy = ex = ey = 0
-        res = 0
-        
+        fullMask = (1 << (rows * cols)) - 1
+        sx, sy = (-1, -1)
+        mask = 0
+
         for x in range(rows):
             for y in range(cols):
-                if grid[x][y] != -1:
-                    available += 1
-                
                 if grid[x][y] == 1:
                     sx, sy = x, y
-                elif grid[x][y] == 2:
-                    ex, ey = x, y
-                    
-        queue = deque([(sx, sy, 1, set([(sx, sy)]))])
-        
-        while queue:
-            x, y, count, visited = queue.popleft()
+                    mask ^= (1 << (sx * cols + sy))
+                
+                if grid[x][y] == -1:
+                    mask ^= (1 << (x * cols + y))        
+
+        @cache
+        def go(x, y, mask):
+            if grid[x][y] == 2:
+                return 1 if mask == fullMask else 0
             
-            if x == ex and y == ey and count == available:
-                res += 1
-                continue
-            
+            res = 0
             for dx, dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-                if 0 <= dx < rows and 0 <= dy < cols and grid[dx][dy] != -1 and count + 1 <= available and (dx, dy) not in visited:
-                    new_visited = visited | {(dx, dy)}
-                    queue.append((dx, dy, count + 1, new_visited))
+                if 0 <= dx < rows and 0 <= dy < cols and grid[dx][dy] != -1 and mask & (1 << (dx * cols + dy)) == 0:
+                    res += go(dx, dy, mask ^ (1 << (dx * cols + dy)))
+            
+            return res
         
-        return res
+        return go(sx, sy, mask)
+
