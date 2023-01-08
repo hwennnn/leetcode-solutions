5fbf26559aec84293a44c33b1c0d67aaeb19b61e
hwennnn <hwendev@gmail.com>
2023-01-08 03:05:15 +0000

    Sync LeetCode submission - Find Xor-Beauty of Array (python3)

--- problems/find_xor-beauty_of_array/solution.py
+++ problems/find_xor-beauty_of_array/solution.py
@@ -1,11 +1,9 @@
 class Solution:
     def xorBeauty(self, nums: List[int]) -> int:
         N = len(nums)
-        curr = 0
         res = 0
         
-        for i, x in enumerate(nums):
-            curr |= x
-            res ^= curr & x
+        for x in nums:
+            res ^= x
         
         return res
