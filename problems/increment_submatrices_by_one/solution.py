3029ff49ebe4756ea302260336860290e36b0366
hwennnn <hwendev@gmail.com>
2023-01-16 14:23:21 +0000

    Sync LeetCode submission - Increment Submatrices by One (python3)

--- problems/increment_submatrices_by_one/solution.py
+++ problems/increment_submatrices_by_one/solution.py
@@ -3,20 +3,20 @@ class Solution:
         A = [[0] * n for _ in range(n)]
         
         for r1, c1, r2, c2 in queries:
-            for r in range(r1, r2 + 1):
-                A[r][c1] += 1
-                if c2 + 1 < n:
-                    A[r][c2 + 1] -= 1
+            A[r1][c1] += 1
+            if c2 + 1 < n:
+                A[r1][c2 + 1] -= 1
+            if r2 + 1 < n:
+                A[r2 + 1][c1] -= 1
+            if r2 + 1 < n and c2 + 1 < n:
+                A[r2 + 1][c2 + 1] += 1
         
-        res = []
         for i in range(n):
-            row = []
+            for j in range(1, n):
+                A[i][j] += A[i][j - 1]
+
+        for i in range(1, n):
             for j in range(n):
-                if j == 0:
-                    row.append(A[i][j])
-                else:
-                    row.append(row[-1] + A[i][j])
-            
-            res.append(row)
-        
-        return res
+                A[i][j] += A[i - 1][j]
+
+        return A
