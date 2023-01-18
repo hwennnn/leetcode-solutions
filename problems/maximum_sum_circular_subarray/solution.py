c2fe9727a5080acf17491a75821e37756d2f73d1
hwennnn <hwendev@gmail.com>
2023-01-18 11:02:45 +0000

    Sync LeetCode submission - Maximum Sum Circular Subarray (python3)

--- problems/maximum_sum_circular_subarray/solution.py
+++ problems/maximum_sum_circular_subarray/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        currMin = mmin = currMax = mmax = total = nums[0]
-        
-        for i in range(1, len(nums)):
-            total += nums[i]
-            currMin = min(nums[i], currMin + nums[i])
-            mmin = min(mmin, currMin)
-            currMax = max(nums[i], currMax + nums[i])
-            mmax = max(mmax, currMax)
+        N = len(nums)
+        currMax = mmax = currMin = mmin = total = nums[0]
 
+        for i in range(1, N):
+            currMax = max(nums[i], nums[i] + currMax)
+            mmax = max(mmax, currMax)
+            currMin = min(nums[i], nums[i] + currMin)
+            mmin = min(mmin, currMin)
+            total += nums[i]
+        
         return max(mmax, total - mmin) if mmax > 0 else mmax
