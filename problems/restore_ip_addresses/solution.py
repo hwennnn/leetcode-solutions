572d5450895aa0d0f59f1de97a8da72e02cd1a8c
hwennnn <hwendev@gmail.com>
2023-01-21 09:34:20 +0000

    Sync LeetCode submission - Restore IP Addresses (python3)

--- problems/restore_ip_addresses/solution.py
+++ problems/restore_ip_addresses/solution.py
@@ -1,23 +1,23 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        n = len(s)
+        N = len(s)
         res = []
-        
-        def backtrack(i, current, count):
-            if count > 4: return
-            if i == n and count == 4:
-                res.append(current)
+
+        def go(index, curr):
+            if len(curr) > 4: return
+            
+            if index == N and len(curr) == 4:
+                res.append(".".join(curr))
                 return
             
-            for j in range(1, 4):
-                if i + j > n: break
-                word = s[i : i + j]
+            for k in range(1, 4):
+                if index + k > N: break
+                word = s[index : index + k]
 
-                if (len(word) > 1 and word[0] == '0') or int(word) > 255: continue
-                        
-                backtrack(i + j, current + word + ('' if count == 3 else '.'), count + 1)
+                if (len(word) > 1 and word[0] == "0") or int(word) > 255:
+                    continue
                 
-        backtrack(0, '', 0)
+                go(index + k, curr + [word])
+        
+        go(0, [])
         return res
-                
-            
