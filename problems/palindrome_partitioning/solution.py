7aec89b9863c1cf0aad190ec15ac0813717e0515
hwennnn <hwendev@gmail.com>
2023-01-22 02:02:03 +0000

    Sync LeetCode submission - Palindrome Partitioning (python3)

--- problems/palindrome_partitioning/solution.py
+++ problems/palindrome_partitioning/solution.py
@@ -1,21 +1,17 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        n = len(s)
+        N = len(s)
         res = []
-        
-        def go(index, path):
-            nonlocal res
-            
-            if index == n:
-                res.append(path)    
+
+        def go(index, curr):
+            if index == N:
+                res.append(curr)
                 return
             
-            for j in range(index, n):
+            for j in range(index, N):
                 x = s[index : j + 1]
-                
                 if x == x[::-1]:
-                    go(j + 1, path + [x])
-        
+                    go(j + 1, curr + [x])
+
         go(0, [])
-        
         return res
