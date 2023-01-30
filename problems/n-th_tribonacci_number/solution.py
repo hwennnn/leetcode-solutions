93301f1c6a829cfa8fcf488ad1e884f7cdd9e776
hwennnn <hwendev@gmail.com>
2023-01-30 12:32:10 +0000

    Sync LeetCode submission - N-th Tribonacci Number (python3)

--- problems/n-th_tribonacci_number/solution.py
+++ problems/n-th_tribonacci_number/solution.py
@@ -1,7 +1,8 @@
 class Solution:
-    @cache
     def tribonacci(self, n: int) -> int:
-        if n == 0: return 0
-        if n <= 2: return 1
+        a, b, c = 0, 1, 1
+
+        for _ in range(n - 2):
+            a, b, c = b, c, a + b + c
         
-        return self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
+        return c if n != 0 else 0
