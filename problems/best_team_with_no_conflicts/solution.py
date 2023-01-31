f88d3fd507e35186a20f485bcf14acbc9a977477
hwennnn <hwendev@gmail.com>
2023-01-31 14:36:51 +0000

    Sync LeetCode submission - Best Team With No Conflicts (python3)

--- problems/best_team_with_no_conflicts/solution.py
+++ problems/best_team_with_no_conflicts/solution.py
@@ -1,16 +1,18 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        n = len(scores)
-        players = [[a, s] for a, s in zip(ages, scores)]
-        players.sort(reverse = True)
-        ans = 0
-        dp = [0]*n
+        N = len(scores)
+        A = [(a, s) for s, a in zip(scores, ages)]
+        A.sort()
+        res = 0
+        dp = [0] * (N + 1)
 
-        for i in range(n):
-            score = players[i][1]
-            dp[i] = score
+        for i in range(N):
+            dp[i] = A[i][1]
+
             for j in range(i):
-                if players[j][1] >= players[i][1]:
-                    dp[i] = max(dp[i], dp[j] + score)
-            ans = max(ans, dp[i])
-        return ans
+                if A[j][1] <= A[i][1]:
+                    dp[i] = max(dp[i], dp[j] + A[i][1])
+            
+            res = max(res, dp[i])
+        
+        return res
